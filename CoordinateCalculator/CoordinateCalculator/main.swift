f67168f7bdb98451b8460fd5067b19f7aa980b12
Kim Jung <godrm77@gmail.com>
2017-10-09 17:49:34 +0900

    프로젝트 설정 및 readme 작성

---
+++ CoordinateCalculator/CoordinateCalculator/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  CoordinateCalculator
+//
+//  Created by JK on 09/10/2017.
+//  Copyright © 2017 Codesquad Inc. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
