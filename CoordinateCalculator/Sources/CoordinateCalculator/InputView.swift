72e827d20e3294639a78f4f06920eb0dd442570c
yang hee jung <puuurm@naver.com>
2017-10-23 10:40:11 +0900

    입력을 처리하는 Input 구조체를 추가하고, 좌표값 입력하는 readInput() 함수를 구현한다.

---
+++ CoordinateCalculator/Sources/CoordinateCalculator/InputView.swift
@@ -0,0 +1,25 @@
+//
+//  InputView.swift
+//  CoordinateCalculator
+//
+//  Created by yangpc on 2017. 10. 23..
+//  Copyright © 2017년 Codesquad Inc. All rights reserved.
+//
+
+import Foundation
+
+struct InputView {
+    
+    func readInput() throws -> (Int, Int) {
+        print("\(ANSICode.cursor.move(row: 1, col: 1))\(ANSICode.clear)\(ANSICode.text.white) 좌표를 입력하세요.")
+        let input = readLine() ?? ""
+        let split = input.index(of: ",") ?? input.endIndex
+        let x = input[input.index(input.startIndex, offsetBy: 1)..<split]
+        let y = input[input.index(split, offsetBy: 1)..<input.index(before: input.endIndex)]
+        
+        let xNum: Int = Int(x) ?? 0
+        let yNum: Int = Int(y) ?? 0
+        return (xNum, yNum)
+    }
+
+}
