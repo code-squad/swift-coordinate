6fd8d8c5c9c9ffc2ed4b5037dfd0e8cb996d8e57
yang hee jung <puuurm@naver.com>
2017-10-24 13:58:25 +0900

    InputView 구조체 수정
    default 생성자 생성
    CharacterSet으로 검사하는 로직을 메소드로 분리
    메소드는 extension으로 분류

--- CoordinateCalculator/CoordinateCalculator/InputView.swift
+++ CoordinateCalculator/CoordinateCalculator/InputView.swift
@@ -21,27 +21,34 @@ enum NumberOfPoints: Int {
 
 struct InputView {
     
-    private(set) var point: MyPoint = MyPoint()
-    private(set) var line: MyLine = MyLine()
+    private(set) var point: MyPoint
+    private(set) var line: MyLine
+    
+    init() {
+        point = MyPoint()
+        line = MyLine()
+    }
+}
+
+// Methods
+extension InputView {
     
     mutating func readInput() throws -> NumberOfPoints? {
         print("\(ANSICode.cursor.move(row: 1, col: 1))\(ANSICode.clear)\(ANSICode.text.white) 좌표를 입력하세요. (종료하려면 엔터 입력)")
         guard let input = readLine() else { throw InputError.emptyInput }
         if input == "" { return nil }
         
-        let validInput = CharacterSet.init(charactersIn: "()-,0123456789")
-        let filter = input.trimmingCharacters(in: validInput)
-        if !filter.isEmpty { throw InputError.invalidInput }
+        if checkInvalidCharacters(input) { throw InputError.invalidInput }
 
-        let points = splitInputToPoint(input: input)
+        let points = splitInputToPoint(input)
         let count = points.count
         
         var pointArray = [MyPoint]()
         
         for point in points {
-            guard let xy = splitXY(point: point) else { throw InputError.invalidInput }
-            guard let xNum: Int = Int(xy.0) else { throw InputError.invalidInput }
-            guard let yNum: Int = Int(xy.1) else { throw InputError.invalidInput }
+            guard let xy = splitXY(point) else { throw InputError.invalidInput }
+            let xNum: Int = Int(xy.0) ?? 0
+            let yNum: Int = Int(xy.1) ?? 0
             if xNum > 24 || yNum > 24 { throw InputError.outOfNumber }
             pointArray.append(MyPoint(x: xNum, y: yNum))
         }
@@ -52,23 +59,29 @@ struct InputView {
         case 2:
             self.line = MyLine(pointA: pointArray[0], pointB: pointArray[1])
             return .line
-        default:
-            break
+        default: break
         }
         return nil
     }
 
     // "-" 기준으로 나누기
-    private func splitInputToPoint(input: String) -> [String] {
+    private func splitInputToPoint(_ input: String) -> [String] {
         return input.split(separator: "-").map(String.init)
     }
     
     // x, y 로 나누기
-    private func splitXY(point: String) -> (Substring, Substring)? {
+    private func splitXY(_ point: String) -> (Substring, Substring)? {
         guard let split = point.index(of: ",") else { return nil }
         let x = point[point.index(point.startIndex, offsetBy: 1)..<split]
         let y = point[point.index(split, offsetBy: 1)..<point.index(before: point.endIndex)]
         return (x, y)
+    }
+    
+    // 입력 스트링이 유효한지 검사
+    private func checkInvalidCharacters(_ input: String) -> Bool {
+        let validInput = CharacterSet.init(charactersIn: "()-,0123456789")
+        let filter = input.trimmingCharacters(in: validInput)
+        if !filter.isEmpty { return true } else { return false }
     }
 }
 
