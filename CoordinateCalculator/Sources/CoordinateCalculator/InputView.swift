9dda446afe4a643e639b0b7981119e2b846a78d6
undervine <undervine@naver.com>
2017-10-26 16:30:33 +0900

    Modify main() to get re-input when the coords is over AxisLimit

--- CoordinateCalculator/Sources/CoordinateCalculator/InputView.swift
+++ CoordinateCalculator/Sources/CoordinateCalculator/InputView.swift
@@ -15,16 +15,19 @@ struct InputView {
         return inputLine
     }
     
-    static func readInput(coordsWithBracelet: String)->MyPoint{
+    static func readInput(rawCoords: String)->MyPoint{
         var userPoint = MyPoint()
         // 좌표 정보는 괄호"(", ")"로 둘러쌓여 있으며 쉼표(,)로 x값과 y값을 구분한다. 문자열 분리한다.
+        var coordsWithBracelet = rawCoords.split(separator: ",")
+        print(rawCoords)
+        print(coordsWithBracelet)
         // 문자열로 얻은 x,y 값으로 MyPoint 객체 생성
         return userPoint
     }
     
-    static func isUnderAxisLimit(_ point: MyPoint)->Bool{
-        var isUnder: Bool = false
-        // ANSICode.axis.AxisLimit 을 넘지 않으면 true 반환. 넘으면 false 반환.
-        return isUnder
+    static func isOverAxisLimit(_ point: MyPoint)->Bool{
+        var isUnderAxisLimit: Bool = false
+        // ANSICode.axis.AxisLimit 을 넘으면 true 반환. 넘지 않으면 false 반환.
+        return isUnderAxisLimit
     }
 }
--- CoordinateCalculator/Sources/CoordinateCalculator/main.swift
+++ CoordinateCalculator/Sources/CoordinateCalculator/main.swift
@@ -11,16 +11,25 @@ import Foundation
 func main(){
     // 좌표축 그림.
     OutputView.drawAxis()
-    // 사용자 입력 메뉴.
-    guard let userInput = InputView.input(message: "좌표를 입력하세요: ") else{ return }
-    // 사용자 입력 문자열에서 x,y 값을 분리하여 MyPoint 객체로 변환.
-    let userPoint = InputView.readInput(coordsWithBracelet: userInput)
-    // X,Y 좌표 모두 최대 24까지만 입력 가능. 입력범위 초과 시, 에러 메시지 출력. 재입력 받음.
-    guard InputView.isUnderAxisLimit(userPoint) else { print(OutputView.axisErrorMessage); return }
+    // 사용자 입력 좌표 저장 변수.
+    var userPoint: MyPoint = MyPoint()
+    // while문 실행 횟수.
+    var execCount: Int = 1
+    var userInput: String = ""
+    repeat{
+        // 사용자 입력 메뉴.
+        userInput = InputView.input(message: "좌표를 입력하세요: ")!
+        // 사용자 입력 문자열에서 x,y 값을 분리하여 MyPoint 객체로 변환.
+        userPoint = InputView.readInput(rawCoords: userInput)
+        // 첫 실행이 아니면 에러 메시지는 스킵.
+        if execCount != 1 { print(OutputView.axisErrorMessage) }
+        // 실행 횟수 +1
+        execCount += 1
+    }while(InputView.isOverAxisLimit(userPoint) || (userInput == "q" || userInput == "quit"))
+    
     // 정상적인 좌표값인 경우, 해당 좌표에 특수문자 표시.
     OutputView.printHeart(at: userPoint)
 }
 
-while true {
-    main()
-}
+main()
+
