6a0d2f2f4abdc17d89a5c19cb80d909a15110c9f
undervine <undervine@naver.com>
2017-10-29 03:31:14 +0900

    Add generateFigures() func to InputView.swift
    Add generateFigures() func to make line, triangle, rectangle etc. from user input points.

--- CoordinateCalculator/Sources/CoordinateCalculator/InputView.swift
+++ CoordinateCalculator/Sources/CoordinateCalculator/InputView.swift
@@ -11,25 +11,23 @@ struct InputView {
     // 입력 가능한 문자열 패턴. ex. (3,5)- 형식 가능.
     static let validInputPattern: String = "\\([0-9]+,[0-9]+\\)-?"
     
-    // MyPoint 구조체 관련 에러 및 에러메시지 종류.
-    enum CoordsError: String, Error{
-        case isNil = "입력값이 없습니다."                           // nil 에러.
-        case outOfBounds = "좌표가 24를 넘지 않도록 입력해주세요."      // 좌표계 최대범위 초과 에러.
-        case invalidInputPattern = "(Int, Int) 또는 (Int, Int)-(Int, Int)-.. 형식을 맞춰주세요."  // 입력 패턴 에러.
-    }
-    
     // 사용자 입력값을 MyPoint 객체로 변환.
-    static func readInput(rawCoords: String) throws -> [MyPoint]{
+    static func readInput(rawCoords: String) throws -> Any{
         let stringsInCoordPattern = rawCoords.split(pattern: InputView.validInputPattern)
         // 입력 패턴이 유효하지 않으면 에러처리.
-        guard stringsInCoordPattern.count > 0 else { throw InputView.CoordsError.invalidInputPattern }
-        
+        guard stringsInCoordPattern.count > 0 else { throw OutputView.CoordsError.invalidInputPattern }
         let points = generatePoints(from: stringsInCoordPattern)
-        
         // 좌표축 최대범위를 넘으면 에러처리. (에러 출력은 메인에서. 출력함수는 OutputView에 구현.)
-        guard isUnderAxisLimit(points) else{ throw InputView.CoordsError.outOfBounds }
-        // 문자열로 얻은 x,y 값으로 MyPoint 객체 생성.
-        return points
+        guard isUnderAxisLimit(points) else{ throw OutputView.CoordsError.outOfBounds }
+        return generateFigures(from: points)
+    }
+    
+    private static func generateFigures(from points: [MyPoint]) -> Any{
+        switch points.count {
+        case 1: return points
+        case 2: return MyLine(pointA: points[0], pointB: points[1])
+        default: return points
+        }
     }
     
     private static func generatePoints(from stringsInCoordPattern: [String]) -> [MyPoint]{
