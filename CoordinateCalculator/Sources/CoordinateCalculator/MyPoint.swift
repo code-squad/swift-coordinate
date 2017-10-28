b5909acb446e3991b8e70f4a6d9eec2487b77f32
undervine <undervine@naver.com>
2017-10-28 23:02:50 +0900

    Add erase(at:) func to remove error message
    Add erase(at:) func to remove error message at the middle of coordinate plane, so that the error message does not overlap with HEART.

--- CoordinateCalculator/Sources/CoordinateCalculator/MyPoint.swift
+++ CoordinateCalculator/Sources/CoordinateCalculator/MyPoint.swift
@@ -8,11 +8,12 @@
 import Foundation
 
 struct MyPoint{
-    var x: Int = 0
-    var y: Int = 0
+    var x: Int = 0      // x좌표 (col)
+    var y: Int = 0      // y좌표 (raw)
     
+    // MyPoint 구조체 관련 에러 및 에러메시지 종류.
     enum PointError: String, Error{
-        case isNil = "입력값이 없습니다."
-        case outOfBounds = "좌표가 24를 넘지 않도록 입력해주세요."
+        case isNil = "입력값이 없습니다."                           // nil 에러.
+        case outOfBounds = "좌표가 24를 넘지 않도록 입력해주세요."      // 좌표계 최대범위 초과 에러.
     }
 }
