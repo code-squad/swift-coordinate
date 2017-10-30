1f544bcc62cf8d21b5d404efefe408e64a019ec9
ChocOZerO <nayalth@gmail.com>
2017-10-30 17:30:28 +0900

    [Update]좌표가 입력된 위치에 특수문자 출력

---
+++ Sources/swift-coordinate/MyPoint.swift
@@ -0,0 +1,16 @@
+//
+//  MyPoint.swift
+//  CoordinateCalculator
+//
+//  Created by TaeHyeonLee on 2017. 10. 27..
+//  Copyright © 2017년 Codesquad Inc. All rights reserved.
+//
+
+import Foundation
+
+struct MyPoint {
+    var x = 0
+    var y = 0
+}
+
+
