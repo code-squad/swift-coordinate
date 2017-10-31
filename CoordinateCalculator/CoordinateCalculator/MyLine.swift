150a59bceb3d1491c29a6f24e0380e0a65437a6e
ChocOZerO <nayalth@gmail.com>
2017-10-31 11:23:23 +0900

    [Add]입력받은 두개의 좌표값 정보처리를 위한 MyLine구조체 추가

---
+++ CoordinateCalculator/CoordinateCalculator/MyLine.swift
@@ -0,0 +1,14 @@
+//
+//  MyLine.swift
+//  CoordinateCalculator
+//
+//  Created by TaeHyeonLee on 2017. 10. 31..
+//  Copyright © 2017년 Codesquad Inc. All rights reserved.
+//
+
+import Foundation
+
+struct MyLine {
+    var pointA = MyPoint(x: 0, y: 0)
+    var pointB = MyPoint(x: 0, y: 0)
+}
