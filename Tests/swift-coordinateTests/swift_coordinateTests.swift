f5ea0ca36fdcd75c8793853e5c152a9b187b3261
undervine <undervine@naver.com>
2017-10-31 10:44:42 +0900

    Add swift_coordinateTests file

--- Tests/swift-coordinateTests/swift_coordinateTests.swift
+++ Tests/swift-coordinateTests/swift_coordinateTests.swift
@@ -2,15 +2,5 @@ import XCTest
 @testable import swift_coordinate
 
 class swift_coordinateTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(swift_coordinate().text, "Hello, World!")
-    }
-
-
-    static var allTests = [
-        ("testExample", testExample),
-    ]
+    
 }
