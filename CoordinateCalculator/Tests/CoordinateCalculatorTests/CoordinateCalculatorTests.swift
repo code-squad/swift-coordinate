2912f0832e70a7e43966c2da39060acad2ceedff
undervine <undervine@naver.com>
2017-11-02 10:36:13 +0900

    Add unit test codes to CoordinateCalculatorTests.swift

--- CoordinateCalculator/Tests/CoordinateCalculatorTests/CoordinateCalculatorTests.swift
+++ CoordinateCalculator/Tests/CoordinateCalculatorTests/CoordinateCalculatorTests.swift
@@ -2,5 +2,29 @@ import XCTest
 @testable import CoordinateCalculator
 
 class CoordinateCalculatorTests: XCTestCase {
+    func testMyPointIsNotNil(){
+        let myPoint = MyPoint(x: 3, y: 5)
+        XCTAssertNotNil(myPoint)
+    }
     
+    func testMyLineIsNotNil(){
+        let pointA = MyPoint(x: 3, y: 6)
+        let pointB = MyPoint(x: 7, y: 9)
+        let myLine = MyLine(pointA: pointA, pointB: pointB)
+        XCTAssertNotNil(myLine)
+    }
+    
+    func testMyTriangleIsNotNil(){
+        let pointA = MyPoint(x: 3, y: 6)
+        let pointB = MyPoint(x: 7, y: 9)
+        let pointC = MyPoint(x: 5, y: 1)
+        let myTriangle = MyTriangle(pointA: pointA, pointB: pointB, pointC: pointC)
+        XCTAssertNotNil(myTriangle)
+    }
+    
+    func testMyRectIsNotNil(){
+        let origin = MyPoint(x: 4, y: 6)
+        let myRect = MyRect(origin: origin, size: CGSize(width: 5, height: 9))
+        XCTAssertNotNil(myRect)
+    }
 }
