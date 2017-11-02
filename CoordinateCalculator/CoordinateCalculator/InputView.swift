a60341d1f367ffe8e99fd54ca66fed62a425fa3e
ChocOZerO <nayalth@gmail.com>
2017-11-02 14:54:32 +0900

    [Refactoring]에러 핸들링을 활용하여 전체 입력로직은 메인에서 처리
    CustomErrors를 만들어서 에러핸들링처리
    기존에 잘못된 입력시 InputView에서만 함수스택이 쌓이면서 재호출했던 로직변경
    main에서 에러 핸들링을 하면서 사용자 입력을 돕는 메시지 출력
    함수스택을 쌓지 않고 throw와 while로 효율적 처리

--- CoordinateCalculator/CoordinateCalculator/InputView.swift
+++ CoordinateCalculator/CoordinateCalculator/InputView.swift
@@ -12,54 +12,40 @@ struct InputView {
     
     let customCharacterSet : CharacterSet = CharacterSet.init(charactersIn: "()-,")
     
-    enum MessageGuide : String {
-        case pointInput = "좌표를 입력하세요."
-        case invalidInput = "입력값에 유효하지 않은 문자 혹은 기호가 들어가 있습니다."
-        case wrongPoint = "좌표 입력이 올바르지 않습니다."
-        case pointRange = "숫자는 1부터 24까지 가능합니다."
-    }
-    
-    func readInput() -> Points {
-        print(MessageGuide.pointInput.rawValue)
+    func readInput() throws -> Points {
         let formula = readLine() ?? ""
         if !checkAvailableCharacterSet(formula: formula) {
-            print(MessageGuide.invalidInput.rawValue)
-            return readInput()
+            throw CustomErrors.InputError.invalidCharacter
         }
         let inputPoints = formula.split(separator: "-").map({String($0)})
-        return getPoints(inputPoints: inputPoints)
+        return try getPoints(inputPoints: inputPoints)
     }
     
-    private func getPoints(inputPoints: [String]) -> Points {
+    private func getPoints(inputPoints: [String]) throws -> Points {
         var points : Points = []
         for inputPoint in inputPoints {
-            points.append(contentsOf: getPoint(formula: inputPoint))
+            points.append(contentsOf: try getPoint(formula: inputPoint))
         }
         return points
     }
     
-    private func getPoint(formula: String) -> Points {
+    private func getPoint(formula: String) throws -> Points {
         let point = formula.trimmingCharacters(in: customCharacterSet).split(separator: ",")
-        if !checkInputValidation(point: point) {
-            print(MessageGuide.wrongPoint.rawValue)
-            return readInput()
-        }
+        try checkInputValidation(point: point)
         let points : Points = [(x: Int(point[0])!, y: Int(point[1])!)]
         return points
     }
     
-    private func checkInputValidation(point: Array<String.SubSequence>) -> Bool {
+    private func checkInputValidation(point: Array<String.SubSequence>) throws {
         guard point.count == 2 else {
-            return false
+            throw CustomErrors.InputError.wrongPoint
         }
         if Int(point[0]) == nil || Int(point[1]) == nil {
-            return false
+            throw CustomErrors.InputError.wrongPoint
         }
         if Int(point[0])! <= 0 || Int(point[0])! > 24  || Int(point[1])! <= 0 || Int(point[1])! > 24 {
-            print(MessageGuide.pointRange.rawValue)
-            return false
+            throw CustomErrors.InputError.invalidRange
         }
-        return true
     }
     
     private func checkAvailableCharacterSet(formula: String) -> Bool {
---
+++ Sources/swift-coordinate/CustomErrors.swift
@@ -0,0 +1,17 @@
+//
+//  CustomErrors.swift
+//  CoordinateCalculator
+//
+//  Created by TaeHyeonLee on 2017. 11. 2..
+//  Copyright © 2017년 Codesquad Inc. All rights reserved.
+//
+
+import Foundation
+
+struct CustomErrors {
+    enum InputError : Error {
+        case invalidCharacter // "입력값에 유효하지 않은 문자 혹은 기호가 들어가 있습니다."
+        case invalidRange // "숫자는 1부터 24까지 가능합니다."
+        case wrongPoint // "좌표 입력이 올바르지 않습니다."
+    }
+}
