33dd45907f7818fa763beacc33ffa5da48e6de18
ChocOZerO <nayalth@gmail.com>
2017-11-02 14:54:32 +0900

    [Refactoring]에러 핸들링을 활용하여 전체 입력로직은 메인에서 처리
    CustomErrors를 만들어서 에러핸들링처리
    기존에 잘못된 입력시 InputView에서만 함수스택이 쌓이면서 재호출했던 로직변경
    main에서 에러 핸들링을 하면서 사용자 입력을 돕는 메시지 출력
    함수스택을 쌓지 않고 throw와 while로 효율적 처리

--- Sources/swift-coordinate/main.swift
+++ Sources/swift-coordinate/main.swift
@@ -11,7 +11,21 @@ import Foundation
 typealias Points = [(x: Int, y: Int)]
 
 let inputView = InputView()
-let points = inputView.readInput()
+var points : Points = []
+while points.count == 0 {
+    do {
+        print("좌표를 입력하세요.")
+        points = try inputView.readInput()
+    } catch CustomErrors.InputError.invalidCharacter {
+        print("입력값에 유효하지 않은 문자 혹은 기호가 들어가 있습니다.")
+    } catch CustomErrors.InputError.invalidRange {
+        print("숫자는 1부터 24까지 가능합니다.")
+    } catch CustomErrors.InputError.wrongPoint {
+        print("좌표 입력이 올바르지 않습니다.")
+    } catch {
+        print("입력이 정상적이지 않습니다.")
+    }
+}
 
 let figureCreator = FigureCreator()
 let figure = figureCreator.createFigure(points: points)
