a9dc7817463349e57ab4a2efa1bb8fb43bda3c7f
DongJin Lee <comets195@gmail.com>
2017-11-03 15:37:53 +0900

    Edit Files Underpinned codacy.

--- CoordinateCalculator/CoordinateCalculator/InputView.swift
+++ CoordinateCalculator/CoordinateCalculator/InputView.swift
@@ -10,6 +10,7 @@ import Foundation
 
 struct InputView {
     private var points: String = ""
+    
     mutating func readInput() {
         print("좌표를 입력하세요.")
         let inputData = readLine()
@@ -19,11 +20,12 @@ struct InputView {
         }
         self.points = points
     }
+    
     mutating func extract() throws -> MyPoint {
         var dotPoint: MyPoint = MyPoint()
         points.remove(at: points.startIndex)
         points.remove(at: points.index(before: points.endIndex))
-        let separatePoint = points.components(separatedBy: ",").flatMap({ Int($0) })
+        let separatePoint = points.components(separatedBy: ",").flatMap{Int($0)}
         if confirm(points: separatePoint) {
             dotPoint.x = separatePoint[0]
             dotPoint.y = separatePoint[1]
@@ -32,6 +34,10 @@ struct InputView {
         throw InputViewError.invalidPoint
     }
     private func confirm(points: [Int]) -> Bool {
+        if points.isEmpty{
+            print("다시입력해주세요. 괄호안에는 정수가 와야합니다. :)")
+            return false
+        }
         for index in 0...1 {
             if points[index] < 0 || points[index] > 24 {
                 print("x,y 값은 0 <= x,y <= 24 합니다. :) 다시입력해주세요.")
@@ -40,4 +46,5 @@ struct InputView {
         }
         return true
     }
+    
 }
