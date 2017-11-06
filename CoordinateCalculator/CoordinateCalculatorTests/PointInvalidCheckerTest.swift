8b06b7a43ae0cb3acb23be88f62ab511a7b7fd49
ChocOZerO <nayalth@gmail.com>
2017-11-06 11:39:38 +0900

    [AddTest]PointInvalidationChecker Unit Test 추가

--- CoordinateCalculator/CoordinateCalculatorTests/PointInvalidCheckerTest.swift
+++ CoordinateCalculator/CoordinateCalculatorTests/PointInvalidCheckerTest.swift
@@ -12,6 +12,8 @@ import XCTest
 
 class PointInvalidCheckerTest: XCTestCase {
     var pointInvalidationChecker : PointInvalidationChecker!
+    let customCharacterSet : CharacterSet = CharacterSet.init(charactersIn: "()-,")
+    
     override func setUp() {
         super.setUp()
         pointInvalidationChecker = PointInvalidationChecker()
@@ -21,20 +23,47 @@ class PointInvalidCheckerTest: XCTestCase {
         super.tearDown()
         pointInvalidationChecker = nil
     }
-    /* private functions
+    
     func testCheckInputValidataion() {
         var formula = "(10,10)"
-        var tmpPoint = formula.trimmingCharacters(in: pointInvalidationChecker.customCharacterSet).split(separator: ",")
+        var tmpPoint = formula.trimmingCharacters(in: customCharacterSet).split(separator: ",")
         XCTAssertEqual(try pointInvalidationChecker.checkInputValidation(formula: formula), tmpPoint)
         formula = "[(10,10)]"
-        tmpPoint = formula.trimmingCharacters(in: pointInvalidationChecker.customCharacterSet).split(separator: ",")
+        tmpPoint = formula.trimmingCharacters(in: customCharacterSet).split(separator: ",")
         XCTAssertThrowsError(try pointInvalidationChecker.checkInputValidation(formula: formula))
         formula = "(25,10)"
-        tmpPoint = formula.trimmingCharacters(in: pointInvalidationChecker.customCharacterSet).split(separator: ",")
+        tmpPoint = formula.trimmingCharacters(in: customCharacterSet).split(separator: ",")
         XCTAssertThrowsError(try pointInvalidationChecker.checkInputValidation(formula: formula))
-        
     }
     
+    func testCheckAvailableCharacterSet() {
+        var formula = "(10,10)"
+        XCTAssertNoThrow(try pointInvalidationChecker.checkAvailableCharacterSet(formula: formula))
+        formula = "(10,test)"
+        XCTAssertThrowsError(try pointInvalidationChecker.checkAvailableCharacterSet(formula: formula))
+        formula = "(10,+)"
+        XCTAssertThrowsError(try pointInvalidationChecker.checkAvailableCharacterSet(formula: formula))
+        formula = "(10,10)-(14,15)"
+        XCTAssertNoThrow(try pointInvalidationChecker.checkAvailableCharacterSet(formula: formula))
+        formula = "(10,10)-(14,15)-(20,8)"
+        XCTAssertNoThrow(try pointInvalidationChecker.checkAvailableCharacterSet(formula: formula))
+        formula = "(10,10)-(14,15)-(20,8)-(2,3)"
+        XCTAssertNoThrow(try pointInvalidationChecker.checkAvailableCharacterSet(formula: formula))
+    }
+    
+    func testCheckPointInvalidation() {
+        var points = [(1,5),(3,5),(1,10),(3,10)]
+        XCTAssertNoThrow(try pointInvalidationChecker.checkPointInvalidation(points: points))
+        points = [(1,5),(1,5),(1,10),(3,10)]
+        XCTAssertThrowsError(try pointInvalidationChecker.checkPointInvalidation(points: points))
+        points = [(1,5),(3,5),(3,10),(3,10)]
+        XCTAssertThrowsError(try pointInvalidationChecker.checkPointInvalidation(points: points))
+        points = [(1,5),(3,5)]
+        XCTAssertNoThrow(try pointInvalidationChecker.checkPointInvalidation(points: points))
+        points = [(1,5),(1,5)]
+        XCTAssertThrowsError(try pointInvalidationChecker.checkPointInvalidation(points: points))
+    }
+    /* private functions
     func testGetAvailableCharacterSet() {
         XCTAssertEqual(0, "(10,10)-".trimmingCharacters(in: pointInvalidationChecker.getAvailableCharacterSet()).count)
         XCTAssertEqual(0, "(10,10)-(14,15)".trimmingCharacters(in: pointInvalidationChecker.getAvailableCharacterSet()).count)
@@ -61,5 +90,5 @@ class PointInvalidCheckerTest: XCTestCase {
         points = [(1,2), (4,2), (1,5), (4,5)]
         XCTAssertNoThrow(try pointInvalidationChecker.countRectY(points: points))
     }
-     */
+    */
 }
