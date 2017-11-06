d9dc6aa9aa9c474d6f839aa8fce770114b7838e4
DongJin Lee <comets195@gmail.com>
2017-11-06 17:56:10 +0900

    Add distance calculate func().

--- CoordinateCalculator/CoordinateCalculator/MyLine.swift
+++ CoordinateCalculator/CoordinateCalculator/MyLine.swift
@@ -11,6 +11,11 @@ import Foundation
 struct MyLine {
     var pointA = MyPoint(x: 0, y: 0)
     var pointB = MyPoint(x: 0, y: 0)
-    
-    
+    //제곱근((A.x - B.x)^제곱 + (A.y - B.y)^제곱)
+    func calcurateDistanceTwoPoints() {
+        let differentialX = Double(pointA.x - pointB.x)
+        let differentialY = Double(pointA.y - pointB.y)
+        let resultDistance = sqrt(Double(pow(differentialX, 2) + pow(differentialY, 2)))
+        print("두점사이의 거리는 : \(resultDistance)")
+    }
 }
