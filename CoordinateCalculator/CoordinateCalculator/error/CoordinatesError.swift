0327b69cbbb89554b790b199b13b2317b51092e3
yuaming <miennes3@gmail.com>
2017-11-07 09:46:27 +0900

    [수정] 좌표계산기 2단계 수정
    1. Error, String Extension 분리
    2. 필요하지 않은 메서드 정리

---
+++ CoordinateCalculator/CoordinateCalculator/error/CoordinatesError.swift
@@ -0,0 +1,27 @@
+//
+//  Error.swift
+//  CoordinateCalculator
+//
+//  Created by yuaming on 2017. 11. 6..
+//  Copyright © 2017년 Codesquad Inc. All rights reserved.
+//
+
+import Foundation
+
+enum CoordinatesError: Error, CustomStringConvertible {
+    case emptyValue
+    case outOfRangeCoordinate
+    case notFormattedValue
+    
+    var description: String {
+        switch self {
+        case .emptyValue:
+            return "입력값이 없습니다."
+        case .outOfRangeCoordinate:
+            return "입력값이 범위를 벗어났습니다. 0-24 사이값으로 입력하세요."
+        case .notFormattedValue: 
+            return "입력형식이 유효하지 않습니다."
+        }
+    }
+}
+
