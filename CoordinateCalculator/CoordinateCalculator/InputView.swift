d317c8a023353f8e48addf8a402ae16071608a1f
dely2p <lhyal2p@gmail.com>
2017-11-09 12:23:39 +0900

    fix spacing word by codacy code review

--- CoordinateCalculator/CoordinateCalculator/InputView.swift
+++ CoordinateCalculator/CoordinateCalculator/InputView.swift
@@ -10,7 +10,6 @@ import Foundation
 
 struct InputView {
     private var inputCoordinate = ""
-    
     mutating func readInput() -> String {
         print("좌표를 입력하세요.")
         if let inputCoordinate = readLine() {
@@ -18,6 +17,7 @@ struct InputView {
         }
         return inputCoordinate
     }
+    
     func separateCoordinateNumber(inputValue: String) -> MyPoint {
         if inputValue.hasPrefix("(") && inputValue.hasSuffix(")") {
             let rangeOfNumber = inputValue.index(after: inputValue.startIndex)..<inputValue.index(before: inputValue.endIndex)
@@ -29,12 +29,16 @@ struct InputView {
         }else {
             print("형식에 맞지 않는 입력값입니다. ()로 감싸지 않은 값")
         }
-        return myPoint
+        return MyPoint(x: 0, y: 0)
     }
-    func separateByComma(rangeOfNumber: String) -> MyPoint{
+    
+    func separateByComma(rangeOfNumber: String) -> MyPoint {
         var myPoint = MyPoint(x: 0, y: 0)
         let values = rangeOfNumber.split(separator: ",").flatMap({Int($0)})
-        print("(x,y): ",values[0], values[1])
+        if values.count != 2 {
+            print("입력된 숫자 갯수가 2개가 아니거나 아니거나 숫자가 아닙니다.")
+            return myPoint
+        }
         myPoint.x = values[0]
         myPoint.y = values[1]
         return myPoint
