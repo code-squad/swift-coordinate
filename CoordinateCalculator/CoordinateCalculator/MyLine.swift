bf8926f23a1a8f8052bf8c1445d1e41b73d7bc71
dely2p <lhyal2p@gmail.com>
2017-11-09 16:14:02 +0900

    add struct MyLine

---
+++ CoordinateCalculator/CoordinateCalculator/MyLine.swift
@@ -0,0 +1,14 @@
+//
+//  MyLine.swift
+//  CoordinateCalculator
+//
+//  Created by Eunjin Kim on 2017. 11. 9..
+//  Copyright © 2017년 Codesquad Inc. All rights reserved.
+//
+
+import Foundation
+
+struct MyLine {
+    var pointA = MyPoint(x: 0, y: 0)
+    var pointB = MyPoint(x: 0, y: 0)
+}
