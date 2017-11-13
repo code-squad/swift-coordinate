ccb1009dee94086ffce890e01a93cf42d82a5c90
dely2p <lhyal2p@gmail.com>
2017-11-13 12:00:44 +0900

    add calculateOfLength() method and fix the name of calculatorOfPosition() method to calculateOfPosition()

--- CoordinateCalculator/CoordinateCalculator/MyLine.swift
+++ CoordinateCalculator/CoordinateCalculator/MyLine.swift
@@ -19,12 +19,12 @@ struct MyLine: MyShape {
         self.pointB = pointB
     }
     
-    func calculatorOfPosition() -> String {
+    func calculateOfPosition() -> String {
         return "\(ANSICode.text.redBright)\(ANSICode.cursor.move(row: 25 - pointA.y, col: (pointA.x * 2) + 3))\(ANSICode.text.dot)\(ANSICode.text.redBright)\(ANSICode.cursor.move(row: 25 - pointB.y, col: (pointB.x * 2) + 3))\(ANSICode.text.dot)"
     }
     
-    func printValueOfResult() {
-        let result = sqrt(abs(pow(Double(pointA.x - pointB.x),2) + pow(Double(pointA.y - pointB.y),2)))
-        print("\(ANSICode.cursor.move(row: 30, col: -1))\(ANSICode.text.black)  두 점 사이 거리는 \(result)")
+    func calculateOfLength() -> Double {
+        return sqrt(abs(pow(Double(pointA.x - pointB.x),2) + pow(Double(pointA.y - pointB.y),2)))
     }
+    
 }
