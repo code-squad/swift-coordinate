4c644d9954a1f870ff4a2c9562ecc84236d8a1cc
dely2p <lhyal2p@gmail.com>
2017-11-13 11:53:52 +0900

    fix object properties to private - MyLine, MyPoint

--- CoordinateCalculator/CoordinateCalculator/MyLine.swift
+++ CoordinateCalculator/CoordinateCalculator/MyLine.swift
@@ -10,12 +10,21 @@ import Foundation
 
 struct MyLine: MyShape {
     var type: Figure
-    var pointA = MyPoint(x: 0, y: 0)
-    var pointB = MyPoint(x: 0, y: 0)
+    private var pointA = MyPoint(x: 0, y: 0)
+    private var pointB = MyPoint(x: 0, y: 0)
 
     init(pointA: MyPoint, pointB: MyPoint) {
         self.type = Figure.line
         self.pointA = pointA
         self.pointB = pointB
+    }
+    
+    func calculatorOfPosition() -> String {
+        return "\(ANSICode.text.redBright)\(ANSICode.cursor.move(row: 25 - pointA.y, col: (pointA.x * 2) + 3))\(ANSICode.text.dot)\(ANSICode.text.redBright)\(ANSICode.cursor.move(row: 25 - pointB.y, col: (pointB.x * 2) + 3))\(ANSICode.text.dot)"
+    }
+    
+    func printValueOfResult() {
+        let result = sqrt(abs(pow(Double(pointA.x - pointB.x),2) + pow(Double(pointA.y - pointB.y),2)))
+        print("\(ANSICode.cursor.move(row: 30, col: -1))\(ANSICode.text.black)  두 점 사이 거리는 \(result)")
     }
 }
