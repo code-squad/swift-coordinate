9dbd79cabd41132349139d10d4ff6a55e35688e7
waitNseE <yellow.cjh@gmail.com>
2017-11-20 23:03:02 +0900

    Add mdthod of convertingToInt

--- CoordinateCalculator/CoordinateCalculator/Checking.swift
+++ CoordinateCalculator/CoordinateCalculator/Checking.swift
@@ -10,23 +10,26 @@ import Foundation
 
 struct Checking {
     enum ErrorMessage: String, Error {
-        case ofInValidInputedValue = "좌표를 입력해주세요."
-        case ofNonexistenceComma = "올바른 형태로 입력해주세요."
-        case ofExceedValidInput = "최대 입력가능한 값은 24입니다."
+        case ofInValidInputedValue = "(x,y)형태로 입력해야 합니다."
+        case ofNonexistenceComma = "x와 y의 값은 콤마로 구분되어야 합니다."
+        case ofExceedValidInput = "x,y 각각 최대 입력가능한 값은 24입니다."
+        case ofValueIsNotInt = "입력된 좌표가 숫자가 아닙니다."
         case ofUnKnownError = "알려지지 않은 에러입니다. 관리자에게 문의하세요."
     }
     
-    func returnCheckedValues (_ input: String?) throws -> MyPoint {
+    func returnCheckedValues (_ input: String) throws -> MyPoint {
         guard let temp = elimateParenthesis(input) else { throw ErrorMessage.ofInValidInputedValue }
         guard let validValues = checkCommaInInputs(temp) else { throw ErrorMessage.ofNonexistenceComma }
-        guard let validValuesHasComma = checkisNotExceed(validValues) else { throw ErrorMessage.ofExceedValidInput}
-        let values = MyPoint(x: Int(validValuesHasComma[0]) ?? 0, y: Int(validValuesHasComma[1]) ?? 0)
+        guard let valueOfInt = converToInt(validValues) else { throw ErrorMessage.ofValueIsNotInt}
+        guard let validValuesHasComma = checkisNotExceed(valueOfInt) else { throw ErrorMessage.ofExceedValidInput}
+        let values = MyPoint(x: validValuesHasComma[0], y: validValuesHasComma[1])
         return values
     }
     
     // 좌우 괄호 제거 : nil이면 입력값이 없다는거고 ofEmptedInput 출력
-    private func elimateParenthesis (_ input: String?) -> String? {
-        if var input = input {
+    private func elimateParenthesis (_ input: String) -> String? {
+        var input = input
+        if input.contains("(") && input.contains(")") {
             input = input.trimmingCharacters(in: ["(", ")"])
             return input
         } else {
@@ -42,10 +45,19 @@ struct Checking {
         return temp
     }
     
+    // 인트로 바꾸기
+    private func converToInt (_ input: Array<String>) -> Array<Int>? {
+        let temp = input.flatMap{ tempValue in Int(tempValue) }
+        if temp.count != input.count {
+            return nil
+        }
+        return temp
+    }
+    
     // 24를 초과했는지 체크
-    private func checkisNotExceed (_ input: Array<String>) -> Array<String>? {
+    private func checkisNotExceed (_ input: Array<Int>) -> Array<Int>? {
         for index in 0 ..< input.count {
-            guard Int(input[index]) ?? 25 <= 24 else { return nil }
+            guard input[index] <= 24 else { return nil }
         }
         return input
     }
