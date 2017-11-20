be2d1bfee349dd5682e4bcc94f36118bec48950f
waitNseE <yellow.cjh@gmail.com>
2017-11-20 21:09:43 +0900

    Write errorHandling code

--- CoordinateCalculator/CoordinateCalculator/main.swift
+++ CoordinateCalculator/CoordinateCalculator/main.swift
@@ -8,6 +8,23 @@
 
 import Foundation
 
-OutputView.deleteAxis()
-OutputView.drawAxis()
+func drawAcoord () {
+    var isGameOn: Bool = true
+    while (isGameOn == true) {
+        do {
+            let checkedValue = try Checking().returnCheckedValues(InputView().readInput())
+            if checkedValue.x == 0 || checkedValue.y == 0 {
+                isGameOn = false
+            }
+        } catch Checking.ErrorMessage.ofInValidInputedValue{
+            print (Checking.ErrorMessage.ofInValidInputedValue.rawValue)
+        } catch Checking.ErrorMessage.ofNonexistenceComma {
+            print (Checking.ErrorMessage.ofNonexistenceComma.rawValue)
+        } catch Checking.ErrorMessage.ofExceedValidInput {
+            print (Checking.ErrorMessage.ofExceedValidInput.rawValue)
+        } catch {
+            print (Checking.ErrorMessage.ofUnKnownError.rawValue)
+        }
+    }
 
+}
