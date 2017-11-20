1118031ca4c9ad94c1cc540ff056a3978e24e0fc
waitNseE <yellow.cjh@gmail.com>
2017-11-20 23:06:18 +0900

    Simplify catchedError

--- CoordinateCalculator/CoordinateCalculator/main.swift
+++ CoordinateCalculator/CoordinateCalculator/main.swift
@@ -8,23 +8,16 @@
 
 import Foundation
 
-func drawAcoord () {
-    var isGameOn: Bool = true
-    while (isGameOn == true) {
-        do {
-            let checkedValue = try Checking().returnCheckedValues(InputView().readInput())
-            if checkedValue.x == 0 || checkedValue.y == 0 {
-                isGameOn = false
-            }
-        } catch Checking.ErrorMessage.ofInValidInputedValue{
-            print (Checking.ErrorMessage.ofInValidInputedValue.rawValue)
-        } catch Checking.ErrorMessage.ofNonexistenceComma {
-            print (Checking.ErrorMessage.ofNonexistenceComma.rawValue)
-        } catch Checking.ErrorMessage.ofExceedValidInput {
-            print (Checking.ErrorMessage.ofExceedValidInput.rawValue)
-        } catch {
-            print (Checking.ErrorMessage.ofUnKnownError.rawValue)
+while (true) {
+    do {
+        let checkedValue = try Checking().returnCheckedValues(InputView().readInput())
+        if checkedValue.x == 0 && checkedValue.y == 0 {
+            break
         }
+        OutputView.drawPoint(checkedValue)
+    } catch let error as Checking.ErrorMessage {
+        print (error.rawValue)
+    } catch {
+        print (Checking.ErrorMessage.ofUnKnownError.rawValue)
     }
-
 }
