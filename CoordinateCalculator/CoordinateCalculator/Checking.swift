032119ef3c8c675c04c0f9821368469f8555eddd
waitNseE <yellow.cjh@gmail.com>
2017-11-20 21:08:40 +0900

    Add Checking and MyPoint objects and modify main

---
+++ CoordinateCalculator/CoordinateCalculator/Checking.swift
@@ -0,0 +1,9 @@
+//
+//  Checking.swift
+//  CoordinateCalculator
+//
+//  Created by Choi Jeong Hoon on 2017. 11. 20..
+//  Copyright © 2017년 Codesquad Inc. All rights reserved.
+//
+
+import Foundation
---
+++ CoordinateCalculator/CoordinateCalculator/MyPoint.swift
@@ -0,0 +1,9 @@
+//
+//  MyPoint.swift
+//  CoordinateCalculator
+//
+//  Created by Choi Jeong Hoon on 2017. 11. 20..
+//  Copyright © 2017년 Codesquad Inc. All rights reserved.
+//
+
+import Foundation
