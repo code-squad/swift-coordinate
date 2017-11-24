7b6c73bf5adeea858cc06ac7523d50ea1243dcd6
youth27 <mayihelpu27@gmail.com>
2017-11-25 00:42:01 +0900

    revise protocol MyShape, add print cases by shapes in outputView

--- CoordinateCalculator/CoordinateCalculator/OutputView.swift
+++ CoordinateCalculator/CoordinateCalculator/OutputView.swift
@@ -14,18 +14,21 @@ struct OutputView{
         print("\(ANSICode.clear)\(ANSICode.home)")
     }
 
-    //[MyPoint]를 파라미터로 받고 for문안에서 프린트
     func drawPoint (_ shape: MyShape) {
         switch shape {
-        case shape is MyPoint: break
+        case let shape as MyPoint:
+            print("\(ANSICode.text.redBright)\(ANSICode.cursor.move(row: 25 - shape.y, col: (shape.x * 2) + 3))\(ANSICode.text.dot)")
+        case let shape as MyLine:
+            print("\(ANSICode.text.redBright)\(ANSICode.cursor.move(row: 25 - shape.pointA.y, col: (shape.pointA.x * 2) + 3))\(ANSICode.text.dot)")
+            print("\(ANSICode.text.redBright)\(ANSICode.cursor.move(row: 25 - shape.pointB.y, col: (shape.pointB.x * 2) + 3))\(ANSICode.text.dot)")
         default:
-            <#code#>
+            print("")
         }
-        print("\(ANSICode.text.redBright)\(ANSICode.cursor.move(row: 25 - point.y, col: (point.x * 2) + 3))\(ANSICode.text.dot)")
+        print("두 점 사이의 거리는  \(shape.calculate())")
     }
     
     func drawAxis () {
         print("\(ANSICode.text.whiteBright)\(ANSICode.axis.draw())")
     }
-    
+
 }
