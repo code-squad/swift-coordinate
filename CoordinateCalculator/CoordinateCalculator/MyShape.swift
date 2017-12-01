cdf90dbe1e45b3f70728e5a906a8861fbd24ded9
waitNseE <yellow.cjh@gmail.com>
2017-12-01 17:34:57 +0900

    Modify and arrange files

---
+++ CoordinateCalculator/CoordinateCalculator/MyShape.swift
@@ -0,0 +1,13 @@
+//
+//  MyShape.swift
+//  CoordinateCalculator
+//
+//  Created by Choi Jeong Hoon on 2017. 12. 1..
+//  Copyright © 2017년 Codesquad Inc. All rights reserved.
+//
+
+import Foundation
+
+protocol MyShape {
+    var type: SyntaxChecker.Shape { get set }
+}
