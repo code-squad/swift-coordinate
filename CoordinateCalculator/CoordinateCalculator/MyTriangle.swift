0fad276d685d2ec0495555f1db004c0920f4101a
youth27 <mayihelpu27@gmail.com>
2017-12-01 11:52:51 +0900

    add computed property in MyTriangle

--- CoordinateCalculator/CoordinateCalculator/MyTriangle.swift
+++ CoordinateCalculator/CoordinateCalculator/MyTriangle.swift
@@ -13,41 +13,42 @@ struct MyTriangle {
     var lineBC = MyLine(pointA: MyPoint(x: 0, y: 0), pointB: MyPoint(x: 0, y: 0))
     var lineAC = MyLine(pointA: MyPoint(x: 0, y: 0), pointB: MyPoint(x: 0, y: 0))
     
+    var pointA : MyPoint {
+        get {
+            return lineAB.pointA
+        }
+    }
+    
+    var pointB : MyPoint {
+        get {
+            return lineAB.pointB
+        }
+    }
+    
+    var pointC : MyPoint {
+        get {
+            return lineBC.pointB
+        }
+    }
+    
     init(tripointA: MyPoint, tripointB: MyPoint, tripointC: MyPoint) { // trianglePointA: MyPoint
         self.lineAB = MyLine(pointA: tripointA, pointB: tripointB) // MyLine의 프로퍼티 이름: init의 파라미터이름! pointA : trianglePointA, pointB : trianglePointB
         self.lineBC = MyLine(pointA: tripointB, pointB: tripointC)
         self.lineAC = MyLine(pointA: tripointA, pointB: tripointC)
-    
     }
 
-    func getPointA () -> MyPoint {
-       let pointA = lineAB.pointA
-        return pointA
-    }
-    
-    func getPointB () -> MyPoint {
-        let pointB = lineAB.pointB
-        return pointB
-    }
-
-    func getPointC () -> MyPoint {
-        let pointC = lineBC.pointB
-        return pointC
-    }
-    
 }
-
 
 extension MyTriangle: MyShape {
     func getMyPoints() -> [MyPoint]  {
-        let myPoints = [MyPoint(x: getPointA().x, y: getPointA().y),
-                        MyPoint(x: getPointB().x, y: getPointB().y),
-                        MyPoint(x: getPointC().x, y: getPointC().y)]
+        let myPoints = [MyPoint(x: pointA.x, y: pointA.y),
+                        MyPoint(x: pointB.x, y: pointB.y),
+                        MyPoint(x: pointC.x, y: pointC.y)]
         return myPoints
     }
 
     func inValidShape() -> Bool {
-        if getPointA() == getPointB() || getPointB() == getPointC() || getPointA() == getPointC() {
+        if pointA == pointB || pointB == pointC || pointA == pointC {
             return true
         }
         if lineAB == lineBC || lineBC == lineAC || lineAB == lineAC{
@@ -58,9 +59,9 @@ extension MyTriangle: MyShape {
     }
     
     func printPoints() {
-        print("\(ANSICode.text.redBright)\(ANSICode.cursor.move(row: 25 - getPointA().y, col: (getPointA().x * 2) + 3))\(ANSICode.text.dot)") //tripointA
-        print("\(ANSICode.text.redBright)\(ANSICode.cursor.move(row: 25 - getPointB().y, col: (getPointB().x * 2) + 3))\(ANSICode.text.dot)") //tripointB
-        print("\(ANSICode.text.redBright)\(ANSICode.cursor.move(row: 25 - getPointC().y, col: (getPointC().x * 2) + 3))\(ANSICode.text.dot)") //tripointC
+        print("\(ANSICode.text.redBright)\(ANSICode.cursor.move(row: 25 - pointA.y, col: (pointA.x * 2) + 3))\(ANSICode.text.dot)") //tripointA
+        print("\(ANSICode.text.redBright)\(ANSICode.cursor.move(row: 25 - pointB.y, col: (pointB.x * 2) + 3))\(ANSICode.text.dot)") //tripointB
+        print("\(ANSICode.text.redBright)\(ANSICode.cursor.move(row: 25 - pointC.y, col: (pointC.x * 2) + 3))\(ANSICode.text.dot)") //tripointC
     }
 
     func calculate() -> Double {
