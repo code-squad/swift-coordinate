5285765ae98042011f4fb7eb5c2111688480ccb6
youth27 <mayihelpu27@gmail.com>
2017-12-03 19:25:08 +0900

    add MyRect, delete ShapeChecker,revise checking invalidShape logic

--- CoordinateCalculator/CoordinateCalculator/FactoryMyShape.swift
+++ CoordinateCalculator/CoordinateCalculator/FactoryMyShape.swift
@@ -10,19 +10,47 @@ import Foundation
 
 struct FactoryMyShape {
 
+    enum ShapeError: String, Error {
+        case wrongLine = "직선을 생성할 수 없는 좌표입니다."
+        case wrongTriangle = "삼각형을 생성할 수 없는 좌표입니다."
+        case wrongRect = "직사각형을 생성할 수 없는 좌표입니다."
+        case unexpected = "Unexpected Error"
+    }
+    
     // [MyPoint]를 그대로 생성자에 넘기는걸 생각해보기
-    func makeShape (_ myPoints: [MyPoint]) -> MyShape {
+    func makeShape (_ myPoints: [MyPoint]) throws -> MyShape {
         switch myPoints.count {
         case 1: return MyPoint(x: myPoints[0].x, y: myPoints[0].y)
+            
         case 2:
+            if MyLine.invalidShape(myPoints) {
+                throw ShapeError.wrongLine
+            }
             return MyLine(pointA: myPoints[0], pointB: myPoints[1])
-        case 3: return MyTriangle(tripointA: myPoints[0], tripointB: myPoints[1], tripointC: myPoints[2])
-        default: return MyPoint(x: myPoints[0].x, y: myPoints[0].y)}
+       
+        case 3:
+            if MyTriangle.invalidShape(myPoints) {
+               throw ShapeError.wrongTriangle
+            }
+            return MyTriangle(tripointA: myPoints[0], tripointB: myPoints[1], tripointC: myPoints[2])
+        
+        case 4:
+            if MyRect.invalidShape(myPoints) {
+                throw ShapeError.wrongRect
+            }
+            let rectWidth = abs(myPoints[2].x - myPoints[0].x)
+            let rectHeight = abs(myPoints[0].y - myPoints[2].y)
+            
+            return MyRect(origin: myPoints[0],
+                          size: CGSize(width: rectWidth, height: rectHeight))
+            
+        default:
+            return myPoints[0]}
     }
 }
 
 protocol MyShape {
-    func inValidShape() -> Bool
+    static func invalidShape(_ myPoints:[MyPoint]) -> Bool
     func getMyPoints() -> [(Int,Int)]
 }
 
