94688473c1bd608296952ffe61b9ff9b2368dfc0
Jack <somedd@naver.com>
2017-12-11 12:20:12 +0900

    Generate functions for drawing point
    1. 예외처리한 메인함수를 구혀했습니다.
    2. OutputView 구조체를 구현했습니다.

--- CoordinateCalculator/CoordinateCalculator/InputView.swift
+++ CoordinateCalculator/CoordinateCalculator/InputView.swift
@@ -9,29 +9,15 @@
 import Foundation
 
 struct InputView {
-    func readInput() {
-        print("실행 좌표를 입력하세요.")
-        print("*Ex) (10,10)")
-        print("* X,Y좌표의 최댓값은 24입니다.")
+    func readInput() -> String {
+        print("좌표를 입력하세요.")
         let userCoordinate = readLine()
         guard let input = userCoordinate else {
-            return
+            return ""
         }
-        print(seperateCoordinates(userInput: input))
+        return input
     }
-    
-    func seperateCoordinates (userInput : String) -> [Int] {
-        var coordinates : [Int] = [0,0]
-        let userInputWithoutLeftBracket = sliceMark(userInput, mark: "(")
-        let userInputWithoutRightBracket = sliceMark(userInputWithoutLeftBracket, mark: ")")
-        
-        var temp = userInputWithoutRightBracket.split(separator: ",").map(String.init)
-        for index in 0..<temp.count {
-            coordinates[index] = Int(temp[index]) ?? 0
-        }
-        return coordinates
-    }
-    
+    //입력 문자열에서 하나의 문자를 제거하고, 그 문자열을 반환하는 함수
     func sliceMark (_ fullString : String, mark : Character) -> String {
         var temp = ""
         let StringsWithoutMark : [String] = fullString.split(separator: mark).map(String.init)
@@ -40,4 +26,18 @@ struct InputView {
         }
         return temp
     }
+    //입력받은 문자열에서 좌표를 반환해주는 함수
+    func seperateCoordinates (userInput : String) -> MyPoint {
+        var coordinates : [Int] = [0,0]
+        let userInputWithoutLeftBracket = sliceMark(userInput, mark: "(")
+        let userInputWithoutRightBracket = sliceMark(userInputWithoutLeftBracket, mark: ")")
+        
+        var temp = userInputWithoutRightBracket.split(separator: ",").map(String.init)
+        for index in 0..<temp.count {
+            coordinates[index] = Int(temp[index]) ?? 0
+        }
+        let userPoints : MyPoint = MyPoint.init(x: coordinates[0], y: coordinates[1])
+        return userPoints
+    }
+
 }
--- CoordinateCalculator/CoordinateCalculator/main.swift
+++ CoordinateCalculator/CoordinateCalculator/main.swift
@@ -8,7 +8,13 @@
 
 import Foundation
 
-OutputView().clearConsole()
-OutputView().drawAxis()
-
-InputView().readInput()
+var runCoordinatesCalculator : Bool = true
+mainLoop : while runCoordinatesCalculator == true {
+    let userInput = InputView().readInput()
+    let userPoints = InputView().seperateCoordinates(userInput: userInput)
+    guard userPoints.x <= 24 || userPoints.y <= 24 else {
+        print("X 또는 Y좌표가 24보다 값이 큽니다. 다시 입력해주세요.")
+        continue
+    }
+    OutputView().drawPoint(inputPoints: userPoints)
+}
