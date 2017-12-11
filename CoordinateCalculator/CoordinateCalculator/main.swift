bc630b984055b24700fbedb541110589c11b15d8
Jack <somedd@naver.com>
2017-12-11 14:52:02 +0900

    Divide functions for each structures
    1. 기존 seperateCoordinates함수를 좌표값만 넘겨주는 함수로
    수정하였습니다.
    2. 메인함수 내부 객체활용을 하나로 줄였습니다.

--- CoordinateCalculator/CoordinateCalculator/main.swift
+++ CoordinateCalculator/CoordinateCalculator/main.swift
@@ -10,11 +10,15 @@ import Foundation
 
 var runCoordinatesCalculator : Bool = true
 mainLoop : while runCoordinatesCalculator == true {
-    let userInput = InputView().readInput()
-    let userPoints = InputView().seperateCoordinates(userInput: userInput)
-    guard userPoints.x <= 24 || userPoints.y <= 24 else {
+    let inputView : InputView = InputView()
+    let userInput = inputView.readInput()
+    let userPoints = inputView.seperateCoordinates(userInput: userInput)
+    
+    let myPoint : MyPoint = MyPoint.init(userPoints)
+    guard myPoint.x <= 24 || myPoint.y <= 24 else {
         print("X 또는 Y좌표가 24보다 값이 큽니다. 다시 입력해주세요.")
         continue
     }
-    OutputView().drawPoint(inputPoints: userPoints)
+    
+    OutputView().drawPoint(inputPoints: myPoint)
 }
