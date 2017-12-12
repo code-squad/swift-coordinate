274e8ed35cd211088eb984f020c30dd32f3ac7fe
Jack <somedd@naver.com>
2017-12-12 14:53:35 +0900

    Add some functions for printing a line

---
+++ CoordinateCalculator/CoordinateCalculator/MyLine.swift
@@ -0,0 +1,28 @@
+//
+//  MyLine.swift
+//  CoordinateCalculator
+//
+//  Created by jack on 2017. 12. 12..
+//  Copyright © 2017년 Codesquad Inc. All rights reserved.
+//
+
+import Foundation
+
+struct MyLine {
+    var pointA = MyPoint(x : 0, y : 0)
+    var pointB = MyPoint(x : 0, y : 0)
+    
+    init(pointA : MyPoint, pointB : MyPoint) {
+        self.pointA = pointA
+        self.pointB = pointB
+    }
+    
+    init(points : [[Int]]) {
+        self.pointA.x = points[0][0]
+        self.pointA.y = points[0][1]
+        
+        self.pointB.x = points[1][0]
+        self.pointB.y = points[1][1]
+    }
+    // 두 점 사이의 거리를 계산하는 함수를 구현
+}
--- CoordinateCalculator/CoordinateCalculator/OutputView.swift
+++ CoordinateCalculator/CoordinateCalculator/OutputView.swift
@@ -18,6 +18,27 @@ struct OutputView {
         drawAxis()
     }
     
+    func drawLine(inputLine : MyLine) {
+        clearConsole()
+        let firstXpoint = inputLine.pointA.x * 2 + 3
+        let firstYpoint = (inputLine.pointA.y - 25) * -1
+        let secondXpoint = inputLine.pointB.x * 2 + 3
+        let secondYpoint = (inputLine.pointB.y - 25) * -1
+        print("\(ANSICode.cursor.move(row : firstYpoint, col : firstXpoint ))\(ANSICode.text.redBright)●")
+        print("\(ANSICode.cursor.move(row : secondYpoint, col : secondXpoint ))\(ANSICode.text.redBright)●")
+        drawAxis()
+        printDistanceOfLine(firstPoint: inputLine.pointA, secondPoint: inputLine.pointB)
+
+    }
+    //MyLine구조체 인스턴스를 넘겨받아 두 점을 출력하는 함수를 추가
+    // 위 함수 안에는 두 점사이의 거리를 27번째 줄에 출력하는 부분도 추가한다.
+    private func printDistanceOfLine(firstPoint : MyPoint, secondPoint : MyPoint) {
+        var distance : Double = 0
+        let temp = (firstPoint.x - secondPoint.x)^2 + (firstPoint.y - secondPoint.y)^2
+        distance = sqrt(Double(temp))
+        print("두 점 사이의 거리는 \(distance)")
+    }
+    
     private func drawAxis() {
         print("\(ANSICode.text.whiteBright)\(ANSICode.axis.draw())")
     }
