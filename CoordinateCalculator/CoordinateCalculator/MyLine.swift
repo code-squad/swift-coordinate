cd25d8221afb49b850880194b5142272e1b897dd
Jack <somedd@naver.com>
2017-12-18 11:16:42 +0900

    Delete a variables called currentShape in structures of MyShape
    1. 프로토콜 내부의 currentShape변수를 삭제했습니다.

--- CoordinateCalculator/CoordinateCalculator/MyLine.swift
+++ CoordinateCalculator/CoordinateCalculator/MyLine.swift
@@ -11,7 +11,6 @@ import Foundation
 struct MyLine : MyShape, canCalculate{
     var pointA = MyPoint(x : 0, y : 0)
     var pointB = MyPoint(x : 0, y : 0)
-    var currentShape: String = "line"
     
     
     init(pointA : MyPoint, pointB : MyPoint) {
