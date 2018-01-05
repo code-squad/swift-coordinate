4fe20f350013b818b0ed66176925382c5ab4e9d3
cocomilktea <coco_milktea@naver.com>
2018-01-05 14:57:02 +0900

    CoordinateCalculator step1, add outputview

---
+++ CoordinateCalculator/CoordinateCalculator/OutputView.swift
@@ -0,0 +1,18 @@
+//
+//  OutputView.swift
+//  CoordinateCalculator
+//
+//  Created by 김수현 on 2018. 1. 5..
+//  Copyright © 2018년 Codesquad Inc. All rights reserved.
+//
+
+import Foundation
+
+public struct OutputView {
+    func drawAxis() {
+        print("\(ANSICode.text.whiteBright)\(ANSICode.axis.draw())")
+    }
+    func clearAxis() {
+        print("\(ANSICode.clear)\(ANSICode.home)")
+    }
+}
