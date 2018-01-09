7760b0c9849816fe32f2532508d34d762ac760eb
cocomilktea <coco_milktea@naver.com>
2018-01-09 16:58:53 +0900

    Edit InputView, separate func, edit optional

--- CoordinateCalculator/CoordinateCalculator/InputView.swift
+++ CoordinateCalculator/CoordinateCalculator/InputView.swift
@@ -10,18 +10,20 @@ import Foundation
 
 struct InputView {
     
-    func readInput() -> Array<String>? {
-        print("좌표를 입력하세요 ex)(10,10) " )
+    func readInput() -> String {
         let input = readLine()
-        guard let inputCoodinates = input else { return nil }
-        let coodinates = inputCoodinates.components(separatedBy: ",")
+        guard let inputCoodinates = input else { return "" }
+        return inputCoodinates
+    }
+    
+    func separateInput(_ input: String) -> Array<String> {
+        let coodinates = input.components(separatedBy: ",")
         return coodinates
     }
     
-    func separateCoodinates(_ input: [String]) -> MyPoint {
-        let first = Int(input[0].dropFirst())
-        let last = Int(input[1].dropLast())
-        return MyPoint.init(x: first!, y: last!)
+    func createPoint(_ input: [String]) -> MyPoint? {
+        guard let first = Int(input[0].dropFirst()), let last = Int(input[1].dropLast()) else { return nil }
+        return MyPoint.init(x: first, y: last)
     }
     
 }
--- CoordinateCalculator/CoordinateCalculator/main.swift
+++ CoordinateCalculator/CoordinateCalculator/main.swift
@@ -9,9 +9,13 @@
 import Foundation
 
 while true {
+    print("좌표를 입력하세요 ex)(10,10) " )
     let input = InputView().readInput()
-    let coodinates = InputView().separateCoodinates(input!)
-    guard coodinates.x < 25 && coodinates.y < 25 else { print("좌표 최댓값은 24입니다. 다시 입력해주세요"); continue }
+    let inputCoodinates = InputView().separateInput(input)
+    guard let coodinates = InputView().createPoint(inputCoodinates) else { continue }
+    guard coodinates.x < 25 && coodinates.y < 25 else {
+        print("좌표 최댓값은 24입니다. 다시 입력해주세요")
+        continue }
     OutputView().drawCoordinates(coodinates)
     break
 }
