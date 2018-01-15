bf0e2b39f247dd83f8bc0426ba8b5fb5db212107
cocomilktea <coco_milktea@naver.com>
2018-01-15 17:13:39 +0900

    MyLine 구조체 추가

--- CoordinateCalculator/CoordinateCalculator/InputView.swift
+++ CoordinateCalculator/CoordinateCalculator/InputView.swift
@@ -9,7 +9,7 @@
 import Foundation
 
 struct InputView {
-    
+
     let validCharacter = CharacterSet.init(charactersIn: "(),-").union(CharacterSet.decimalDigits)
 
     func readInput(_ message: String) -> String {
@@ -21,18 +21,54 @@ struct InputView {
 
     func checkInput(_ input: String) -> Bool {
         let check = input.components(separatedBy: validCharacter).filter{ $0 != "" }
-        if check.count == 0 { return true } 
+        if check.count == 0 { return true }
         return false
     }
-    
-    func separateInput(_ input: String) -> Array<String> {
+
+    func checkNumber(_ point: MyPoint) -> Bool {
+        return point.x <= 24 && point.y <= 24
+    }
+
+    func checkMaxValue(_ value: [MyPoint]) -> Bool {
+        var maxValue: Bool = true
+        for index in 0..<value.count {
+            guard checkNumber(value[index]) != false else{ maxValue = false; return false }
+        }
+        guard maxValue == true else { return false }
+        return true
+    }
+
+    //(10,10)-(20,20) "-" 기준으로 분리
+    func separateInput(_ input: String) -> [MyPoint]? {
+        var coordinates: [MyPoint] = []
+        let temp = input.components(separatedBy: "-")
+        for index in 0..<temp.count {
+            coordinates.append(createPoint(separateComma(temp[index]))!)
+        }
+        return coordinates
+    }
+
+    func selectPoint(_ point: [MyPoint]) -> MyLine {
+        switch point.count {
+        case 2:
+            let line: MyLine = MyLine.init(point)
+            return line
+        default:
+            let point: MyLine = MyLine.init(point)
+            return point
+        }
+    }
+
+    //(10,10) "," 기준으로 분리
+    func separateComma(_ input: String) -> Array<String> {
         let coodinates = input.components(separatedBy: ",")
         return coodinates
     }
-    
+
     func createPoint(_ input: [String]) -> MyPoint? {
         guard let first = Int(input[0].dropFirst()), let last = Int(input[1].dropLast()) else { return nil }
         return MyPoint.init(x: first, y: last)
     }
-    
+
 }
+
---
+++ CoordinateCalculator/CoordinateCalculator/MyLine.swift
@@ -0,0 +1,26 @@
+//
+//  File.swift
+//  CoordinateCalculator
+//
+//  Created by 김수현 on 2018. 1. 15..
+//  Copyright © 2018년 Codesquad Inc. All rights reserved.
+//
+
+import Foundation
+
+struct MyLine {
+    var pointA = MyPoint(x: 0, y: 0)
+    var pointB = MyPoint(x: 0, y: 0)
+
+    init(_ coordinates : [MyPoint]) {
+        self.pointA = coordinates[0]
+        self.pointB = coordinates[1]
+    }
+    
+    func calculateDistance() -> Double {
+        let x = Double(self.pointA.x - self.pointB.x)
+        let y = Double(self.pointA.y - self.pointB.y)
+        let distance = sqrt(Double(pow(x, 2) + Double(pow(y, 2))))
+        return distance
+    }
+}
