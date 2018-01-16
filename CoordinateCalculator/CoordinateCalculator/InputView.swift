1fc1e8b90e44d7cd72e5aba263b1972ef1658422
cocomilktea <coco_milktea@naver.com>
2018-01-16 11:53:34 +0900

    Edit function name, Edit parameter

--- CoordinateCalculator/CoordinateCalculator/InputView.swift
+++ CoordinateCalculator/CoordinateCalculator/InputView.swift
@@ -18,7 +18,7 @@ struct InputView {
     }
     
     //(10,10)-(20,20) "-" 기준으로 분리
-    func separateInput(_ input: String) -> [MyPoint]? {
+    func makePoints(_ input: String) -> [MyPoint]? {
         var coordinates: [MyPoint] = []
         let temp = input.components(separatedBy: "-")
         for index in 0..<temp.count {
@@ -28,12 +28,12 @@ struct InputView {
     }
     
     //(10,10) "," 기준으로 분리
-    func separateComma(_ input: String) -> Array<String> {
+    private func separateComma(_ input: String) -> Array<String> {
         let coodinates = input.components(separatedBy: ",")
         return coodinates
     }
     
-    func createPoint(_ input: [String]) -> MyPoint? {
+    private func createPoint(_ input: [String]) -> MyPoint? {
         guard let first = Int(input[0].dropFirst()), let last = Int(input[1].dropLast()) else { return nil }
         return MyPoint.init(x: first, y: last)
     }
