ceb0970179677535cd018fd1972ac03b941df329
cocomilktea <coco_milktea@naver.com>
2018-03-06 14:25:48 +0900

    unit test equal func 추가

--- CoordinateCalculator/CoordinateCalculator/MyTriangle.swift
+++ CoordinateCalculator/CoordinateCalculator/MyTriangle.swift
@@ -8,11 +8,15 @@
 
 import Foundation
 
-struct MyTriangle: MyShape, MyCalculate {
+struct MyTriangle: MyShape, MyCalculate, Equatable {
     
-    private var lineAB = MyLine(pointA: MyPoint(x: 0, y: 0), pointB: MyPoint(x: 0, y: 0))
-    private var lineBC = MyLine(pointA: MyPoint(x: 0, y: 0), pointB: MyPoint(x: 0, y: 0))
-    private var lineAC = MyLine(pointA: MyPoint(x: 0, y: 0), pointB: MyPoint(x: 0, y: 0))
+    static func ==(lhs: MyTriangle, rhs: MyTriangle) -> Bool {
+        return lhs.lineAB.pointA.x == rhs.lineAB.pointA.x && lhs.lineAB.pointA.y == rhs.lineAB.pointA.y && lhs.lineAB.pointB.x == rhs.lineAB.pointB.x && lhs.lineAB.pointB.y == rhs.lineAB.pointB.y && lhs.lineBC.pointA.x == rhs.lineBC.pointA.x && lhs.lineBC.pointA.y == rhs.lineBC.pointA.y && lhs.lineBC.pointB.x == rhs.lineBC.pointB.x && lhs.lineBC.pointB.y == rhs.lineBC.pointB.y && lhs.lineAC.pointA.x == rhs.lineAC.pointA.x && lhs.lineAC.pointA.y == rhs.lineAC.pointA.y && lhs.lineAC.pointB.x == rhs.lineAC.pointB.x && lhs.lineAC.pointB.y == rhs.lineAC.pointB.y
+    }
+    
+    var lineAB = MyLine(pointA: MyPoint(x: 0, y: 0), pointB: MyPoint(x: 0, y: 0))
+    var lineBC = MyLine(pointA: MyPoint(x: 0, y: 0), pointB: MyPoint(x: 0, y: 0))
+    var lineAC = MyLine(pointA: MyPoint(x: 0, y: 0), pointB: MyPoint(x: 0, y: 0))
     
     init(pointA: MyPoint, pointB: MyPoint, pointC: MyPoint) {
         self.lineAB = MyLine(pointA: pointA, pointB: pointB)
@@ -41,4 +45,3 @@ struct MyTriangle: MyShape, MyCalculate {
     }
     
 }
-
