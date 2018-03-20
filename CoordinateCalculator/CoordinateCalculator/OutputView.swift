d8099d02abc7321ba91fdecd8319dc2d33c3f8ed
HaeSeongPark <cord7894@gmail.com>
2018-03-20 11:26:39 +0900

    struct OutputView 추가, clean()와  drawAxis()함수 추가

---
+++ CoordinateCalculator/CoordinateCalculator/OutputView.swift
@@ -0,0 +1,19 @@
+//
+//  OutputView.swift
+//  CoordinateCalculator
+//
+//  Created by rhino Q on 2018. 3. 20..
+//  Copyright © 2018년 Codesquad Inc. All rights reserved.
+//
+
+import Foundation
+
+struct OutputView {
+    
+    static func clean(){
+        print("\(ANSICode.clear)\(ANSICode.home)")
+    }
+    static func drawAxis(){
+        print("\(ANSICode.text.whiteBright)\(ANSICode.axis.draw())")
+    }
+}
