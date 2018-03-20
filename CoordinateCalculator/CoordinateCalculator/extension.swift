8847d24e343cb7da2513e146c6f2ac51b78e6cb8
HaeSeongPark <cord7894@gmail.com>
2018-03-20 16:46:43 +0900

    extension Int : XofCoordinates, YofCoordinates 추가

---
+++ CoordinateCalculator/CoordinateCalculator/extension.swift
@@ -0,0 +1,14 @@
+//
+//  extension.swift
+//  CoordinateCalculator
+//
+//  Created by rhino Q on 2018. 3. 20..
+//  Copyright © 2018년 Codesquad Inc. All rights reserved.
+//
+
+import Foundation
+
+extension Int{
+    var XofCoordinates:Int { return self * 2 + 3 }
+    var YofCoordinates:Int { return 25 - self }
+}
