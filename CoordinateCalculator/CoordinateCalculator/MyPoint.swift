c0ca0edd97c76e832a366df4f5b4b71344c81ac2
HaeSeongPark <cord7894@gmail.com>
2018-03-21 16:35:50 +0900

    yOfCoordinates, xOfCorrdinates Extension에서 MyPoint로 이동

--- CoordinateCalculator/CoordinateCalculator/MyPoint.swift
+++ CoordinateCalculator/CoordinateCalculator/MyPoint.swift
@@ -11,4 +11,7 @@ import Foundation
 struct MyPoint {
     var x = 0
     var y = 0
+    
+    var xOfCorrdinates:Int { return self.x * 2 + 3}
+    var yOfCoordinates:Int { return ANSICode.axis.AxisLimit + 1 - self.y }
 }
