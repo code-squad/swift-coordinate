48c20f11459f45901e1ca2f9b49a403174e9e15b
Moon <myssun0325@gmail.com>
2018-04-01 16:51:44 +0900

    Add 'InputView' struct
    처음 사용자의 입력을 입력받는 함수와 에러 enum 추가

---
+++ CoordinateCalculator/CoordinateCalculator/InputView.swift
@@ -0,0 +1,36 @@
+//
+//  InputView.swift
+//  CoordinateCalculator
+//
+//  Created by moon on 2018. 3. 30..
+//  Copyright © 2018년 Codesquad Inc. All rights reserved.
+//
+
+import Foundation
+
+
+enum InputViewError: Error {
+    case invalidInput
+    
+    var localizedDescription: String {
+        switch self {
+        case .invalidInput:
+            return "입력을 확인해주세요."
+        }
+    }
+}
+
+struct InputView {
+    
+    func readInput() throws -> String {
+        print("좌표를 입력하세요.")
+        guard let input = readLine() else {
+            throw InputViewError.invalidInput
+        }
+        
+        return input
+    }
+    
+    
+    
+}
