de40dafc232004c9c85b7004fe9378fb79b02776
Moon <myssun0325@gmail.com>
2018-04-01 17:20:37 +0900

    Add 'StringSpliter' struct
    StringSpliter: 문자열을 쪼개줌

---
+++ CoordinateCalculator/CoordinateTests/CoordinateTests.swift
@@ -0,0 +1,25 @@
+//
+//  CoordinateTests.swift
+//  CoordinateTests
+//
+//  Created by moon on 2018. 4. 1..
+//  Copyright © 2018년 Codesquad Inc. All rights reserved.
+//
+
+import XCTest
+
+class CoordinateTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    // test 
+    
+}
