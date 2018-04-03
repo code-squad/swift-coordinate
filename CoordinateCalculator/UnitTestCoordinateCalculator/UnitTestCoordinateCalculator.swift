5f8253fd7cac066524a554c5f97822d6856c6438
linsaeng <duwjdtmd91@gamil.com>
2018-04-03 23:20:20 +0900

    mian 함수 생성

---
+++ CoordinateCalculator/UnitTestCoordinateCalculator/UnitTestCoordinateCalculator.swift
@@ -0,0 +1,35 @@
+//
+//  UnitTestCoordinateCalculator.swift
+//  UnitTestCoordinateCalculator
+//
+//  Created by Jung seoung Yeo on 2018. 4. 3..
+//  Copyright © 2018년 Codesquad Inc. All rights reserved.
+//
+
+import XCTest
+
+class UnitTestCoordinateCalculator: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
