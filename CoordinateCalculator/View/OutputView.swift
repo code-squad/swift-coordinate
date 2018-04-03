6d72580b73782dc932bb33fa94e2aaa3acc90e72
linsaeng <duwjdtmd91@gamil.com>
2018-04-03 23:21:41 +0900

    Output생성(drawAxis()함수 추가)

--- CoordinateCalculator/View/OutputView.swift
+++ CoordinateCalculator/View/OutputView.swift
@@ -5,5 +5,9 @@
 //  Created by Jung seoung Yeo on 2018. 4. 3..
 //  Copyright © 2018년 Codesquad Inc. All rights reserved.
 //
-
-import Foundation
+struct OutputView {
+    static func drawAxis() {
+        print("\(ANSICode.clear)\(ANSICode.home)")
+        print("\(ANSICode.text.cyanBright)\(ANSICode.axis.draw())")
+    }
+}
