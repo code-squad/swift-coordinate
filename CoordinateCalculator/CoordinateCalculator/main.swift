72dd11935c73668d593bfb5d6f1e903576cb637e
Moon <myssun0325@gmail.com>
2018-04-08 01:47:43 +0900

    Modify InputView

--- CoordinateCalculator/CoordinateCalculator/main.swift
+++ CoordinateCalculator/CoordinateCalculator/main.swift
@@ -11,32 +11,6 @@ import Foundation
 
 func main() {
     
-    let inputChecker = InputChecker()
-    let outputView = OutputView()
-    let firgureMaker = FigureMaker()
-    
-    var errorFlag = true
-    
-    while errorFlag {
-        do {
-            let text: String = InputView.readInput(question: Question.coordinate.rawValue)
-            let coordinateText: String = try inputChecker.checkMatching(text: text, with: InputChecker.validPattern)
-            let coordinates: [Int] = firgureMaker.getCoordinateFrom(text: coordinateText)
-            let point: MyPoint = firgureMaker.makeMyPointFrom(coordinates: coordinates)
-            
-            print("\(ANSICode.clear)\(ANSICode.home)")
-            outputView.drawPoint(point)
-            outputView.drawAxis()
-            
-            errorFlag = false
-            
-        } catch let error as InputError {
-            print(error.localizedDescription)
-            continue
-        } catch {
-            fatalError("Unexpected Error")
-        }
-    }
 }
 
 main()
