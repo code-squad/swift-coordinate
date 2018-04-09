a8264dd799c98ac9e879bbf5f8ecaaa0cb20fcfc
Moon <myssun0325@gmail.com>
2018-04-10 07:44:42 +0900

    Add Figurable protocol

--- CoordinateCalculator/CoordinateCalculator/FigureFactory.swift
+++ CoordinateCalculator/CoordinateCalculator/FigureFactory.swift
@@ -8,11 +8,26 @@
 
 import Foundation
 
-struct FigureFactory {
+struct FigureFactory: Figurable {
     
     private var myPoints: [MyPoint]
     
     init(_ myPoints: [MyPoint]) {
         self.myPoints = myPoints
     }
+    
+    func makeFigure() {
+        switch self.myPoints.count {
+        case 1:
+            //makeMyPoint()
+        case 2:
+            //makeMyLine()
+        default:
+            makeMyPoint()
+        }
+    }
+}
+
+protocol Figurable {
+    func calculate() -> Double
 }
