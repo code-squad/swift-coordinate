a93ee27c98ae35187bb37f65aabcb99db1050403
Moon <myssun0325@gmail.com>
2018-04-10 11:45:30 +0900

    Add Figure Information struct

---
+++ CoordinateCalculator/CoordinateCalculator/FigureInformation.swift
@@ -0,0 +1,15 @@
+//
+//  FigureInformation.swift
+//  CoordinateCalculator
+//
+//  Created by moon on 2018. 4. 10..
+//  Copyright © 2018년 Codesquad Inc. All rights reserved.
+//
+
+import Foundation
+
+struct FigureInformation {
+    var figure: FigureCalculatable
+    var distance: Double = 0.0
+    var point: [MyPoint]
+}
