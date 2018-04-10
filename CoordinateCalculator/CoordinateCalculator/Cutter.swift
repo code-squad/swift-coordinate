762b422c74f7bf0e64c4e2f4fb0e6da2fa2abb0a
DrakeYang <drake.the.programmer@gmail.com>
2018-04-10 18:00:06 +0900

    테스트함수 k 삭제

--- CoordinateCalculator/CoordinateCalculator/Cutter.swift
+++ CoordinateCalculator/CoordinateCalculator/Cutter.swift
@@ -7,22 +7,7 @@
 //
 
 import Foundation
-func k(){
-    let testStr = ANSICode.axis.draw()
-    let axisDrawPattern = "([0-9]{1,2});([0-9]{1,2})"
-    
-    if let regex = try? NSRegularExpression(pattern: axisDrawPattern, options: []) {
-        let testNSStr = testStr as NSString
-        let matches2 = regex.matches(in : testStr, options: [], range: NSRange(location : 0 , length : testNSStr.length))
-        let result2 = matches2.map {
-            testNSStr.substring(with: $0.range)
-        }
-        print(result2)
-    }
-    else {
-        print("정규식 작성 실패!")
-    }
-}
+ß
 /// 커터 구조체
 struct Cutter {
     
