751b33b17a09a1f0bcf73dbb35991b9478367a68
DrakeYang <drake.the.programmer@gmail.com>
2018-04-10 15:20:26 +0900

    정규식 자르는 테스트 성공. 복붙한 코드 분석중

--- CoordinateCalculator/CoordinateCalculator/main.swift
+++ CoordinateCalculator/CoordinateCalculator/main.swift
@@ -8,5 +8,17 @@
 
 import Foundation
 
-print("Hello, World!")
+func main(){
+    ///OutputView 구조체 선언
+    let testStr = ANSICode.axis.draw()
+    let axisDrawPattern = "([0-9]{1,2});([0-9]{1,2})"
+    let regex = try! NSRegularExpression(pattern: axisDrawPattern, options: [])
+    let matches = regex.matches(in : testStr, options: [], range: NSRange(testStr.startIndex..., in: testStr))
+//    print(matches.count)
+    let result = matches.map {
+        String(testStr[Range($0.range, in: testStr)!])
+    }
+    print(result)
+}
 
+main()
