3da0b1265f91a3518a8400a98df2798c3ece1005
DrakeYang <drake.the.programmer@gmail.com>
2018-04-10 16:07:40 +0900

    정규식 두번째 스타일 테스트 성공. 둘 중 뭘 고를지 고민중.

--- CoordinateCalculator/CoordinateCalculator/main.swift
+++ CoordinateCalculator/CoordinateCalculator/main.swift
@@ -12,13 +12,24 @@ func main(){
     ///OutputView 구조체 선언
     let testStr = ANSICode.axis.draw()
     let axisDrawPattern = "([0-9]{1,2});([0-9]{1,2})"
-    let regex = try! NSRegularExpression(pattern: axisDrawPattern, options: [])
-    let matches = regex.matches(in : testStr, options: [], range: NSRange(testStr.startIndex..., in: testStr))
-//    print(matches.count)
-    let result = matches.map {
-        String(testStr[Range($0.range, in: testStr)!])
+    do {
+        let regex = try NSRegularExpression(pattern: axisDrawPattern, options: [])
+        let matches = regex.matches(in : testStr, options: [], range: NSRange(testStr.startIndex..., in: testStr))
+        let result = matches.map {
+            String(testStr[Range($0.range, in: testStr)!])
+        }
+        let testNSStr = testStr as NSString
+        let matches2 = regex.matches(in : testStr, options: [], range: NSRange(location : 0 , length : testNSStr.length))
+        let result2 = matches2.map {
+            testNSStr.substring(with: $0.range)
+        }
+        print(result2)
+    } catch {
+        print("정규식 작성 실패!")
     }
-    print(result)
+    
+    
+    
 }
 
 main()
