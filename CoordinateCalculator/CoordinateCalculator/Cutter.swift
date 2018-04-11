1c56c1fd7edf65f265f95db2912735ee331a5c5c
DrakeYang <drake.the.programmer@gmail.com>
2018-04-11 13:58:42 +0900

    테스트를 위해 제거헀던 private 다시 선언

--- CoordinateCalculator/CoordinateCalculator/Cutter.swift
+++ CoordinateCalculator/CoordinateCalculator/Cutter.swift
@@ -12,7 +12,7 @@ import Foundation
 struct Cutter {
     
     /// 입력한 문자열로 정규식을 만들어주는 함수
-     func makeRegexForm(regexTry : String)->NSRegularExpression?{
+    private func makeRegexForm(regexTry : String)->NSRegularExpression?{
         guard let regex = try? NSRegularExpression(pattern: regexTry, options: []) else {
             print("잘못된 정규식 표현입니다")
             return nil
@@ -21,7 +21,7 @@ struct Cutter {
     }
     
     /// 문자열과 정규식을 받아서 정규식에 맞는 문자열 배열로 리턴
-     func cuttingLattersFrom(originLatters : String, regex : NSRegularExpression) -> Array<String>{
+    private func cuttingLattersFrom(originLatters : String, regex : NSRegularExpression) -> Array<String>{
         let originForRange = originLatters as NSString
         return regex.matches(in : originLatters, options: [], range: NSRange(location : 0 , length : originForRange.length)).map{
             originForRange.substring(with: $0.range)
