b5dc8e5f0b15231b5871f82fcb1380ba316011d0
Jung seoung Yeo <duwjdtmd91@gmail.com>
2018-04-11 18:17:18 +0900

    정규식 변경, 입력가능한 CharacterSet 체크 추가

--- CoordinateCalculator/Inspetion/InputViewChecker.swift
+++ CoordinateCalculator/Inspetion/InputViewChecker.swift
@@ -9,12 +9,25 @@
 import Foundation
 struct InputViewChecker {
     static func formatChekcer(checkerFormat: String) throws {
-        let COORDINATE_VALID_PATTERN = "\\(([0-9]|1[0-9]|2[0-4]),([0-9]|1[0-9]|2[0-4])\\)"
+        try validInputChecker(checkerFormat)
+        // ) 로 끝나고 - 은 옵셔널 -> (0,0) or (0,0)-(0,0)
+        let COORDINATE_VALID_PATTERN = "\\(([0-9]|1[0-9]|2[0-4]),([0-9]|1[0-9]|2[0-4])\\)$-?"
+        
         guard let regex = try? NSRegularExpression(pattern: COORDINATE_VALID_PATTERN) else {
             throw CoordinateError.regexError
         }
         guard regex.matches(in: checkerFormat, options: [], range: NSMakeRange(0, checkerFormat.count)).count > 0 else {
             throw CoordinateError.invalidInputError
+        }
+    }
+    
+    // 입력 가능한 문자 체크
+    static func validInputChecker(_ checkerFormat: String) throws {
+        let COORDINATE_VALID_STRING = "0123456789()-,"
+        let validInput = CharacterSet.init(charactersIn: COORDINATE_VALID_STRING)
+        
+        guard checkerFormat.components(separatedBy: validInput).joined().count <= 0 else {
+            throw CoordinateError.imPossibleStringError
         }
     }
 }
