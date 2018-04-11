a5ab96730f6f139b614a043c0619af304c2fb947
Jung seoung Yeo <duwjdtmd91@gmail.com>
2018-04-11 17:56:06 +0900

    point 프로토콜, line 프로토콜 생성

--- CoordinateCalculator/Common/Protocol/CoordinateProtocol.swift
+++ CoordinateCalculator/Common/Protocol/CoordinateProtocol.swift
@@ -5,10 +5,12 @@
 //  Created by Jung seoung Yeo on 2018. 4. 10..
 //  Copyright © 2018년 Codesquad Inc. All rights reserved.
 //
+
 protocol PointProtocol {
-    func getPoint() -> [MyPoint]
+    func getPoints() -> [MyPoint]
 }
 
-protocol LineProtocol: PointProtocol {
+// SOLID 원칙 중 인터페이스 분리 작업으로 MyPoint는 직선의거리를 구현 할 필요가 없어서 분리
+protocol LineProtocol: PointProtocol{
     func getDistance() -> Double
 }
