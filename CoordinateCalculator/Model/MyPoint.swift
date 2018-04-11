b068ed037cfb010cd53928c24ae196adef2c3b94
Jung seoung Yeo <duwjdtmd91@gmail.com>
2018-04-11 17:58:22 +0900

    기본 초기화, 사용자 정의 초기화 생성

--- CoordinateCalculator/Model/MyPoint.swift
+++ CoordinateCalculator/Model/MyPoint.swift
@@ -5,12 +5,22 @@
 //  Created by Jung seoung Yeo on 2018. 4. 8..
 //  Copyright © 2018년 Codesquad Inc. All rights reserved.
 //
+
+import Foundation
+
+// MyPoint 저장 변수 Private(set) 으로 setter만 private로 선언
 struct MyPoint: Equatable {
-    var x = 0
-    var y = 0
+    private(set) var x: Int = 0
+    private(set) var y: Int = 0
     
     init(x: Int, y: Int) {
         self.x = x
         self.y = y
     }
+    
+    init(_ mypoint: MyPoint) {
+        self = mypoint
+    }
+    
 }
+
