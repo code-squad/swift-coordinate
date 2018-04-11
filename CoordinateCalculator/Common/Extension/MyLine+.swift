0df82579f27da3d8afb069dfbf5378351127aefa
Jung seoung Yeo <duwjdtmd91@gmail.com>
2018-04-11 17:55:35 +0900

    String, MyPoint, MyLine extension

--- CoordinateCalculator/Common/Extension/MyLine+.swift
+++ CoordinateCalculator/Common/Extension/MyLine+.swift
@@ -5,5 +5,18 @@
 //  Created by Jung seoung Yeo on 2018. 4. 10..
 //  Copyright © 2018년 Codesquad Inc. All rights reserved.
 //
-
 import Foundation
+
+extension MyLine: LineProtocol {
+
+    func getPoints() -> [MyPoint] {
+        return [self.pointA, self.pointB]
+    }
+    
+    func getDistance() -> Double {
+        let differenceX = pow(Decimal(self.pointA.x-self.pointB.x), 2)
+        let differenceY = pow(Decimal(self.pointA.y-self.pointB.y), 2)
+        let sum: Double = NSDecimalNumber(decimal: differenceX + differenceY).doubleValue
+        return sqrt(sum)
+    }
+}
--- CoordinateCalculator/Common/Extension/MyPoint+.swift
+++ CoordinateCalculator/Common/Extension/MyPoint+.swift
@@ -6,4 +6,9 @@
 //  Copyright © 2018년 Codesquad Inc. All rights reserved.
 //
 
-import Foundation
+// point 프로토콜을 준수
+extension MyPoint: PointProtocol {
+    func getPoints() -> [MyPoint] {
+        return [self]
+    }
+}
