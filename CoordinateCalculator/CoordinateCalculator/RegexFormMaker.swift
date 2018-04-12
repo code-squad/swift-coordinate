260c1cada65ac3a3649a02214a2e6bfbdb7cc5b2
DrakeYang <drake.the.programmer@gmail.com>
2018-04-12 12:30:10 +0900

    파일명 및 구조체명 변경

--- CoordinateCalculator/CoordinateCalculator/RegexFormMaker.swift
+++ CoordinateCalculator/CoordinateCalculator/RegexFormMaker.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 /// 정규식을 만드는 구조체
-struct Regexmaker {
+struct RegexMaker {
     /// 입력한 문자열로 정규식을 만들어주는 함수
     private func makeRegexForm(regexTry : String)->NSRegularExpression?{
         guard let regex = try? NSRegularExpression(pattern: regexTry, options: []) else {
