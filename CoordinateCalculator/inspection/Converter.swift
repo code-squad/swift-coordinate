0ea8b6c70d5ef82bbb714222c1e5a5d31cd283bf
Jung seoung Yeo <duwjdtmd91@gmail.com>
2018-04-12 15:55:21 +0900

    변환하는 객체 생성

--- CoordinateCalculator/inspection/Converter.swift
+++ CoordinateCalculator/inspection/Converter.swift
@@ -7,3 +7,23 @@
 //
 
 import Foundation
+
+// 변환기
+struct Converter {
+    static func getMyPoints(_ mypointStringFormat: String) throws -> [MyPoint] {
+        var mypoints: [MyPoint] = []
+        for mypoint in mypointStringFormat.splitDobulePoint() {
+            let coordinates = try Spliter.splitSaveFormat(mypoint)
+            mypoints.append(MyPoint(x: coordinates[0], y: coordinates[1]))
+        }
+        return mypoints
+    }
+    
+    static func getModel(_ points: [MyPoint]) throws -> BaseProtocol {
+        switch points.count {
+        case CoordCase.Point.rawValue: return MyPoint(points[0])
+        case CoordCase.Line.rawValue: return MyLine(points)
+        default: throw CoordinateError.unknownCaseError
+        }
+    }
+}
