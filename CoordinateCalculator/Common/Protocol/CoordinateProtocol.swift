28adbd734ff148a889d788a99f62314c90b0102d
Jung seoung Yeo <duwjdtmd91@gmail.com>
2018-04-12 15:51:40 +0900

    이름 변경 : pointprotocol -> BaseProtocol

--- CoordinateCalculator/Common/Protocol/CoordinateProtocol.swift
+++ CoordinateCalculator/Common/Protocol/CoordinateProtocol.swift
@@ -6,11 +6,11 @@
 //  Copyright © 2018년 Codesquad Inc. All rights reserved.
 //
 
-protocol PointProtocol {
+protocol BaseProtocol {
     func getPoints() -> [MyPoint]
 }
 
 // SOLID 원칙 중 인터페이스 분리 작업으로 MyPoint는 직선의거리를 구현 할 필요가 없어서 분리
-protocol LineProtocol: PointProtocol{
+protocol LineProtocol: BaseProtocol{
     func getDistance() -> Double
 }
