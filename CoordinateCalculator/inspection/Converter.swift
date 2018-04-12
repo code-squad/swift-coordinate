ba9ccd2c36fb94d7be13b38707cd10bdb61c9b8c
Jung seoung Yeo <duwjdtmd91@gmail.com>
2018-04-12 15:49:35 +0900

    이름 변경 : key -> CoordCase로 변경

---
+++ CoordinateCalculator/inspection/Converter.swift
@@ -0,0 +1,9 @@
+//
+//  Converter.swift
+//  CoordinateCalculator
+//
+//  Created by Jung seoung Yeo on 2018. 4. 12..
+//  Copyright © 2018년 Codesquad Inc. All rights reserved.
+//
+
+import Foundation
