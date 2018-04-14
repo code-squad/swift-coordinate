f83adf5beffa525e18145763d00a634cd6886410
Moon <myssun0325@gmail.com>
2018-04-14 10:10:12 +0900

    Add MyRect struct

---
+++ CoordinateCalculator/CoordinateCalculator/MyRect.swift
@@ -0,0 +1,31 @@
+//
+//  MyRect.swift
+//  CoordinateCalculator
+//
+//  Created by moon on 2018. 4. 14..
+//  Copyright © 2018년 Codesquad Inc. All rights reserved.
+//
+
+import Foundation
+
+struct MyRect: Figure, FigureCalculatable {
+    
+    var myPoints: [MyPoint]
+    
+    var leftTop = MyPoint()
+    var rightBottom = MyPoint()
+    
+    func calculateDistance() -> Double {
+        let width = self.rightBottom.x - self.leftTop.x
+        let height = self.leftTop.y - self.rightBottom.y
+        
+        return Double(width * height)
+    }
+}
+
+extension MyRect: DistancePrintable {
+    
+    var prefixText: String {
+        return "사각형 넓이는"
+    }
+}
