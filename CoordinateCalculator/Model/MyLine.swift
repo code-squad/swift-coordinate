a53c25715df8d8698ec1e18020ee6994e6b30579
Jung seoung Yeo <duwjdtmd91@gmail.com>
2018-04-16 17:24:20 +0900

    init 추가

--- CoordinateCalculator/Model/MyLine.swift
+++ CoordinateCalculator/Model/MyLine.swift
@@ -8,11 +8,21 @@
 
 // private(set)으로 선언 하여 Setter만 private
 struct MyLine: Equatable {
-    private(set) var pointA: MyPoint
-    private(set) var pointB: MyPoint
+    private(set) var pointA: MyPoint = MyPoint()
+    private(set) var pointB: MyPoint = MyPoint()
+    
+    init() {
+        self.pointA = MyPoint(x: 0, y: 0)
+        self.pointB = MyPoint(x: 0, y: 0)
+    }
     
     init(_ myPoint: [MyPoint]) {
         self.pointA = myPoint[0]
         self.pointB = myPoint[1]
+    }
+    
+    init(_ pointA: MyPoint, _ pointB: MyPoint) {
+        self.pointA = pointA
+        self.pointB = pointB
     }
 }
