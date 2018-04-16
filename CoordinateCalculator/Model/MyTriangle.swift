77e425c90cd872e1d9dd3aaa18b9cdc697446e94
Jung seoung Yeo <duwjdtmd91@gmail.com>
2018-04-16 17:16:18 +0900

    삼각형 case 추가

---
+++ CoordinateCalculator/Model/MyTriangle.swift
@@ -0,0 +1,9 @@
+//
+//  MyTriangle.swift
+//  CoordinateCalculator
+//
+//  Created by Jung seoung Yeo on 2018. 4. 14..
+//  Copyright © 2018년 Codesquad Inc. All rights reserved.
+//
+
+import Foundation
