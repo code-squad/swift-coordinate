b64a80b71d47dbcb3d83f12086c1923fe23b0758
Jung seoung Yeo <duwjdtmd91@gmail.com>
2018-04-17 01:47:45 +0900

    사각형 figureprotocol 준수

--- CoordinateCalculator/Common/Extension/MyRect+.swift
+++ CoordinateCalculator/Common/Extension/MyRect+.swift
@@ -6,4 +6,21 @@
 //  Copyright © 2018년 Codesquad Inc. All rights reserved.
 //
 
-import Foundation
+extension MyRect: FigureProtocol {
+    func figureCalculation() -> Double {
+        return Double((rightBottom.x - leftTop.x) * (leftTop.y - rightBottom.y))
+    }
+    
+    var description: String {
+        return "사각형 넓이는 "
+    }
+    
+    func getPoints() -> [MyPoint] {
+        let leftBottom = MyPoint(x: self.leftTop.x, y: self.rightBottom.y)
+        let rightTop = MyPoint(x: self.rightBottom.x, y: self.leftTop.y)
+        
+        return [leftBottom, leftTop, rightBottom, rightTop]
+    }
+    
+    
+}
