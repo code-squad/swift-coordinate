a68857c902eddce4a4e36b8425103114c8a1f7c0
Jung seoung Yeo <duwjdtmd91@gmail.com>
2018-04-17 01:46:14 +0900

    사각형 case 추가

---
+++ CoordinateCalculator/Common/Extension/MyRect+.swift
@@ -0,0 +1,9 @@
+//
+//  MyRect+.swift
+//  CoordinateCalculator
+//
+//  Created by Jung seoung Yeo on 2018. 4. 17..
+//  Copyright © 2018년 Codesquad Inc. All rights reserved.
+//
+
+import Foundation
---
+++ CoordinateCalculator/Model/MyRect.swift
@@ -0,0 +1,13 @@
+//
+//  MyRect.swift
+//  CoordinateCalculator
+//
+//  Created by Jung seoung Yeo on 2018. 4. 16..
+//  Copyright © 2018년 Codesquad Inc. All rights reserved.
+//
+
+import Cocoa
+
+class MyRect: NSObject {
+
+}
