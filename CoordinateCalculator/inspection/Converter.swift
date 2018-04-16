cf99667ebaac1b74e710f90340a4307ba4a62259
Jung seoung Yeo <duwjdtmd91@gmail.com>
2018-04-16 17:21:33 +0900

    지수 계산 operator인 ^ 추가

--- CoordinateCalculator/inspection/Converter.swift
+++ CoordinateCalculator/inspection/Converter.swift
@@ -19,11 +19,28 @@ struct Converter {
         return mypoints
     }
     
-    static func getModel(_ points: [MyPoint]) throws -> BaseProtocol {
+    static func getFigure(_ points: [MyPoint]) throws -> BaseProtocol {
         switch points.count {
-        case CoordCase.Point.rawValue: return MyPoint(points[0])
+        case CoordCase.Point.rawValue: return MyPoint(points)
         case CoordCase.Line.rawValue: return MyLine(points)
+        case CoordCase.Triangle.rawValue: return MyTriangle(points)
         default: throw CoordinateError.unknownCaseError
         }
     }
 }
+
+infix operator ^
+
+// 지수 계산 : 2.0^2 = 2.0 * 2.0 = 4, 2.0^3 = 2.0 * 2.0 * 2.0 = 8.0, 1.0^0 = 1.0 * 0 = 0
+func ^ (lhs: Double, rhs: Int) -> Double {
+    guard rhs != 0 else {
+        return 0.0
+    }
+
+    var result = lhs
+    for _ in 0 ..< (rhs - 1) {
+        result *= lhs
+    }
+    return result
+}
+
