a93be901f0cb977fddeebd75085de45c107d649d
Jung seoung Yeo <duwjdtmd91@gmail.com>
2018-04-16 17:17:24 +0900

    삼각형 계산 추가

--- CoordinateCalculator/Common/Extension/MyTriangle+.swift
+++ CoordinateCalculator/Common/Extension/MyTriangle+.swift
@@ -6,8 +6,32 @@
 //  Copyright © 2018년 Codesquad Inc. All rights reserved.
 //
 
-import Cocoa
+import Foundation
 
-class MyTriangle_: NSObject {
-
+extension MyTriangle: FigureProtocol {
+    
+    func getPoints() -> [MyPoint] {
+        return [self.lineAB.pointA,
+                self.lineAB.pointB,
+                self.lineBC.pointA,
+                self.lineBC.pointB,
+                self.lineAC.pointA,
+                self.lineAC.pointB ]
+    }
+    
+    func figureCalculation() -> Double {
+        let a = self.lineBC.figureCalculation()
+        let b = self.lineAC.figureCalculation()
+        let c = self.lineAB.figureCalculation()
+        
+        let cosB = (a^2 + c^2 - b^2) / (2 * a * c)
+        let sinB = sqrt(1 - cosB * cosB)
+        let s = a * c * sinB / 2
+        
+        return s
+    }
+    
+    var description: String {
+        return "삼각형 넓이는 "
+    }
 }
