0ab251805cad259cfd537dba04fd28eb0d0801ff
Jung seoung Yeo <duwjdtmd91@gmail.com>
2018-04-16 17:22:10 +0900

    삼각형 모델 생성

--- CoordinateCalculator/Model/MyTriangle.swift
+++ CoordinateCalculator/Model/MyTriangle.swift
@@ -6,4 +6,20 @@
 //  Copyright © 2018년 Codesquad Inc. All rights reserved.
 //
 
-import Foundation
+struct MyTriangle: Equatable {
+    var lineAB = MyLine( MyPoint(), MyPoint())
+    var lineBC = MyLine( MyPoint(), MyPoint())
+    var lineAC = MyLine( MyPoint(), MyPoint())
+    
+    init( _ pointA: MyPoint, _ pointB: MyPoint, _ pointC: MyPoint) {
+        self.lineAC = MyLine(pointA, pointB)
+        self.lineBC = MyLine(pointB, pointC)
+        self.lineAC = MyLine(pointA, pointC)
+    }
+    
+    init(_ points: [MyPoint]) {
+        self.lineAB = MyLine(points[0], points[1])
+        self.lineBC = MyLine(points[1], points[2])
+        self.lineAC = MyLine(points[0], points[2])
+    }
+}
