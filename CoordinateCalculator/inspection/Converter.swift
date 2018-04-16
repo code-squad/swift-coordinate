857eace10c977b921f502c9e378423c3fbad6f9a
Jung seoung Yeo <duwjdtmd91@gmail.com>
2018-04-17 01:50:07 +0900

    직사각형 검사부, 사각형 return case 추가

--- CoordinateCalculator/inspection/Converter.swift
+++ CoordinateCalculator/inspection/Converter.swift
@@ -21,11 +21,56 @@ struct Converter {
     
     static func getFigure(_ points: [MyPoint]) throws -> BaseProtocol {
         switch points.count {
-        case CoordCase.Point.rawValue: return MyPoint(points)
-        case CoordCase.Line.rawValue: return MyLine(points)
-        case CoordCase.Triangle.rawValue: return MyTriangle(points)
+            case CoordCase.Point.rawValue: return MyPoint(points)
+            case CoordCase.Line.rawValue: return MyLine(points)
+            case CoordCase.Triangle.rawValue: return MyTriangle(points)
+            case CoordCase.Rect.rawValue:
+                let (origin, size) = try getMyRactOfOrigin(points)
+                return MyRect(origin: origin, size: size)
         default: throw CoordinateError.unknownCaseError
         }
+    }
+    
+    static private func getMyRactOfOrigin(_ mypoints: [MyPoint]) throws -> (MyPoint, CGSize) {
+        var xSet = Set<Int>()
+        var ySet = Set<Int>()
+        
+        for mypoint in mypoints {
+            xSet.insert(mypoint.x)
+            ySet.insert(mypoint.y)
+        }
+        
+        let (xMin, xMax, yMin, yMax) = try getMinWithMax(xSet, ySet)
+        
+        let leftTop = MyPoint(x: xMin, y: yMax)
+        let size = CGSize(width: xMax - xMin, height: yMax - yMin)
+        
+        return (leftTop, size)
+    }
+    
+    static private func getMinWithMax(_ xPoints: Set<Int>, _ yPoints: Set<Int>) throws -> (Int,Int,Int,Int) {
+        
+        guard xPoints.count == 2 && yPoints.count == 2 else {
+            throw CoordinateError.isRect
+        }
+        
+        guard let xMin = xPoints.min() else {
+            throw CoordinateError.isRect
+        }
+        
+        guard let xMax = xPoints.max() else {
+            throw CoordinateError.isRect
+        }
+        
+        guard let yMin = yPoints.min() else {
+            throw CoordinateError.isRect
+        }
+        
+        guard let yMax = yPoints.max() else {
+            throw CoordinateError.isRect
+        }
+        
+        return (xMin, xMax, yMin, yMax)
     }
 }
 
