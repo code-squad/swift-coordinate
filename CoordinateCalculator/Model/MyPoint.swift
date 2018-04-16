6840105c14b9c8d2f39e57c788df7bc6c61865b5
Jung seoung Yeo <duwjdtmd91@gmail.com>
2018-04-16 17:23:48 +0900

    init 추가

--- CoordinateCalculator/Model/MyPoint.swift
+++ CoordinateCalculator/Model/MyPoint.swift
@@ -10,17 +10,21 @@ import Foundation
 
 // MyPoint 저장 변수 Private(set) 으로 setter만 private로 선언
 struct MyPoint: Equatable {
-    private(set) var x: Int = 0
-    private(set) var y: Int = 0
+    private(set) var x: Int
+    private(set) var y: Int
+    
+    init() {
+        self.x = 0
+        self.y = 0
+    }
     
     init(x: Int, y: Int) {
         self.x = x
         self.y = y
     }
     
-    init(_ mypoint: MyPoint) {
-        self = mypoint
+    init(_ mypoint: [MyPoint]) {
+        self = mypoint[0]
     }
-    
 }
 
