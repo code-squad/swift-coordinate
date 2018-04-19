4e05e1594758bf814d058da5371d15846891d4b0
DrakeYang <drake.the.programmer@gmail.com>
2018-04-19 19:01:04 +0900

    입력값 체크 기능까지 구현

--- CoordinateCalculator/CoordinateCalculator/main.swift
+++ CoordinateCalculator/CoordinateCalculator/main.swift
@@ -9,9 +9,18 @@
 import Foundation
 
 func main(){
+
     /// 프린트용 구조체 선언
     let outputView = OutputView()
     outputView.drawAxis()
+    /// 인풋뷰 구조체 선언
+    let cutter = Cutter()
+    /// 유저인풋을 받음
+    guard let pp = cutter.cutRangeFrom() else {
+        return 
+    }
 }
 
 main()
+
+
