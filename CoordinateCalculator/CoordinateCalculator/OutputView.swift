f2d91ea671a56692361262ed19c8e01438345371
DrakeYang <drake.the.programmer@gmail.com>
2018-04-20 11:26:43 +0900

    좌표를 먼저 그리고 포인트를 그리게 수정 포인트 그리는 문자를 마이포인트에서 얻도록 설정

--- CoordinateCalculator/CoordinateCalculator/OutputView.swift
+++ CoordinateCalculator/CoordinateCalculator/OutputView.swift
@@ -11,8 +11,18 @@ import Foundation
 struct OutputView{
     /// 좌표축을 프린트
     func drawAxis(){
+        // 화면 지우기
         print("\(ANSICode.clear)\(ANSICode.home)")
+        // 좌표 그리기
         print("\(ANSICode.text.redBright)\(ANSICode.axis.draw())")
+    }
+    
+    /// 입력된 자표를 출력
+    func drawPoint(myPoint : MyPoint){
+        // 입력받은 위치로 커서 이동
+        print("\(ANSICode.cursor.move(row:25-myPoint.y, col: myPoint.x*2+3))\(myPoint.mark)")
+        // 특수문자 출력
+        print("\(ANSICode.cursor.move(row:26, col: 0))")
     }
 }
 
