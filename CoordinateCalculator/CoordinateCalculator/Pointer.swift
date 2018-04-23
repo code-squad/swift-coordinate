07f5757bf8654afb7d5a7ebc14e33219fd352642
DrakeYang <drake.the.programmer@gmail.com>
2018-04-23 15:49:39 +0900

    Pointer 함수 타입메소드로 변경

--- CoordinateCalculator/CoordinateCalculator/Pointer.swift
+++ CoordinateCalculator/CoordinateCalculator/Pointer.swift
@@ -10,7 +10,7 @@ import Foundation
 
 struct Pointer {
     /// 배열을 받아서 마이포인트로 리턴
-    func makeMyPoint(numbers : Array<Int>)->MyPoint {
+    static func makeMyPoint(numbers : Array<Int>)->MyPoint {
         // 배열이 2개짜리이므로 바로 리턴
         return MyPoint(x: numbers[0], y: numbers[1])
     }
