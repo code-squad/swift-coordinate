93129250abcb6a89ef2d253db9113795b32ba48c
DrakeYang <drake.the.programmer@gmail.com>
2018-04-23 14:41:41 +0900

    outputView 선언을 밑으로 내림

--- CoordinateCalculator/CoordinateCalculator/main.swift
+++ CoordinateCalculator/CoordinateCalculator/main.swift
@@ -9,8 +9,6 @@
 import Foundation
 
 func main(){
-    // 프린트용 구조체 선언
-    let outputView = OutputView()
     // 인풋뷰 구조체 선언
     let cutter = Cutter()
     // 유저인풋을 받음
@@ -21,6 +19,8 @@ func main(){
     let pointer = Pointer()
     // 정규화를 거친 좌표값을 마이포인트로 선언
     let myPoint = pointer.makeMyPoint(numbers: axis)
+    // 프린트용 구조체 선언
+    let outputView = OutputView()
     // 좌표축 프린트
     outputView.drawAxis()
     // 마이포인트 좌표에 특수문자 출력
