a431431c2fb70d5ccc726dd5ccc9daeb42c221e3
DrakeYang <drake.the.programmer@gmail.com>
2018-04-24 19:41:00 +0900

    입력값 받는 부분에서 라인인지도 체크하는 부분추가

--- CoordinateCalculator/CoordinateCalculator/InputView.swift
+++ CoordinateCalculator/CoordinateCalculator/InputView.swift
@@ -18,11 +18,11 @@ struct InputView {
     }
     
     /// 정규식을 통과할때까지 유저입력을 받는 함수
-    func receiveUserAxis()->Array<Int>? {
+    func receiveUserAxis()->Array<MyPoint>? {
         // 유저입력값을 저장하기 위한 변수
         var userAxis = ""
         // 결과값을 저장하기 위한 배열 선언
-        var axisNumbers : Array<Int>?
+        var myPointList : Array<MyPoint>?
         
         // 정규식을 이용하기 위해서 커터 선언
         let cutter = Cutter()
@@ -30,11 +30,21 @@ struct InputView {
         repeat {
             // 입력을 위한 안내 메세지 출력
             print("좌표를 입력하세요. 예시: (12,4)")
-            // 유저입력값을 저장하는 변수
+            // 유저입력값을 저장
             userAxis = receiveUserInput()
-            axisNumbers = cutter.cutAxisFrom(userAxis: userAxis)
-        } while axisNumbers == nil
-        return axisNumbers
+            // 1개 좌표단위로 자름
+            guard let axisList = cutter.cutAxisFrom(userAxis: userAxis) else {
+                continue
+            }
+            guard let myPointListCheck = cutter.makeMyPointListFrom(confirmedAxisList: axisList) else {
+                continue
+            }
+            myPointList = myPointListCheck
+            if !cutter.isitLineIn(userAxis: userAxis) && myPointList!.count > 1 {
+                myPointList = nil
+            }
+        } while myPointList == nil
+        return myPointList
     }
 }
     
