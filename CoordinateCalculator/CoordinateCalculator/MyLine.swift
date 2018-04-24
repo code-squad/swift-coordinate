50aec809cacce10b2f3814ca9f8ae1cd2966c2e1
DrakeYang <drake.the.programmer@gmail.com>
2018-04-24 11:31:17 +0900

    두 점 사이의 거리를 리턴하는 함수 생성

--- CoordinateCalculator/CoordinateCalculator/MyLine.swift
+++ CoordinateCalculator/CoordinateCalculator/MyLine.swift
@@ -7,3 +7,13 @@
 //
 
 import Foundation
+
+struct MyLine {
+    var pointA = MyPoint(x: 0, y: 0)
+    var pointB = MyPoint(x: 0, y: 0)
+    
+    /// 두 점 사이의 거리를 리턴
+    func distanceBetween()->Double {
+        return sqrt(Double((self.pointA.x - self.pointB.x)^2 + (self.pointA.y - self.pointB.y)^2))
+    }
+}
