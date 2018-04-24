ccf9a95da303fd72488040b6a05e1d810b0e012b
DrakeYang <drake.the.programmer@gmail.com>
2018-04-24 15:03:56 +0900

    라인 체크용 정규식표현 추가

--- CoordinateCalculator/CoordinateCalculator/Regex.swift
+++ CoordinateCalculator/CoordinateCalculator/Regex.swift
@@ -9,8 +9,10 @@
 import Foundation
 /// 정규식이 담겨있는 구조체
 struct Regex {
-    /// 정규식화용 정규식표현
+    /// 좌표1개 추출 정규식표현
     static let forAxisCheck = "\\([0-9]{1,}\\,[0-9]{1,}\\)"
     /// 좌표값만 추출하는 정귝식표현
     static let forNumberCheck = "[0-9]{1,2}"
+    /// 라인추출용 정규식 표현
+    static let forLineCheck = "\\([0-9]{1,}\\,[0-9]{1,}\\)\\-\\([0-9]{1,}\\,[0-9]{1,}\\)"
 }
