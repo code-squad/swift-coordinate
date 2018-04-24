6d5d7aec35007948122fa14b7e5f91f7cf719829
DrakeYang <drake.the.programmer@gmail.com>
2018-04-24 19:38:14 +0900

    넘버체크에 숫자개수 제한 없앰 - 오류체크범위 확대

--- CoordinateCalculator/CoordinateCalculator/Regex.swift
+++ CoordinateCalculator/CoordinateCalculator/Regex.swift
@@ -12,7 +12,7 @@ struct Regex {
     /// 좌표1개 추출 정규식표현
     static let forAxisCheck = "\\([0-9]{1,}\\,[0-9]{1,}\\)"
     /// 좌표값만 추출하는 정귝식표현
-    static let forNumberCheck = "[0-9]{1,2}"
+    static let forNumberCheck = "[0-9]{1,}"
     /// 라인추출용 정규식 표현
     static let forLineCheck = "\\([0-9]{1,}\\,[0-9]{1,}\\)\\-\\([0-9]{1,}\\,[0-9]{1,}\\)"
 }
