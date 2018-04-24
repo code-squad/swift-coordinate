62bb68499cbd27740ad760c37897cdf6a7437c8d
DrakeYang <drake.the.programmer@gmail.com>
2018-04-24 19:42:44 +0900

    두점일 경우 출력하는 함수 추가

--- CoordinateCalculator/CoordinateCalculator/OutputView.swift
+++ CoordinateCalculator/CoordinateCalculator/OutputView.swift
@@ -18,11 +18,22 @@ struct OutputView{
     }
     
     /// 입력된 자표를 출력
-    func drawPoint(myPoint : MyPoint){
+    private func drawPoint(myPoint : MyPoint){
         // 입력받은 위치로 커서 이동
         print("\(ANSICode.cursor.move(row:25-myPoint.y, col: myPoint.x*2+3))\(myPoint.mark)")
         // 특수문자 출력
         print("\(ANSICode.cursor.move(row:26, col: 0))")
+    }
+    
+    /// 마이포인트 리스트를 받아서 라인이나 포인트로 바꿔서 프린트
+    func draw(myPointList:Array<MyPoint>){
+        for myPoint in myPointList {
+            drawPoint(myPoint: myPoint)
+        }
+        if myPointList.count == 2 {
+            let myLine = MyLine(pointA : myPointList[0], pointB : myPointList[1])
+            print("두 점 사이 거리는 \(myLine.distanceBetween())")
+        }
     }
 }
 
