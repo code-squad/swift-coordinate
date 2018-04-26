de6e8dce4b1743ea48c49ce63f60cd336471ee79
DrakeYang <drake.the.programmer@gmail.com>
2018-04-26 15:39:27 +0900

    입력값 검증하는 부분을 체커로 나누어 옮김 정규식으로 형태검사 이후에 숫자로 좌표범위 체크 예정 정규시긍로 체크하는 부분 작성

--- CoordinateCalculator/CoordinateCalculator/Checker.swift
+++ CoordinateCalculator/CoordinateCalculator/Checker.swift
@@ -10,7 +10,7 @@ import Foundation
 
 struct Checker {
     
-    /// 좌료값 범위를 체크
+    /// 숫자를 받앙서 좌료값 범위를 체크
     func checkPointRange(pointList : Array<Int>)->Bool{
         // 좌표값의 범위 체크
         for point in pointList {
@@ -32,5 +32,50 @@ struct Checker {
             numbers.append(number)
         }
         return numbers
+    }
+    
+    /// 문자열이 2개좌표 형태가 맞는지 체크
+    private func isCorrectLine(latters:String)->Bool {
+        // 라인 정규식을 통과시킨다
+        guard let line = Extracter.extractLineFrom(originLatters: latters) else {
+            return false
+        }
+        // 통과한 문자열의 카운트를 체크한다
+        guard line.count > 0 else {
+            // 통과 못하면 2개좌표의 형태가 아니다
+            return false
+        }
+        // 맞으면 참 리턴
+        return true
+    }
+    
+    /// 문자열이 1개좌표 형태가 몇개인지 체크
+    private func howManyPointIn(latters:String)->Int? {
+        // 1개좌표 정규식을 통과시킨다
+        guard let point = Extracter.extractPointFrom(originLatters: latters) else {
+            return nil
+        }
+        // 통과한 문자열의 카운트를 체크한다
+        let pointCount = point.count
+        guard pointCount > 0 else {
+            // 통과를 못하면 1개좌표의 형태가 아님
+            return nil
+        }
+        // 검증을 통과했으니 카운트 리턴
+        return pointCount
+    }
+    
+    /// 좌표의 형태 전부를 한번에 체크
+    func isCorrectPointType(latters:String)->Bool{
+        //1개좌표 형태는 무조건 맞아야됨
+        guard let pointCount = howManyPointIn(latters: latters) else {
+            return false
+        }
+        // 2개좌표형태가 아닌데 1개좌표가 2개 이상이면 안됨
+        if !isCorrectLine(latters: latters) || pointCount > 1 {
+            return false
+        }
+        // 전부 통과했으면 참 리턴
+        return true
     }
 }
