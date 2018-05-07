b7e7ba336e11e10e54a451b372cb03a69ba6ef0e
DrakeYang <drake.the.programmer@gmail.com>
2018-05-07 20:13:16 +0900

    삼각형의 넓이 리턴하는 함수 추가

--- CoordinateCalculator/CoordinateCalculator/MyTriangle.swift
+++ CoordinateCalculator/CoordinateCalculator/MyTriangle.swift
@@ -24,16 +24,22 @@ struct MyTriangle : Points {
     }
     /// 삼각형의 넓이를 구하는 함수
     func calculateArea() -> Double {
-        //세 변의 길이를 구한다
-        let lengthA = lineAB.distanceBetweenPonints()
-        let lengthB = lineAC.distanceBetweenPonints()
-        let lengthC = lineBC.distanceBetweenPonints()
+        //세 변의 길이를 구한다. 소숫점 2자리 까지만 출력
+        let lengthA = (lineAB.distanceBetweenPonints()*100).rounded() / 100
+        let lengthB = (lineAC.distanceBetweenPonints()*100).rounded() / 100
+        let lengthC = (lineBC.distanceBetweenPonints()*100).rounded() / 100
+        // 계산식이 너무 복잡해서 나눔
+        let step1 = (lengthA+lengthB+lengthC)
+        let step2 = (-lengthA+lengthB+lengthC)
+        let step3 = (lengthA-lengthB+lengthC)
+        let step4 = (lengthA+lengthB-lengthC)
         
+        let step5 = (step1*step2*step3*step4*100).rounded() / 100
         // 공식은 헤론의 공식을 사용함
-        let area = sqrt((lengthA+lengthB+lengthC)(-lengthA+lengthB+lengthC)(lengthA-lengthB+lengthC)(lengthA+lengthB-lengthC))/4
-        // 결과값 리턴 
-        return area
-    }
+        let area = sqrt(step5)/4
+        // 결과값 리턴
+        return (area*100).rounded() / 100
+    } 
     
     /// 프로토콜을 준수
     func getPoints() -> [MyPoint] {
