9d2d3d57aa91e1c197a372ba587dc6ee108f7302
DrakeYang <drake.the.programmer@gmail.com>
2018-05-07 17:47:51 +0900

    정규식 이름 수정

--- CoordinateCalculator/CoordinateCalculator/Regex.swift
+++ CoordinateCalculator/CoordinateCalculator/Regex.swift
@@ -10,7 +10,7 @@ import Foundation
 /// 정규식이 담겨있는 구조체
 struct Regex {
     /// 좌표1개 추출 정규식표현
-    static let forAxisCheck = "\\([0-9]{1,}\\,[0-9]{1,}\\)"
+    static let forPointCheck = "\\([0-9]{1,}\\,[0-9]{1,}\\)"
     /// 좌표값만 추출하는 정귝식표현
     static let forNumberCheck = "[0-9]{1,}"
     /// 라인추출용 정규식 표현
