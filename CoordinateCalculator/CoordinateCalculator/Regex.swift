f2887f5ea3acda28cd9af749574572cf540aaa79
DrakeYang <drake.the.programmer@gmail.com>
2018-05-08 18:49:37 +0900

    사각형용 정규식 표현 추가

--- CoordinateCalculator/CoordinateCalculator/Regex.swift
+++ CoordinateCalculator/CoordinateCalculator/Regex.swift
@@ -9,12 +9,14 @@
 import Foundation
 /// 정규식이 담겨있는 구조체
 struct Regex {
-    /// 좌표1개 추출 정규식표현
-    static let forPointCheck = "\\([0-9]{1,}\\,[0-9]{1,}\\)"
     /// 좌표값만 추출하는 정귝식표현
     static let forNumberCheck = "[0-9]{1,}"
+    /// 좌표1개 추출 정규식표현
+    static let forPointCheck = "\\("+forNumberCheck+"\\,"+forNumberCheck+"\\)"
     /// 라인추출용 정규식 표현
-    static let forLineCheck = "\\([0-9]{1,}\\,[0-9]{1,}\\)\\-\\([0-9]{1,}\\,[0-9]{1,}\\)"
+    static let forLineCheck = forPointCheck+"\\-"+forPointCheck
     /// 삼각형 추출용 정규식 표현
-    static let forTriangle = "\\([0-9]{1,}\\,[0-9]{1,}\\)\\-\\([0-9]{1,}\\,[0-9]{1,}\\)\\-\\([0-9]{1,}\\,[0-9]{1,}\\)"
+    static let forTriangle = forLineCheck+"\\-"+forPointCheck
+    /// 사각형 추출용 정규식 표현
+    static let forRectangle = forTriangle+"\\-"+forPointCheck
 }
