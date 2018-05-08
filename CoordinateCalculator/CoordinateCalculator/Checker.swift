5c34f8e04186c3fb837afe52ca0137c249696bcc
DrakeYang <drake.the.programmer@gmail.com>
2018-05-08 13:58:08 +0900

    체크부분의 공통부분 좀 더 공통화

--- CoordinateCalculator/CoordinateCalculator/Checker.swift
+++ CoordinateCalculator/CoordinateCalculator/Checker.swift
@@ -22,9 +22,9 @@ struct Checker {
     }
     
     /// 숫자가 섞인 문자열을 받아서 숫자로 변환 후 좌표 범위 체크
-    func checkPointRange(latters:String)->Bool{
+    func checkPointRange(letters:String)->Bool{
         // 입력값을 숫자만 있는 문자형 배열로 정규화
-        guard let regexedNumbers = Extracter.extractNumbersFrom(originLetters: latters) else {
+        guard let regexedNumbers = Extracter.extractNumbersFrom(originLetters: letters) else {
             return false
         }
         // 숫자 문자형배열을 숫자형 배열로 리턴
@@ -35,10 +35,14 @@ struct Checker {
         return checkPointRange(pointList:numbers)
     }
     
-    /// 입력받은 문자열의 count 수가 0 이상이면 참. 아니면 거짓. 정규식을 통과시킨 결과값으로 형태가 맞는지 체크.
-    func isCountable(letters : [String])->Bool{
+    /// 정규화 시도 후 결과 배열의 count 수가 0 이상이면 참. 아니면 거짓.
+    func isCountable(letters : [String]?)->Bool{
+        // 옵셔널인 정규화 결과를 입력
+        guard let line = letters else {
+            return false
+        }
         // 문자열의 카운트를 체크한다
-        guard letters.count > 0 else {
+        guard line.count > 0 else {
             // 통과 못하면 2개좌표의 형태가 아니다
             return false
         }
@@ -47,29 +51,20 @@ struct Checker {
     }
     
     /// 문자열이 2개좌표 형태가 맞는지 체크
-    private func isCorrectLine(latters:String)->Bool {
-        // 라인 정규식을 통과시킨다
-        guard let line = Extracter.extractLineFrom(originLetters: latters) else {
-            return false
-        }
-        // 통과한 문자열의 카운트를 체크한다
-        return isCountable(letters: line)
+    private func isCorrectLine(letters:String)->Bool {
+        return isCountable(letters: Extracter.extractLineFrom(originLetters: letters))
     }
     
     /// 문자열이 3개좌표 형태가 맞는지 체크
-    private func isCorrectTriangle(latters:String)->Bool {
-        // 라인 정규식을 통과시킨다
-        guard let line = Extracter.extractTriangleFrom(originLetters: latters) else {
-            return false
-        }
+    private func isCorrectTriangle(letters:String)->Bool {
         // 통과한 문자열의 카운트를 체크한다
-        return isCountable(letters: line)
+        return isCountable(letters: Extracter.extractTriangleFrom(originLetters: letters) )
     }
     
     /// 문자열이 1개좌표 형태가 몇개인지 체크
-    private func howManyPointIn(latters:String)->Int? {
+    private func howManyPointIn(letters:String)->Int? {
         // 1개좌표 정규식을 통과시킨다
-        guard let point = Extracter.extractPointFrom(originLetters: latters) else {
+        guard let point = Extracter.extractPointFrom(originLetters: letters) else {
             return nil
         }
         // 통과한 문자열의 카운트를 체크한다
@@ -83,17 +78,17 @@ struct Checker {
     }
     
     /// 좌표의 형태 전부를 한번에 체크
-    func isCorrectPointType(latters:String)->Bool{
+    func isCorrectPointType(letters:String)->Bool{
         //1개좌표 형태는 무조건 맞아야됨
-        guard let pointCount = howManyPointIn(latters: latters) else {
+        guard let pointCount = howManyPointIn(letters: letters) else {
             return false
         }
         // 좌표가 2개 이상인데 라인이 아니면 체크
-        if isCorrectLine(latters: latters)==false && pointCount <= 1 {
+        if isCorrectLine(letters: letters)==false && pointCount <= 1 {
             return false
         }
         // 좌표2개가 아닌데 3개인 경우를 체크
-        guard isCorrectTriangle(latters: latters) else {
+        guard isCorrectTriangle(letters: letters) else {
             return false
         }
         // 전부 통과했으면 참 리턴
