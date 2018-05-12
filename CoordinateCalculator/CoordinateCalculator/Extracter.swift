097857888230a7ea10af5d40872fb5242f98ebbf
DrakeYang <drake.the.programmer@gmail.com>
2018-05-12 12:30:51 +0900

    사각형 모양 체크를 위한 최대,최소점 추출함수 추가. CGSize 추출 함수 추가

--- CoordinateCalculator/CoordinateCalculator/Extracter.swift
+++ CoordinateCalculator/CoordinateCalculator/Extracter.swift
@@ -68,4 +68,82 @@ struct Extracter {
         }
         return numbers
     }
+    
+    /// 마이포인트 배열을 받아서 x 좌표값을 모아서 배열로 리턴
+    static func extractXPointsFrom(points:[MyPoint])->[Int]{
+        // x,y 좌표를 받을 배열 선언
+        var xPoints = [Int]()
+        
+        // 입력받은 마이포인트에서 좌표를 뽑아낸다
+        for point in points {
+            xPoints.append(point.getXAxis())
+        }
+        return xPoints
+    }
+    
+    /// 마이포인트 배열을 받아서 x 좌표값을 모아서 배열로 리턴
+    static func extractYPointsFrom(points:[MyPoint])->[Int]{
+        // x,y 좌표를 받을 배열 선언
+        var yPoints = [Int]()
+        
+        // 입력받은 마이포인트에서 좌표를 뽑아낸다
+        for point in points {
+            yPoints.append(point.getYAxis())
+        }
+        return yPoints
+    }
+    
+    /// 마이포인트 배열을 받아서 x 좌표의 최대값을 리턴
+    static func extractMaxXPoint(_ points:[MyPoint])->Int{
+        // x,y 좌표를 받을 배열 선언
+        let xPoints = Extracter.extractXPointsFrom(points: points)
+        // 최대 값을 리턴
+        return xPoints.max()!
+    }
+    
+    /// 마이포인트 배열을 받아서 y 좌표의 최대값을 리턴
+    static func extractMaxYPoint(_ points:[MyPoint])->Int{
+        // x,y 좌표를 받을 배열 선언
+        let yPoints = Extracter.extractYPointsFrom(points: points)
+        // 최대 값을 리턴
+        return yPoints.max()!
+    }
+    
+    /// 마이포인트 배열을 받아서 x 좌표의 최소값을 리턴
+    static func extractMinXPoint(_ points:[MyPoint])->Int{
+        // x,y 좌표를 받을 배열 선언
+        let xPoints = Extracter.extractXPointsFrom(points: points)
+        // 최대 값을 리턴
+        return xPoints.min()!
+    }
+    
+    /// 마이포인트 배열을 받아서 y 좌표의 최소값을 리턴
+    static func extractMinYPoint(_ points:[MyPoint])->Int{
+        // x,y 좌표를 받을 배열 선언
+        let yPoints = Extracter.extractXPointsFrom(points: points)
+        // 최대 값을 리턴
+        return yPoints.min()!
+    }
+    
+    /// 마이포인트 배열을 받아서 가로세로를 구해서 Int CGSize 로 리턴
+    static func extractCGSize(_ points:[MyPoint])->CGSize{
+        // 최대 최소 값을 구한다.
+        let maxXPoint = Extracter.extractMaxXPoint(points)
+        let maxYPoint = Extracter.extractMaxYPoint(points)
+        let minXPoint = Extracter.extractMinXPoint(points)
+        let minYPoint = Extracter.extractMinYPoint(points)
+        
+        // 최대값에서 최소값을 빼서 가로 세로 를 구한다
+        return CGSize(width: maxXPoint-minXPoint, height: maxYPoint-minYPoint)
+    }
+    
+    /// 마이포인트 배열을 받아서 기준점을 리턴
+    static func extractOriginPoint(_ points:[MyPoint])->MyPoint{
+        // 최대Y 최소X 값을 구한다.
+        let maxYPoint = Extracter.extractMaxYPoint(points)
+        let minXPoint = Extracter.extractMinXPoint(points)
+        
+        // 기준점은 왼쪽 위 이니 마이포인트로 리턴한다
+        return MyPoint(x: minXPoint, y: maxYPoint)
+    }
 }
