74b23abdab3664c8ecb8a8b7f5ae42b7c0ce2ed3
superhakgoman <superhakgoman@gmail.com>
2018-07-12 05:37:17 +0900

    안 쓰는 것 삭제

--- CoordinateCalculator/CoordinateCalculator/StaticData.swift
+++ CoordinateCalculator/CoordinateCalculator/StaticData.swift
@@ -20,17 +20,11 @@ public struct StaticData{
         case notRectangle
     }
     static let ErrStrDict : [InputError : String] = [
+        InputError.unknown : "알 수 없 는 오 류",
         InputError.empty : "입 력 없 음",
         InputError.not2D : "2 차 원 좌 표 만",
         InputError.notInt : "정 수 만",
         InputError.outRange : "0 부 터 2 4 까 지",
         InputError.notRectangle : "직 사 각 형 아 님"
     ];
-    enum RunMode {
-        case none;
-        case point;
-        case line;
-        case triangle;
-        case rectangle;
-    }
 }
