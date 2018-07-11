94dcea92fbb8608351b04f08925889f2f4603fcd
superhakgoman <superhakgoman@gmail.com>
2018-07-12 03:22:10 +0900

    property로 변경

--- CoordinateCalculator/CoordinateCalculator/ANSICode.swift
+++ CoordinateCalculator/CoordinateCalculator/ANSICode.swift
@@ -146,13 +146,15 @@ public struct ANSICode {
     public struct MyLine : MyDraw {
         var pointA = MyPoint(x: 0, y: 0)
         var pointB = MyPoint(x: 0, y: 0)
-        func getLength() -> Double {
-            let deltaX = pointB.x - pointA.x
-            let deltaY = pointB.y - pointA.y
-            return sqrt( Double((deltaX*deltaX) + (deltaY*deltaY)))
+        var length : Double{
+            get{
+                let deltaX = pointB.x - pointA.x
+                let deltaY = pointB.y - pointA.y
+                return sqrt( Double((deltaX*deltaX) + (deltaY*deltaY)))
+            }
         }
         public func draw() -> String {
-            return "\(ANSICode.cursor.move(row:axis.AxisLimit + 3, col: 0))두 점 사이의 거리는 \(getLength())"
+            return "\(ANSICode.cursor.move(row:axis.AxisLimit + 3, col: 0))두 점 사이의 거리는 \(length)"
         }
     }
     
