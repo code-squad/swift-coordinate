2274079b50aa613526ee142b4dc41438fc4d86c4
Jung Kim <godrm77@gmail.com>
2018-07-12 18:08:40 +0900

    Revert "ist official commit"
    This reverts commit 87e9845f7cefa1cbb825b4b4967a7145ba6f0c6e.

--- CoordinateCalculator/CoordinateCalculator/main.swift
+++ CoordinateCalculator/CoordinateCalculator/main.swift
@@ -6,12 +6,7 @@
 //  Copyright © 2017 Codesquad Inc. All rights reserved.
 //
 
-// test QnA: CLI에서 swift file 실행하는 법을 모르겠어요(예: main.swift나 OutView.swift에 ANSICode.swift 구조체 메쏘드를 구현해서 실행)
-
 import Foundation
 
 print("Hello, World!")
 
-print("\(ANSICode.clear)\(ANSICode.home)")
-
-print("\(ANSICode.text.whiteBright)\(ANSICode.axis.draw())")
