f91c9a94091ff58abda414aad4d56c6984f30c71
ehdrjsdlzzzz <ehdrjsdlzzzz@gmail.com>
2018-07-24 14:19:57 +0900

    검증을 담당하는 객체에게 위임

--- CoordinateCalculator/CoordinateCalculator/InputView.swift
+++ CoordinateCalculator/CoordinateCalculator/InputView.swift
@@ -9,40 +9,53 @@
 import Foundation
 
 struct InputView {
+    
+    struct Validation {
+        private let target: String
+        
+        init(target: String) {
+            self.target = target
+        }
+        
+        func makeValidPoint()-> MyPoint? {
+            if !isValidCoordinateFormat() { return nil }
+            let parsed = parse()
+            guard let x = isContainOnlyDigit(parsed.x), let y = isContainOnlyDigit(parsed.y) else { return nil }
+            return MyPoint(x: x, y: y)
+        }
+        
+        private func isValidCoordinateFormat() -> Bool {
+            if target.first != "(" || target.last != ")" || !target.contains(",") {
+                print("올바르지 않은 입력 형식입니다.")
+                return false
+            }
+            return true
+        }
+        
+        private func parse() -> (x: String?, y: String?) { // only parsing
+            var data = target
+            data = data.replacingOccurrences(of: "(", with: "")
+            data = data.replacingOccurrences(of: ")", with: "")
+            let splited = data.split(separator: ",").map { String($0) }
+            return splited.count == 2 ? (splited[0], splited[1]) : (nil, nil)
+        }
+        
+        private func isContainOnlyDigit(_ element: String?) -> String? {
+            guard let element = element else { return nil }
+            let withoutNegative = element.replacingOccurrences(of: "-", with: "") // if "-4" -> "4"
+            let digitCharacterSet = CharacterSet(charactersIn: "0123456789")
+            if CharacterSet(charactersIn: withoutNegative).isSubset(of: digitCharacterSet) {
+                return element
+            }
+            print("숫자만 입력해주세요.")
+            return nil
+        }
+    }
+    
     static func read() -> MyPoint? {
         print("좌표를 입력하세요.")
         guard let input = readLine() else { return nil }
-        if !isValidCoordinateFormat(input) { return nil }
-        let parsed = parse(input: input)
-        guard let x = isContainOnlyDigit(parsed.x) else { return nil }
-        guard let y = isContainOnlyDigit(parsed.y) else { return nil }
-        return MyPoint(x: x, y: y)
-    }
-    
-    static private func isValidCoordinateFormat(_ format: String) -> Bool {
-        if format.first != "(" || format.last != ")" || !format.contains(",") {
-            print("올바르지 않은 입력 형식입니다.")
-            return false
-        }
-        return true
-    }
-    
-    static private func parse(input:String) -> (x: String?, y: String?) { // only parsing
-        var data = input
-        data = data.replacingOccurrences(of: "(", with: "")
-        data = data.replacingOccurrences(of: ")", with: "")
-        let splited = data.split(separator: ",").map { String($0) }
-        return splited.count == 2 ? (splited[0], splited[1]) : (nil, nil)
-    }
-    
-    static private func isContainOnlyDigit(_ data: String?) -> String? {
-        guard let data = data else { return nil }
-        let withoutNegative = data.replacingOccurrences(of: "-", with: "") // if "-4" -> "4"
-        let digitCharacterSet = CharacterSet(charactersIn: "0123456789")
-        if CharacterSet(charactersIn: withoutNegative).isSubset(of: digitCharacterSet) {
-            return data
-        }
-        print("숫자만 입력해주세요.")
-        return nil
+        guard let myPoint = Validation(target: input).makeValidPoint() else { return nil }
+        return myPoint
     }
 }
