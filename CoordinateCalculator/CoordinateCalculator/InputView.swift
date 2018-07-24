320625408a5512c397fb1282ba19e81842d6e31e
ehdrjsdlzzzz <ehdrjsdlzzzz@gmail.com>
2018-07-24 10:19:56 +0900

    parse와 isValudCoordinateFomrat을 분리

--- CoordinateCalculator/CoordinateCalculator/InputView.swift
+++ CoordinateCalculator/CoordinateCalculator/InputView.swift
@@ -12,30 +12,33 @@ struct InputView {
     static func read() -> MyPoint? {
         print("좌표를 입력하세요.")
         guard let input = readLine() else { return nil }
-        guard let validCoordinate = generateValidCoordinate(parse(input: input)) else {
-            print("올바르지 않은 값의 형식입니다.")
-            return nil
-        }
-        return MyPoint(x: validCoordinate.x, y: validCoordinate.y)
+        if !isValidCoordinateFormat(input) { return nil }
+        let parsed = parse(input: input)
+        guard let x = isContainOnlyDigit(parsed.x) else { return nil }
+        guard let y = isContainOnlyDigit(parsed.y) else { return nil }
+        return MyPoint(x: x, y: y)
     }
+    
+    static private func isValidCoordinateFormat(_ format: String) -> Bool {
+        if format.first != "(" || format.last != "(" || !format.contains(",") {
+            return false
+        }
+        return true
+    }
+    
     static private func parse(input:String) -> (x: String?, y: String?) { // only parsing
         var data = input
-        data = data.replacingOccurrences(of: "(", with: "") // remove "("
-        data = data.replacingOccurrences(of: ")", with: "") // remove ")"
+        // "(" , ")" 제거 - 앞뒤에 있다는 것이 증명이 되었으니
+        data.remove(at: data.startIndex)
+        data.remove(at: data.endIndex)
         let splited = data.split(separator: ",").map {String($0)}
         return splited.count == 2 ? (splited[0], splited[1]) : (nil, nil)
     }
     
-    static private func generateValidCoordinate(_ coordinates: (x: String?, y: String?)) -> (x: String, y:String)? { // check is valid coordinates
-        guard let x = coordinates.x else { return nil }
-        guard let y = coordinates.y else { return nil }
-        return isContainOnlyDigit(x) && isContainOnlyDigit(y) ? (x,y) : nil
-    }
-    
-    static private func isContainOnlyDigit(_ data: String) -> Bool { // check is contain non-digit characters
+    static private func isContainOnlyDigit(_ data: String?) -> String? {
+        guard let data = data else { return nil }
         let withoutNegative = data.replacingOccurrences(of: "-", with: "") // if "-4" -> "4"
         let digitCharacterSet = CharacterSet(charactersIn: "0123456789")
-        return CharacterSet(charactersIn: withoutNegative).isSubset(of: digitCharacterSet)
+        return CharacterSet(charactersIn: withoutNegative).isSubset(of: digitCharacterSet) ? data : nil
     }
-    
 }
