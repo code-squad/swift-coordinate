17c2eae66495fcf86e88841e83946bec67d78db5
oingbong <oingbong@gmail.com>
2018-07-26 17:45:21 +0900

    Divided method in main.swift

--- CoordinateCalculator/CoordinateCalculator/main.swift
+++ CoordinateCalculator/CoordinateCalculator/main.swift
@@ -8,39 +8,44 @@
 
 import Foundation
 
-public func main() {
-    print("좌표를 입력하세요.")
-    let input:String? = readLine()
-    
-    guard let inputValue = input else { return main() }
-    if inputValue.isEmpty {
-        print("값이 비어있습니다. 다시 입력해주세요.")
-        return main()
-    }
-    
-    let coordinates:[MyPoint] = InputView.readInput(input: inputValue)
-    
+// coordinates 개수에 따라 분류하는 함수
+private func selectShape(coordinates:[MyPoint]) -> Bool {
     switch coordinates.count {
     case 1: // point
         let valueX = coordinates[0].valueX
         let valueY = coordinates[0].valueY
-        let point = MyPoint.init(x: valueX, y: valueY)
         OutputView.drawAxis()
+        let point = MyPoint.init(x: valueX, y: valueY)
         OutputView.printCoordinateMyPoint(point: point)
+        return false
     case 2: // line
         let valueX = coordinates[0]
         let valueY = coordinates[1]
-        let point = MyLine.init(pointA: valueX, pointB: valueY)
         OutputView.drawAxis()
+        let point = MyLine.init(pointA: valueX, pointB: valueY)
         OutputView.printCoordinateMyLine(line: point)
+        return false
     default:
         /*
          좌표값 범위를 넘거나 입력할 수 없는 문자가 포함된 경우 빈값을 리턴받아 해당 부분에 들어옵니다.
          또한, 원하는 값 이상으로 좌표가 입력된 경우에도 해당 부분으로 들어옵니다.
          */
-        return main()
+        return true
     }
 }
 
+public func main() -> Bool {
+    let initValue = InputView.readInput()
+    
+    guard let inputValue = InputView.checkEmpty(value: initValue) else { return true }
+    
+    let coordinates:[MyPoint] = InputView.changeShape(value: inputValue)
+    let result = selectShape(coordinates: coordinates)
+    
+    return result
+}
 
-main()
+var play:Bool = true
+while play {
+    play = main()
+}
