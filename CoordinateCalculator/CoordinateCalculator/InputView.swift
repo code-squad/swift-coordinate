2261db27c5f816d5380496b2b1fff67923e1f14a
oingbong <oingbong@gmail.com>
2018-07-26 17:44:55 +0900

    Refactoring InputView.swift

--- CoordinateCalculator/CoordinateCalculator/InputView.swift
+++ CoordinateCalculator/CoordinateCalculator/InputView.swift
@@ -40,45 +40,37 @@ struct InputView {
     }
     
     // 입력 범위 확인하는 함수
-    private static func checkInputRange(elements: MyPoint) -> Bool {
+    private static func isExceedRange(elements: MyPoint) -> Bool {
         let valueX = elements.valueX
         let valueY = elements.valueY
-        
-        var result = false
-        if valueX >= 1 && valueX <= 24 && valueY >= 1 && valueY <= 24 {
-            result = true
+        guard valueX >= 1 && valueX <= 24 && valueY >= 1 && valueY <= 24 else {
+            print("좌표값 범위를 초과하였습니다. 다시 입력해주세요.")
+            return false
         }
-        return result
-    }
-    private static func checkInputRange2(x:Int? , y:Int?) -> Bool? {
-        guard let valueX = x else { return nil }
-        guard let valueY = y else { return nil }
-        
-        var result = false
-        if valueX >= 1 && valueX <= 24 && valueY >= 1 && valueY <= 24 {
-            result = true
-        }
-        return result
+        return true
     }
     
-    // 입력할 수 없는 문자 확인
+    // 입력할 수 없는 문자 확인 함수
     private static func isAllowCharacters(text:String) -> Bool {
         let allowCharacterSet = CharacterSet.init(charactersIn: "1234567890()-,")
         let notAllowCharacter = text.trimmingCharacters(in: allowCharacterSet)
-        return notAllowCharacter.isEmpty ? true : false
+        guard notAllowCharacter.isEmpty else {
+            print("입력할 수 없는 문자가 포함되어 있습니다. 다시 입력해주세요.")
+            return false
+        }
+        return true
     }
     
-    // 입력값 받는 함수
-    public static func readInput(input:String) -> [MyPoint] {
+    // 검사하고
+    // - 기준으로 나누고
+    // MyPoint 반환하고 
+    public static func changeShape(value:String) -> [MyPoint] {
         var results = [MyPoint]()
 
-        guard isAllowCharacters(text: input) else {
-            print("입력할 수 없는 문자가 포함되어 있습니다. 다시 입력해주세요.")
-            return results
-        }
+        guard isAllowCharacters(text: value) else { return results }
         
         // '-' 기준으로 나누기
-        let elements:Array<String> = input.components(separatedBy: "-")
+        let elements:Array<String> = value.components(separatedBy: "-")
         for element in elements {
             /*
              1. 입력값 x,y 로 나누기
@@ -87,16 +79,30 @@ struct InputView {
              */
             let dividedNumbers = extractCoordinate(elements: element)
             let pointValues = changePointValue(from: dividedNumbers)
-            let checkValue = checkInputRange(elements: pointValues)
             // 범위를 초과하는 경우 : results값을 비워주고 빈값을 리턴하여 main.swift 에서 다시 처음부터 시작합니다.
-            guard checkValue else {
+            guard isExceedRange(elements: pointValues) else {
                 results.removeAll()
-                print("좌표값 범위를 초과하였습니다. 다시 입력해주세요.")
                 return results
             }
             results.append(pointValues)
         }
         return results
+    }
+    
+    // 입력값 받는 함수
+    public static func readInput() -> String? {
+        print("좌표를 입력하세요.")
+        return readLine()
+    }
+    
+    // 입력값 비어 있는지 확인하는 함수
+    public static func checkEmpty(value:String?) -> String? {
+        guard let inputValue = value else { return nil }
+        if inputValue.isEmpty {
+            print("값이 비어있습니다. 다시 입력해주세요.")
+            return nil
+        }
+        return inputValue
     }
     
 }
