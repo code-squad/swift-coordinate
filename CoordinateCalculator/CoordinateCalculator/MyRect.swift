e16eda1dc5dc5496c02efbe112610880cac0d0d1
ehdrjsdlzzzz <ehdrjsdlzzzz@gmail.com>
2018-07-31 13:46:18 +0900

    MyRect Over1DFigureProtocol 준수

--- CoordinateCalculator/CoordinateCalculator/MyRect.swift
+++ CoordinateCalculator/CoordinateCalculator/MyRect.swift
@@ -8,12 +8,28 @@
 
 import Foundation
 
-struct MyRect {
+struct MyRect: Over1DFigureProtocol {
     private var leftTop: MyPoint
     private var rightBottom: MyPoint
+    private var size: CGSize
+    
+    var descriptionPrefix: String {
+        return "사각형의 넓이는"
+    }
+    
+    var valueOfFigure: Double {
+        return Double(size.width * size.height)
+    }
+    
+    var points: [MyPoint] {
+        let rightTop = MyPoint(x: leftTop.x + Int(size.width), y: leftTop.y)
+        let leftBottom = MyPoint(x: leftTop.x, y: leftTop.y - Int(size.height))
+        return [leftTop, rightTop, leftBottom, rightBottom]
+    }
     
     init(origin: MyPoint, size: CGSize) {
         self.leftTop = origin
-        self.rightBottom = MyPoint(x: leftTop.x + Int(size.width), y: leftTop.y + Int(size.height))
+        self.rightBottom = MyPoint(x: leftTop.x + Int(size.width), y: leftTop.y - Int(size.height))
+        self.size = size
     }
 }
