e2a3ba011bac9978756d3eb78e5c2cfacc5c4fa4
oingbong <oingbong@gmail.com>
2018-07-31 13:08:53 +0900

    Add FigureProtocol

---
+++ CoordinateCalculator/CoordinateCalculator/FigureProtocol.swift
@@ -0,0 +1,13 @@
+//
+//  FigureProtocol.swift
+//  CoordinateCalculator
+//
+//  Created by oingbong on 2018. 7. 31..
+//  Copyright © 2018년 Codesquad Inc. All rights reserved.
+//
+
+import Foundation
+
+protocol FigureProtocol {
+    func coordinates() -> String?
+}
