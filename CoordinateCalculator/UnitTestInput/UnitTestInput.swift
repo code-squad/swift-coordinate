0f413b29de516335a56495675fd318e1b7990124
popsmile <popsmile7@gmail.com>
2018-10-05 15:04:46 +0900

    입력값을 처리하는 부분을 단위테스트 하기위해  UnitTestInput을 추가한다

---
+++ CoordinateCalculator/UnitTestInput/UnitTestInput.swift
@@ -0,0 +1,27 @@
+//
+//  UnitTestInput.swift
+//  UnitTestInput
+//
+//  Created by 윤지영 on 05/10/2018.
+//  Copyright © 2018 Codesquad Inc. All rights reserved.
+//
+
+import XCTest
+
+class UnitTestInput: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+
+}
