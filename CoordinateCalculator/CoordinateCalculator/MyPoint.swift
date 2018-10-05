70dee18c7f0198c9cc98c511576b21d8956def2d
popsmile <popsmile7@gmail.com>
2018-10-05 15:03:38 +0900

    좌표값 X, Y 정보를 처리하기 위한 MyPoint 구조체를 생성한다

---
+++ CoordinateCalculator/CoordinateCalculator/MyPoint.swift
@@ -0,0 +1,14 @@
+//
+//  MyPoint.swift
+//  CoordinateCalculator
+//
+//  Created by 윤지영 on 04/10/2018.
+//  Copyright © 2018 Codesquad Inc. All rights reserved.
+//
+
+import Foundation
+
+struct MyPoint {
+    var x = 0
+    var y = 0
+}
