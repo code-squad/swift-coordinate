91e79a95192da43a37b61023e9bc29ff9b6b9ae0
popsmile <popsmile7@gmail.com>
2018-10-08 15:54:38 +0900

    drawPoint() 함수를 수정한다
    drawPoint() 안으로 drawAxis() 함수를 포함시킨다. 좌표를 표시해줄 marker를 상수로 선언한다.

--- CoordinateCalculator/CoordinateCalculator/OutputView.swift
+++ CoordinateCalculator/CoordinateCalculator/OutputView.swift
@@ -10,8 +10,9 @@ import Foundation
 
 struct OutputView {
     private static let originOfCoordinates = MyPoint(x:3,y:25)
+    private static let marker = "🔵"
     
-    static func drawAxis() {
+    private static func drawAxis() {
         print("\(ANSICode.clear)\(ANSICode.home)")
         print("\(ANSICode.text.whiteBright)\(ANSICode.axis.draw())")
     }
@@ -21,8 +22,8 @@ struct OutputView {
     }
     
     static func drawPoint(point:MyPoint) {
-        print("\(ANSICode.text.white)")
-        print("\(ANSICode.cursor.move(row:originOfCoordinates.y - point.y, col:originOfCoordinates.x + point.x*2))@")
+        drawAxis()
+        print("\(ANSICode.cursor.move(row:originOfCoordinates.y - point.y, col:originOfCoordinates.x + point.x*2))\(marker)")
         moveCursorToEnd()
     }
     
