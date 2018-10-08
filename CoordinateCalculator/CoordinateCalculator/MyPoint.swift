62888d2e24315e4e8de02c4e5f45d1115517b409
popsmile <popsmile7@gmail.com>
2018-10-08 15:56:25 +0900

    MyPoint에 init 메소드를 추가한다
    x,y값이 int나 string일 때 모두 초기화가 가능하도록 두 가지 init을 둔다. string으로 초기화시 Int로의 변환가능성과 범위를 체크해준다.

--- CoordinateCalculator/CoordinateCalculator/MyPoint.swift
+++ CoordinateCalculator/CoordinateCalculator/MyPoint.swift
@@ -11,4 +11,17 @@ import Foundation
 struct MyPoint {
     var x = 0
     var y = 0
+    
+    init(x:Int, y:Int) {
+        self.x = x
+        self.y = y
+    }
+    
+    init(x:String, y:String) {
+        guard let x = Int(x) else { return }
+        guard let y = Int(y) else { return }
+        if (x > 24 || y > 24) { return }
+        self.x = x
+        self.y = y
+    }
 }
