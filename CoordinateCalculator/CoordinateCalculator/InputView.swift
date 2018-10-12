698f599f0cd44dd689215c243c0f5e244fc96a59
hngfu <djsl1018@naver.com>
2018-10-12 17:30:18 +0900

    [InputView] validCheck() 생성

--- CoordinateCalculator/CoordinateCalculator/InputView.swift
+++ CoordinateCalculator/CoordinateCalculator/InputView.swift
@@ -9,18 +9,23 @@
 import Foundation
 
 struct InputView {
-    func readInput() -> MyPoint {
-        print("좌표를 입력하세요.")
-        let xyPoint = getXYPoint(readLine()!)
-        
-        return xyPoint
+    static func readInput() -> MyPoint {
+        while true {
+            print("좌표를 입력하세요. ex- (10,18) //단, 숫자의 최대 크기는 24")
+            let xyPoint = getXYPoint(readLine() ?? "다시 입력해 주세요.")
+            guard validCheck(xyPoint.x) && validCheck(xyPoint.y) else {continue}
+            return xyPoint
+        }
     }
 
-    private func getXYPoint(_ input:String) -> MyPoint {
+    static private func getXYPoint(_ input:String) -> MyPoint {
         let xPoint = input.split(separator: "(")[0].split(separator: ",")[0]
         let yPoint = input.split(separator: ",")[1].split(separator: ")")[0]
         let xyPoint = MyPoint(x:Int(xPoint) ?? -1, y:Int(yPoint) ?? -1)
         return xyPoint
     }
     
+    static private func validCheck(_ num:Int) -> Bool {
+        return num >= 0 && num <= 24
+    }
 }
--- CoordinateCalculator/CoordinateCalculator/main.swift
+++ CoordinateCalculator/CoordinateCalculator/main.swift
@@ -8,5 +8,5 @@
 
 import Foundation
 
-
+print(InputView.readInput())
 
