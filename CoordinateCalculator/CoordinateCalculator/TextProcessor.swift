57e17312516551ad8535dc8c60e973497b47499f
popsmile <popsmile7@gmail.com>
2018-10-12 15:19:57 +0900

    InputView의 역할을 축소하고, 문자열을 처리하는 TextProcessor 구조체를 추가한다

---
+++ CoordinateCalculator/CoordinateCalculator/TextProcessor.swift
@@ -0,0 +1,27 @@
+//
+//  TextProcessor.swift
+//  CoordinateCalculator
+//
+//  Created by 윤지영 on 12/10/2018.
+//  Copyright © 2018 Codesquad Inc. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+    func splitByComma() -> [String] {
+        return self.split(separator:",").map({String($0)})
+    }
+    func removeBothFirstAndLast() -> String {
+        return String(self.dropFirst().dropLast())
+    }
+}
+
+struct TextProcessor {
+    static func extractXandY(fromCoordinate coordinate:String) -> (x:Int, y:Int){
+        let xy : [String] = coordinate.removeBothFirstAndLast().splitByComma()
+        let x = Int(xy.first ?? "0") ?? 0
+        let y = Int(xy.last ?? "0") ?? 0
+        return (x, y)
+    }
+}
---
+++ CoordinateCalculator/UnitTestCoordinateCalculator/UnitTestTextProcessor.swift
@@ -0,0 +1,52 @@
+//
+//  UnitTestMyPoint.swift
+//  UnitTestInput
+//
+//  Created by 윤지영 on 08/10/2018.
+//  Copyright © 2018 Codesquad Inc. All rights reserved.
+//
+
+import XCTest
+
+class UnitTestTextProcessor: XCTestCase {
+    let coordinateString = "(10,9)"
+    let coordinateX = 10
+    let coordinateY = 9
+    
+    let noValue = ""
+    let incompleteValue = "(,3"
+    let defaultCoordinateValue = 0
+    
+    let leftIncompleteValue = "(2,)"
+    let leftIncompleteCoordinateValue = 2
+    let rightIncompleteValue = "(,5)"
+    let rightIncompleteCoordinateValue = 5
+
+    override func setUp() {}
+    override func tearDown() {}
+    
+    func test_extractXandY_whenTextIsAllowedForm() {
+        let coordiante = TextProcessor.extractXandY(fromCoordinate: coordinateString)
+        XCTAssertTrue(coordiante.x==coordinateX && coordiante.y==coordinateY)
+    }
+    
+    func test_extractXandY_whenTextHasNoValue() {
+        let coordiante = TextProcessor.extractXandY(fromCoordinate: noValue)
+        XCTAssertTrue(coordiante.x==defaultCoordinateValue && coordiante.y==defaultCoordinateValue)
+    }
+    
+    func test_extractXandY_whenTextIsIncompleteForm() {
+        let coordiante = TextProcessor.extractXandY(fromCoordinate: incompleteValue)
+        XCTAssertTrue(coordiante.x==defaultCoordinateValue && coordiante.y==defaultCoordinateValue)
+    }
+    
+    func test_extractXandY_whenTextIsLeftIncompleteForm() {
+        let coordiante = TextProcessor.extractXandY(fromCoordinate: leftIncompleteValue)
+        XCTAssertTrue(coordiante.x==leftIncompleteCoordinateValue && coordiante.y==leftIncompleteCoordinateValue)
+    }
+    
+    func test_extractXandY_whenTextIsRightIncompleteForm() {
+        let coordiante = TextProcessor.extractXandY(fromCoordinate: rightIncompleteValue)
+        XCTAssertTrue(coordiante.x==rightIncompleteCoordinateValue && coordiante.y==rightIncompleteCoordinateValue)
+    }
+}
