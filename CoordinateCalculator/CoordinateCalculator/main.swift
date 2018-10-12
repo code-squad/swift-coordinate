d6e23e4f624cdec5917e985bdc2e86c915ca273c
popsmile <popsmile7@gmail.com>
2018-10-12 19:50:21 +0900

    정상적으로 좌표가 출력되기 전까지 프로그램이 계속 재실행되도록 수정한다

--- CoordinateCalculator/CoordinateCalculator/main.swift
+++ CoordinateCalculator/CoordinateCalculator/main.swift
@@ -9,9 +9,16 @@
 import Foundation
 
 struct CoordinateCalculator {
+    static var runAgain = true
+    
     static func run() {
         let coordinate = InputView.readInput()
-        let coordinatesValidation = TextValidator(text:coordinate).checkTextError()
+        let textValidator = TextValidator(text:coordinate)
+        guard textValidator.hasNoInvalidCharacter() else {
+            print("\(TextValidation.invalidForm.rawValue) \(TextValidation.outOfRangeInt.rawValue)")
+            return
+        }
+        let coordinatesValidation = textValidator.checkTextError()
         guard coordinatesValidation == .success else {
             print(coordinatesValidation.rawValue)
             return
@@ -19,7 +26,10 @@ struct CoordinateCalculator {
         let coordinates = TextProcessor.extractXY(from: coordinate)
         let myPoint = MyPoint(x:coordinates.x, y:coordinates.y)
         OutputView.drawPoint(point: myPoint)
+        runAgain = false
     }
 }
 
-CoordinateCalculator.run()
+while(CoordinateCalculator.runAgain) {
+    CoordinateCalculator.run()
+}
