af0b190abaf6b64041b2699ed46832eaac7bf535
popsmile <popsmile7@gmail.com>
2018-10-12 19:48:32 +0900

    MyLine 구조체를 추가한다

---
+++ CoordinateCalculator/CoordinateCalculator/MyLine.swift
@@ -0,0 +1,14 @@
+//
+//  MyLine.swift
+//  CoordinateCalculator
+//
+//  Created by 윤지영 on 12/10/2018.
+//  Copyright © 2018 Codesquad Inc. All rights reserved.
+//
+
+import Foundation
+
+struct MyLine {
+    private var pointA = MyPoint(x: 0, y: 0)
+    private var pointB = MyPoint(x: 0, y: 0)
+}
