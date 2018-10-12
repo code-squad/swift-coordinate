1dd5519b8a0bc75cbf55eb892fd2178b36f7704d
hngfu <djsl1018@naver.com>
2018-10-12 15:25:47 +0900

    [MyPoint] 구조체 생성

---
+++ CoordinateCalculator/CoordinateCalculator/MyPoint.swift
@@ -0,0 +1,13 @@
+//
+//  MyPoint.swift
+//  CoordinateCalculator
+//
+//  Created by 조재흥 on 2018. 10. 12..
+//  Copyright © 2018년 Codesquad Inc. All rights reserved.
+//
+
+import Foundation
+
+struct MyPoint {
+    
+}
