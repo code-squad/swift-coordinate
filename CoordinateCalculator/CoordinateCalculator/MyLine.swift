b5c0d578e63233fc12a7448e3d13d02e061454ba
popsmile <popsmile7@gmail.com>
2018-10-13 18:03:20 +0900

    MyLine 구조체를 생성한다
    입력값에서 하이픈으로 좌표값을 나누는 부분과 MyLine을 출력하는 메소드를 추가한다.

--- CoordinateCalculator/CoordinateCalculator/MyLine.swift
+++ CoordinateCalculator/CoordinateCalculator/MyLine.swift
@@ -9,6 +9,11 @@
 import Foundation
 
 struct MyLine {
-    private var pointA = MyPoint(x: 0, y: 0)
-    private var pointB = MyPoint(x: 0, y: 0)
+    private(set) var pointA = MyPoint(x: 0, y: 0)
+    private(set) var pointB = MyPoint(x: 0, y: 0)
+    
+    init(pointA:MyPoint, pointB:MyPoint) {
+        self.pointA = pointA
+        self.pointB = pointB
+    }
 }
--- CoordinateCalculator/CoordinateCalculator/OutputView.swift
+++ CoordinateCalculator/CoordinateCalculator/OutputView.swift
@@ -11,6 +11,7 @@ import Foundation
 struct OutputView {
     private static let originOfX = 3
     private static let originOfY = 25
+    private static let distanceMessage = "두 점 사이의 거리는 "
     private static let marker = "🔵"
     
     private static func drawAxis() {
@@ -25,6 +26,19 @@ struct OutputView {
     static func drawPoint(point:MyPoint) {
         drawAxis()
         print("\(ANSICode.cursor.move(row: originOfY - point.y, col: originOfX + point.x * 2))\(marker)")
+        moveCursorToEnd()
+    }
+    
+    private static func calculateDistance(between pointA:MyPoint, and pointB:MyPoint) {
+        let distance = sqrt(pow(Double(pointA.x-pointB.x), 2) + pow(Double(pointA.y-pointB.y), 2))
+        print("\(distanceMessage)\(distance)")
+    }
+    
+    static func drawLine(line:MyLine) {
+        drawAxis()
+        drawPoint(point: line.pointA)
+        drawPoint(point: line.pointB)
+        calculateDistance(between: line.pointA, and: line.pointB)
         moveCursorToEnd()
     }
 }
