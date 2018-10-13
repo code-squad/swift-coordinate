7879a0da5755911a8b70f313e16931656b9907be
popsmile <popsmile7@gmail.com>
2018-10-13 18:03:45 +0900

    MyPoint, MyLine의 상위 프로토콜 Shape을 생성한다

---
+++ CoordinateCalculator/CoordinateCalculator/Shape.swift
@@ -0,0 +1,13 @@
+//
+//  Shape.swift
+//  CoordinateCalculator
+//
+//  Created by 윤지영 on 13/10/2018.
+//  Copyright © 2018 Codesquad Inc. All rights reserved.
+//
+
+import Foundation
+
+protocol Shape {
+    var points: [MyPoint] { get }
+}
