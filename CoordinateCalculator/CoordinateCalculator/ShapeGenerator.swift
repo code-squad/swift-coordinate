693e8b44e303dab4a2a856c75080d8ca43e40929
popsmile <popsmile7@gmail.com>
2018-10-16 16:40:00 +0900

    MyRect 관련 코드 중 불필요한 부분을 삭제한다

--- CoordinateCalculator/CoordinateCalculator/ShapeGenerator.swift
+++ CoordinateCalculator/CoordinateCalculator/ShapeGenerator.swift
@@ -11,6 +11,7 @@ import Foundation
 public enum ShapeValidation: String {
     case notSatisfyTriangleInequality = "삼각형이 성립되지않는 좌표입니다."
     case notSatisfyRectangle = "직사각형이 성립되지않는 좌표입니다."
+    case notSatisfyRectangleSupported = "지원하지 않는 직사각형입니다. x축과 y축에 모두 평행한 직사각형만 지원합니다."
     case outOfRangeMyPoint = "최대 3개의 좌표까지 입력 가능합니다."
 }
 
@@ -43,7 +44,7 @@ struct ShapeGenerator {
         return sideLengths[0] + sideLengths[1] > sideLengths[2]
     }
     
-    private static func makeRectangleLengthSetArray(by points: [MyPoint]) -> [Double] {
+    private static func makeRectangleLengthSet(by points: [MyPoint]) -> Set<Double> {
         var lengthSet = Set<Double>()
         lengthSet.insert(calculateDistance(between: points[0], and: points[1]))
         lengthSet.insert(calculateDistance(between: points[0], and: points[2]))
@@ -51,31 +52,33 @@ struct ShapeGenerator {
         lengthSet.insert(calculateDistance(between: points[1], and: points[2]))
         lengthSet.insert(calculateDistance(between: points[1], and: points[3]))
         lengthSet.insert(calculateDistance(between: points[2], and: points[3]))
-        return lengthSet.sorted()
+        return lengthSet
     }
     
     private static func satisfyRegtangle(of points: [MyPoint]) -> Bool {
-        let lengthSet = makeRectangleLengthSetArray(by: points)
+        let lengthSet = makeRectangleLengthSet(by: points)
         return (lengthSet.count == 2 || lengthSet.count == 3)
+    }
+    
+    private static func satisfyRegtangleSupported(of points: [MyPoint]) -> Bool {
+        var xSet = Set<Int>(), ySet = Set<Int>()
+        for point in points {
+            xSet.insert(point.x)
+            ySet.insert(point.y)
+        }
+        return ((xSet.count == 1 || xSet.count == 2 ) && (ySet.count == 1 || ySet.count == 2))
     }
     
     private static func sortXbyASC(first: MyPoint, second: MyPoint) -> Bool {
         return first.x < second.x
     }
     
-    private static func sortYbyASC(first: MyPoint, second: MyPoint) -> Bool {
-        return first.y < second.y
-    }
-    
     private static func searchRectProperty(of points: [MyPoint]) -> (myPoint: MyPoint, cgSize: CGSize) {
         var points = points
         points.sort(by: sortXbyASC)
-        let origin = sortYbyASC(first: points[0], second: points[1]) ? points[1] : points[0]
-        let leftBottom = sortYbyASC(first: points[0], second: points[1]) ? points[0] : points[1]
-        let rightTop = sortYbyASC(first: points[2], second: points[3]) ? points[3] : points[2]
-        let width = calculateDistance(between: origin, and: rightTop)
-        let height = calculateDistance(between: origin, and: leftBottom)
-        return (origin, CGSize(width: width, height: height))
+        let origin = points[0].y < points[1].y ? points[1] : points[0]
+        let rightBottom = points[2].y < points[3].y ? points[2] : points[3]
+        return (origin, CGSize(width: rightBottom.x-origin.x, height: origin.y-rightBottom.y))
     }
     
     static func generateShape(by points: [MyPoint]) -> Shape? {
@@ -95,6 +98,10 @@ struct ShapeGenerator {
         case 4:
             guard satisfyRegtangle(of: points) else {
                 OutputView.printMessage(of: .notSatisfyRectangle)
+                return nil
+            }
+            guard satisfyRegtangleSupported(of: points) else {
+                OutputView.printMessage(of: .notSatisfyRectangleSupported)
                 return nil
             }
             let rectProperty = searchRectProperty(of: points)
