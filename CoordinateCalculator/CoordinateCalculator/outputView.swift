f8535020efadd8da5494f5c4c7723f4747e01f9f
김장수 <jangsookim.me@gmail.com>
2018-10-16 17:29:50 +0900

    제대로 출력되도록 수정

--- CoordinateCalculator/CoordinateCalculator/outputView.swift
+++ CoordinateCalculator/CoordinateCalculator/outputView.swift
@@ -10,6 +10,7 @@ import Foundation
 
 struct OutputView {
     static func drawAxis() {
+        print("\(ANSICode.clear)\(ANSICode.home)")
         print("\(ANSICode.text.whiteBright)\(ANSICode.axis.draw())")
     }
 }
