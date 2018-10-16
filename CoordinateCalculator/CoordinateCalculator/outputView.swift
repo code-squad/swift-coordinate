43718cff90fa1fce1640a04eb8273006d2f62acf
김장수 <jangsookim.me@gmail.com>
2018-10-16 15:03:51 +0900

    OutputView 구조체 & drawAxis 함수 추가

---
+++ CoordinateCalculator/CoordinateCalculator/outputView.swift
@@ -0,0 +1,15 @@
+//
+//  outputView.swift
+//  CoordinateCalculator
+//
+//  Created by 김장수 on 2018. 10. 16..
+//  Copyright © 2018년 Codesquad Inc. All rights reserved.
+//
+
+import Foundation
+
+struct OutputView {
+    public func drawAxis() -> String {
+        return ""
+    }
+}
