0ce8ab42fcb40b2889e64f139eed018ba6222800
hngfu <djsl1018@naver.com>
2018-10-18 16:25:21 +0900

    [inputView] readXY(), readPoint(), repeatRead()로 분해
    기존에 readInput()을 여러개의 메소드로 분리하였습니다.

--- CoordinateCalculator/CoordinateCalculator/InputView.swift
+++ CoordinateCalculator/CoordinateCalculator/InputView.swift
@@ -11,29 +11,35 @@ import Foundation
 struct InputView {
     static private let maxValue = ANSICode.axis.AxisLimit
     
-    static func readInput() -> MyPoint {
-        
-        
-        
-        let typingMention = "좌표를 입력하세요. //단, 숫자의 최대 크기는 24"
-        let retypeMention = "숫자의 최대 크기는 24입니다. 입력값을 확인하시고 다시 입력 해주세요."
-        var isRetype = false
-        while true {
-            isRetype ? print(retypeMention) : print(typingMention)
-            let xyPoint = getXYPoint(readLine() ?? "")
-            guard validCheck(xyPoint.x) && validCheck(xyPoint.y) else {isRetype = true; continue}
-            return xyPoint
-        }
+    static private func readInput(ment:String) -> String {
+        print(ment)
+        return readLine() ?? ""
     }
     
-    static private func getXYPoint(_ input:String) -> MyPoint {
-        let xPoint = input.split(separator: "(")[0].split(separator: ",")[0]
-        let yPoint = input.split(separator: ",")[1].split(separator: ")")[0]
+    static private func readPoint(isRepeat:Bool) -> MyPoint {
+        let typingMent = "좌표를 입력하세요. //단, 숫자의 최대 크기는 24"
+        let retypeMent = "숫자의 최대 크기는 24입니다. 입력값을 확인하시고 다시 입력 해주세요."
+        let point = isRepeat == true ? readInput(ment: retypeMent) : readInput(ment: typingMent)
+        return readXY(point)
+    }
+    
+    static private func readXY(_ point:String) -> MyPoint {
+        let xPoint = point.split(separator: "(")[0].split(separator: ",")[0]
+        let yPoint = point.split(separator: ",")[1].split(separator: ")")[0]
         let xyPoint = MyPoint(x:Int(xPoint) ?? maxValue + 1, y:Int(yPoint) ?? maxValue + 1)
         return xyPoint
     }
     
-    static private func validCheck(_ num:Int) -> Bool {
-        return num <= maxValue
+    static func repeatRead() -> MyPoint {
+        var isRepeat = false
+        while true {
+            let point = readPoint(isRepeat: isRepeat)
+            guard validCheck(point: point) else {isRepeat = true ; continue}
+            return point
+        }
+    }
+    
+    static private func validCheck(point:MyPoint) -> Bool {
+        return point.x <= maxValue && point.y <= maxValue
     }
 }
