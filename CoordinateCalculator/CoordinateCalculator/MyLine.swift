bf82d965bab92312866fdfe9617bed3e951da365
hngfu <djsl1018@naver.com>
2018-10-18 17:41:35 +0900

    [MyLine] 구조체 생성

---
+++ CoordinateCalculator/CoordinateCalculator/MyLine.swift
@@ -0,0 +1,14 @@
+//
+//  MyLine.swift
+//  CoordinateCalculator
+//
+//  Created by 조재흥 on 2018. 10. 18..
+//  Copyright © 2018년 Codesquad Inc. All rights reserved.
+//
+
+import Foundation
+
+struct MyLine {
+    private(set) var pointA : MyPoint
+    private(set) var pointB : MyPoint
+}
