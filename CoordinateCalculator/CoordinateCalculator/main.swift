a6e05130c6118a18336b53097613895fab55ab82
Dongmin.yoon <ehdals8965@naver.com>
2018-10-19 15:49:04 +0900

    2단계 요구사항 구현

--- CoordinateCalculator/CoordinateCalculator/main.swift
+++ CoordinateCalculator/CoordinateCalculator/main.swift
@@ -11,10 +11,17 @@
 import Foundation
 
 func main(){
-    let outputView: OutputView = OutputView()
+    let inputView = InputView()
+    let outputView = OutputView()
+    var point : MyPoint
+     
+    repeat{
+        point = inputView.splitXandYlocation(location: inputView.readInput(message: "좌표를 입력해주세요."))
+    }while CheckUserInput.IsOverFlowNumber(position: point.xPosition) || CheckUserInput.IsOverFlowNumber(position: point.yPosition)
     
     outputView.clearBackground()
     outputView.drawAxis()
+    outputView.drawPoint(point: point)
 }
 
 main()
