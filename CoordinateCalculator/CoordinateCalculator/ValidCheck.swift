3a75c070da3e7e88aa5fb0dc56d9f95d40a3fbc8
hngfu <djsl1018@naver.com>
2018-10-19 15:26:40 +0900

    [ValidCheck] characterSet 생성

--- CoordinateCalculator/CoordinateCalculator/ValidCheck.swift
+++ CoordinateCalculator/CoordinateCalculator/ValidCheck.swift
@@ -10,8 +10,11 @@ import Foundation
 
 struct ValidCheck {
     static private let maxValue = ANSICode.axis.AxisLimit
+    static private let characterSet = ["(",")","-","0","1","2","3","4","5","6","7","8","9"]
     
     static func lengthCheck(point:MyPoint) -> Bool {
         return point.x <= maxValue && point.y <= maxValue
     }
+    
+    
 }
