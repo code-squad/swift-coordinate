4eb96121b9ed2db65938628cefbbeab92d7bedf7
김장수 <jangsookim.me@gmail.com>
2018-10-20 02:21:15 +0900

    구조체 분리 후, main파일 완성

--- CoordinateCalculator/CoordinateCalculator/main.swift
+++ CoordinateCalculator/CoordinateCalculator/main.swift
@@ -8,16 +8,25 @@
 
 import Foundation
 
-func main() -> Bool {
-    let inputPoint = InputView(mention: "좌표를 입력하세요.").readInput()
+func main() {
+    let inputValue = InputView(mention: "좌표를 입력하세요.").readInput()
+    var convertedPoint = ConvertInput(initialValue: inputValue).convertToPoint()
     
-//    let outputView = OutputView(point: myPoint)
-//    outputView.drawAxis()   // 출력을 책임지는 구조체의 좌표를 그리는 메소드
-//    outputView.markPoint()  // 출력을 책임지는 구조체의 원하는 포인트를 찍는 메소드
+    // 
+    while !Validity(point: convertedPoint).isRight() {
+        let nextValue = InputView(mention: "올바른 좌표를 입력하세요.").readInput()
+        let nextPoint = ConvertInput(initialValue: nextValue).convertToPoint()
+        convertedPoint = nextPoint
+        if Validity(point: nextPoint).isRight() {
+            break
+        }
+    }
     
-    return true
+    let myPoint = MyPoint(point: convertedPoint).delievePoint()
+    
+    let outputView = OutputView(point: myPoint)
+    outputView.drawAxis()   // 출력을 책임지는 구조체의 좌표를 그리는 메소드
+    outputView.markPoint()  // 출력을 책임지는 구조체의 원하는 포인트를 찍는 메소드
 }
 
-while(true) {
-    if !main() {break}
-}
+main()
