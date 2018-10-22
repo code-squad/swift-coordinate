fef14aef3180dc036d006392edbbfd343dd137a5
Dongmin.yoon <ehdals8965@naver.com>
2018-10-22 16:21:30 +0900

    메소드 이름 명확하게 변경

--- CoordinateCalculator/CoordinateCalculator/CheckUserInput.swift
+++ CoordinateCalculator/CoordinateCalculator/CheckUserInput.swift
@@ -10,7 +10,7 @@ import Foundation
 
 struct CheckUserInput {
     // 유저의 입력이 범위를 초과한 경우 ENUM 타입 리턴
-    static func IsOverFlowNumber(notNilNumber1: Int, notNilNumber2: Int) -> StateInput{
+    static func IsOverFlowNumber(notNilNumber1: Int, notNilNumber2: Int) -> ErrorState{
         guard notNilNumber1 > 24 || notNilNumber1 < 0 && notNilNumber2 > 24 || notNilNumber2 < 0 else {
             return .rightInput
         }
@@ -18,7 +18,7 @@ struct CheckUserInput {
     }
     
     // 유저의 입력의 상태를 검사하여 그에 맞는 ENUM 상태 리턴
-    static func returnInputState(positionX: String, positionY: String) -> StateInput{
+    static func checkErrorState(positionX: String, positionY: String) -> ErrorState{
         guard Int(positionX) == nil || Int(positionY) == nil else {
             return CheckUserInput.IsOverFlowNumber(notNilNumber1: Int(positionX)!, notNilNumber2: Int(positionY)!)
         }
--- CoordinateCalculator/CoordinateCalculator/main.swift
+++ CoordinateCalculator/CoordinateCalculator/main.swift
@@ -10,7 +10,7 @@
 
 import Foundation
 
-enum StateInput {
+enum ErrorState {
     case nilNumber
     case overFlowNumber
     case rightInput
@@ -20,11 +20,11 @@ func main(){
     let inputView = InputView()
     let outputView = OutputView()
     var inputFromUser : (positionX: String, positionY: String) = ("", "")
-    var stateCurrent : StateInput = .nilNumber
+    var stateCurrent : ErrorState = .nilNumber
     
     while stateCurrent == .nilNumber || stateCurrent == .overFlowNumber {
         inputFromUser = inputView.splitXandYlocation(input: inputView.readInput(message: "좌표를 입력해주세요."))
-        stateCurrent = CheckUserInput.returnInputState(positionX: inputFromUser.positionX, positionY: inputFromUser.positionY)
+        stateCurrent = CheckUserInput.checkErrorState(positionX: inputFromUser.positionX, positionY: inputFromUser.positionY)
         if stateCurrent == .nilNumber{ print("올바른 숫자를 입력하시오.") }
         else if stateCurrent == .overFlowNumber { print("입력범위를 초과하였습니다.") }
     }
