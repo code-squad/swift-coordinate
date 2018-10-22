a7f4d5dc5a2ac9dd1fd26bf52014d625fd742245
Dongmin.yoon <ehdals8965@naver.com>
2018-10-23 00:32:25 +0900

    MyPoint()객체를 생성해주는 ShapeCreator구조체 생성

--- CoordinateCalculator/CoordinateCalculator/main.swift
+++ CoordinateCalculator/CoordinateCalculator/main.swift
@@ -19,19 +19,19 @@ enum ErrorState {
 func main(){
     let inputView = InputView()
     let outputView = OutputView()
-    var inputFromUser : (positionX: String, positionY: String) = ("", "")
-    var stateCurrent : ErrorState = .nilNumber
+    var inputUser : String
+    var xyPosition : (positionX: String, positionY: String) = ("", "")
+    var stateError : ErrorState = .nilNumber
     
-    while stateCurrent == .nilNumber || stateCurrent == .overFlowNumber {
-        inputFromUser = inputView.splitXandYlocation(input: inputView.readInput(message: "좌표를 입력해주세요."))
-        stateCurrent = CheckUserInput.checkErrorState(positionX: inputFromUser.positionX, positionY: inputFromUser.positionY)
-        if stateCurrent == .nilNumber{ print("올바른 숫자를 입력하시오.") }
-        else if stateCurrent == .overFlowNumber { print("입력범위를 초과하였습니다.") }
+    while stateError == .nilNumber || stateError == .overFlowNumber {
+        inputUser = inputView.readInput(message: "좌표를 입력해주세요.")
+        xyPosition = inputView.splitXandYlocation(input: inputUser)
+        stateError = CheckUserInput.checkErrorState(positionX: xyPosition.positionX, positionY: xyPosition.positionY)
+        if stateError == .nilNumber{ print("올바른 숫자를 입력하시오.") }
+        else if stateError == .overFlowNumber { print("입력범위를 초과하였습니다.") }
     }
-    guard let positionX = Int(inputFromUser.positionX) else { return }
-    guard let positionY = Int(inputFromUser.positionY) else { return }
-    let point : MyPoint = MyPoint(xPosition: positionX, yPosition: positionY)
-    
+    guard let point = ShapeCreator.createPoint(xPosition: xyPosition.positionX, yPosition: xyPosition.positionY) else { return }
+
     outputView.clearBackground()
     outputView.drawAxis()
     outputView.drawPoint(point: point)
