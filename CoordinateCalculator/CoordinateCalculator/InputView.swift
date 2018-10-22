16944032f5996d8c96c78555ac4adbfb38b8f84b
Dongmin.yoon <ehdals8965@naver.com>
2018-10-22 16:15:53 +0900

    splitXandYlocation()기능 분리

--- CoordinateCalculator/CoordinateCalculator/InputView.swift
+++ CoordinateCalculator/CoordinateCalculator/InputView.swift
@@ -10,13 +10,13 @@ import Foundation
 
 struct InputView {
     // X,Y 좌표로 나누어 줌
-    func splitXandYlocation() -> (String, String){
-        var splitUserInput : [String] = readInput(message: "좌표를 입력해주세요").components(separatedBy: [",", ")", "("])
+    func splitXandYlocation(input: String) -> (String, String){
+        var splitUserInput : [String] = input.components(separatedBy: [",", ")", "("])
         return (splitUserInput[1], splitUserInput[2])
     }
     
     // 사용자의 입력을 받음
-    private func readInput(message: String) -> String {
+    func readInput(message: String) -> String {
         print(message)
         guard let input = readLine() else { return "" }
         return input
---
+++ CoordinateCalculator/CoordinateCalculator/possibleCharacterSet.swift
@@ -0,0 +1,11 @@
+//
+//  possibleCharacterSet.swift
+//  CoordinateCalculator
+//
+//  Created by 윤동민 on 2018. 10. 22..
+//  Copyright © 2018년 Codesquad Inc. All rights reserved.
+//
+
+import Foundation
+
+
