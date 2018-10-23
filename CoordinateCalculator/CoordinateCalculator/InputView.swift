d236b81b4e67d12bff56734f01114a2cd5223990
Jun ho Lee <june20516@gmail.com>
2018-10-23 14:27:13 +0900

    InputView 생성
    - readInput : 값을 입력받아 coordinateForm으로 리턴하는 함수
    - receiveInput : 멘트를 출력하고 readLine으로 값을 입력받는 함수
    - checkCoordinateForm : 값이 좌표 표현에 맞는지 확인하고 가공하여 반환, 틀린 값은 -1 반환
    
    - checkCoordinateValue : 입력값이 음수 인 경우 메인 함수에서 흐름제어

---
+++ CoordinateCalculator/CoordinateCalculator/InputView.swift
@@ -0,0 +1,38 @@
+//
+//  InputView.swift
+//  CoordinateCalculator
+//
+//  Created by Jun ho Lee on 2018. 10. 23..
+//  Copyright © 2018년 Codesquad Inc. All rights reserved.
+//
+
+import Foundation
+
+class InputView {
+    
+    //값을 입력 받은 후 가공하여 출력
+    func readInput() -> (Int,Int){
+        let rawInput = receiveInput(ment: "좌표를 입력하세요 (x,y)")
+        return checkCoordinateForm(rawInput: rawInput)
+    }
+    
+    // 멘트를 출력하고 값을 입력받음
+    private func receiveInput(ment: String) -> String {
+        print(ment)
+        return readLine() ?? ""
+    }
+    
+    // 입력받은 값을 좌표 형식에 맞게 검사 후 반환, 틀린 값은 -1 반환
+    private func checkCoordinateForm(rawInput: String) -> (Int,Int) {
+        let pointElements = rawInput.trimmingCharacters(in: CharacterSet(charactersIn: "()")).split(separator: ",")
+        if pointElements.count != 2 {
+            return (-1,-1)
+        }
+        let numericElements = pointElements.compactMap {Int($0) ?? -1}
+        return (numericElements[0], numericElements[0])
+    }
+    
+    func checkCoordinateValue(numbers: (Int,Int)) -> Bool {
+        return numbers.0 < 0 || numbers.1 < 0
+    }
+}
