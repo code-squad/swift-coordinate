23f111c4a474c4bbd10a7458f9f0ee1518d7e520
Jun ho Lee <june20516@gmail.com>
2018-10-24 11:54:42 +0900

    InputView에 값 검사 함수 작성
    readInput
      - numerize 함수로 입력값을 숫자 배열로 만든 다음
      - checkWrong 함수들로 입력값 검사
      - 틀린 값이면 재귀 호출
    main
      - mypoint에 넘겨주는 값의 형식을 배열로 수정

--- CoordinateCalculator/CoordinateCalculator/InputView.swift
+++ CoordinateCalculator/CoordinateCalculator/InputView.swift
@@ -10,10 +10,22 @@ import Foundation
 
 class InputView {
     
-    //값을 입력 받은 후 가공하여 출력
-    func readInput() -> (Int,Int){
+    //값을 입력 받은 후 검사&가공하여 출력
+    func readInput() -> [Int]{
+        // 값 입력받음
         let rawInput = receiveInput(ment: "좌표를 입력하세요 (x,y)")
-        return checkCoordinateForm(rawInput: rawInput)
+        var numberInput = numerizeInput(rawInput: rawInput)
+        // 입력 형식 확인 후 틀리면 재귀 호출
+        if checkWrongForm(inputNumbers: numberInput) {
+            print("입력 형식은 (x,y)입니다")
+            numberInput = readInput()
+        }
+        // 값 범위 확인 후 틀리면 재귀 호출
+        if checkWrongRange(inputNumbers: numberInput) {
+            print("입력 범위는 0=<x<25, 0=<y<25 입니다")
+            numberInput = readInput()
+        }
+        return numberInput
     }
     
     // 멘트를 출력하고 값을 입력받음
@@ -22,17 +34,26 @@ class InputView {
         return readLine() ?? ""
     }
     
-    // 입력받은 값을 좌표 형식에 맞게 검사 후 반환, 틀린 값은 -1 반환
-    private func checkCoordinateForm(rawInput: String) -> (Int,Int) {
+    //입력받은 값을 숫자로 바꿔 배열로 반환
+    private func numerizeInput(rawInput: String) -> [Int] {
         let pointElements = rawInput.trimmingCharacters(in: CharacterSet(charactersIn: "()")).split(separator: ",")
-        if pointElements.count != 2 {
-            return (-1,-1)
-        }
         let numericElements = pointElements.compactMap {Int($0) ?? -1}
-        return (numericElements[0], numericElements[1])
+        return numericElements
     }
     
-    func checkWrongCoordinate(numbers: (Int,Int)) -> Bool {
-        return numbers.0 < 0 || numbers.1 < 0
+    // 입력받는 값이 형식에 맞는지 검사
+    func checkWrongForm(inputNumbers: [Int]) -> Bool {
+        return inputNumbers.count != 2
     }
+    
+    // 입력받는 값이 범위에 맞는지 검사
+    func checkWrongRange(inputNumbers: [Int]) -> Bool {
+        for number in inputNumbers {
+            if number < 0 || number > 24 {return true}
+        }
+        return false
+    }
+    
+    
 }
+
