0cbe4f2db9c155eea12c1908942c1995ebc25486
Jun ho Lee <june20516@gmail.com>
2018-10-24 12:57:13 +0900

    Input 검사 분리를 main 흐름에 적용
    while로 입력받은 값을 검사하여 재입력받기
    printView 에 저장 값이 없으므로, static으로 변경

--- CoordinateCalculator/CoordinateCalculator/PrintView.swift
+++ CoordinateCalculator/CoordinateCalculator/PrintView.swift
@@ -11,18 +11,18 @@ import Foundation
 class PrintView {
     //row = y, col = x
     //변환에 쓰일 상수
-    private let rowConstant = 25
-    private let colConstant = 3
+    private static let rowConstant = 25
+    private static let colConstant = 3
     
     // 출력을 위한 값으로 바꾸기
-    private func modifyForPrint(point: MyPoint) -> (row: Int, col: Int) {
+    static func modifyForPrint(point: MyPoint) -> (row: Int, col: Int) {
         let row = rowConstant - point.y
         let col = colConstant + point.x * 2
         return (row, col)
     }
     
     // ANSICode로 그리기
-    func drawCoordinate(point: MyPoint) {
+    static func drawCoordinate(point: MyPoint) {
         let modifiedPoint = modifyForPrint(point: point)
         print("\(ANSICode.text.whiteBright)\(ANSICode.axis.draw())")
         print("\(ANSICode.text.yellowBright)\(ANSICode.cursor.move(row:modifiedPoint.row, col: modifiedPoint.col))•")
@@ -30,7 +30,7 @@ class PrintView {
     }
     
     // 화면 지우기
-    func eraseDisplay() {
+    static func eraseDisplay() {
         print("\(ANSICode.clear)\(ANSICode.home)")
     }
     
--- CoordinateCalculator/CoordinateCalculator/main.swift
+++ CoordinateCalculator/CoordinateCalculator/main.swift
@@ -9,12 +9,16 @@
 import Foundation
 
 func main() {
-    let input = InputView()
-    let inputedCoordinate = input.readInput()
+    // 처음 값 받음
+    var inputedCoordinate = InputView.readInput()
+    // 검사 후 틀리면 재실행
+    while Error.checkInputError(numberInput: inputedCoordinate) {
+        inputedCoordinate = InputView.readInput()
+    }
+    // mypoint에 저장
     let myPoint = MyPoint(x: inputedCoordinate[0], y: inputedCoordinate[1])
-    let output = PrintView()
-    output.eraseDisplay()
-    output.drawCoordinate(point: myPoint)
+    PrintView.eraseDisplay()
+    PrintView.drawCoordinate(point: myPoint)
 }
 
 main()
