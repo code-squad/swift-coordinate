274e56bedda22ea50611a4ea8a70712a03407560
Jun ho Lee <june20516@gmail.com>
2018-10-25 20:50:55 +0900

    전체적인 변수,메서드명 수정
    API Guideline에 따라 네이밍 수정, 가독성 향상

--- CoordinateCalculator/CoordinateCalculator/InputView.swift
+++ CoordinateCalculator/CoordinateCalculator/InputView.swift
@@ -13,8 +13,8 @@ class InputView {
     //값을 입력 받은 후 가공하여 출력
     static func readInput() -> [Int]{
         let rawInput = receiveInput(ment: "좌표를 입력하세요 (x,y)")
-        let numberInput = numerizeInput(rawInput: rawInput)
-        return numberInput
+        let numericInput = makeNumericArrayBy(rawInput: rawInput)
+        return numericInput
     }
     
     // 멘트를 출력하고 값을 입력받음
@@ -24,7 +24,7 @@ class InputView {
     }
     
     //입력받은 값을 숫자로 바꿔 배열로 반환
-    private static func numerizeInput(rawInput: String) -> [Int] {
+    private static func makeNumericArrayBy(rawInput: String) -> [Int] {
         let pointElements = rawInput.trimmingCharacters(in: CharacterSet(charactersIn: "()")).split(separator: ",")
         let numericElements = pointElements.compactMap {Int($0) ?? -1}
         return numericElements
--- CoordinateCalculator/CoordinateCalculator/main.swift
+++ CoordinateCalculator/CoordinateCalculator/main.swift
@@ -12,16 +12,18 @@ func main() {
     // 처음 값 받음
     var inputedCoordinate = InputView.readInput()
     // 검사 후 틀리면 재실행
-    var inputValidity = Validator.checkInputValidity(numberInput: inputedCoordinate)
+    var inputValidity = Validator.validate(rawCoordinate: inputedCoordinate)
     while inputValidity != Validity.valid {
         inputValidity.printMessage()
         inputedCoordinate = InputView.readInput()
-        inputValidity = Validator.checkInputValidity(numberInput: inputedCoordinate)
+        inputValidity = Validator.validate(rawCoordinate: inputedCoordinate)
     }
     // mypoint에 저장
     let myPoint = MyPoint(x: inputedCoordinate[0], y: inputedCoordinate[1])
+    // 화면 초기화
     PrintView.eraseDisplay()
-    PrintView.drawCoordinate(point: myPoint)
+    // 좌표계 위에 포인트 출력
+    PrintView.printOnCoordSystem(myPoint)
 }
 
 main()
