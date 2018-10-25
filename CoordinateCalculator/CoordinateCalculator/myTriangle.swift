4a842c5e21ca49353b4490d06694909f3764da30
김장수 <jangsookim.me@gmail.com>
2018-10-25 20:38:20 +0900

    3개의 점을 입력받을 때 에러핸들링

---
+++ CoordinateCalculator/CoordinateCalculator/myTriangle.swift
@@ -0,0 +1,13 @@
+//
+//  myTriangle.swift
+//  CoordinateCalculator
+//
+//  Created by 김장수 on 25/10/2018.
+//  Copyright © 2018 Codesquad Inc. All rights reserved.
+//
+
+import Foundation
+
+struct MyTriangle {
+    
+}
