9eb977691f9753754c958df2ab60af51cab24d7d
Dongmin.yoon <ehdals8965@naver.com>
2018-10-26 15:47:01 +0900

    좌표 값을 5개 이상 입력 시 재입력 받음

--- CoordinateCalculator/CoordinateCalculator/InputState.swift
+++ CoordinateCalculator/CoordinateCalculator/InputState.swift
@@ -10,6 +10,7 @@ import Foundation
 
 enum InputState : String {
     case outOfRangeCharacter = "캐릭터 셋에 있는 문자를 입력하세요."   // 캐릭터 셋에 없은 입력이 들어온 경우
+    case notSupportRange = "지원되지 않는 도형입니다."               // 지원되지 않는 도형을 입력한 경우
     case wrongOrder = "(숫자,숫자) 형태로 입력하세요."               // 순서에 맞지 않는 입력을 한 경우
     case overFlowNumber = "범위에 맞는 숫자를 입력하세요."            // 범위를 벗어나느 숫자를 입력한 경우
     case rightInput = ""                                      // 올바른 입력을 했을 경우
---
+++ CoordinateCalculator/CoordinateCalculator/MyRect.swift
@@ -0,0 +1,13 @@
+//
+//  MyRect.swift
+//  CoordinateCalculator
+//
+//  Created by 윤동민 on 26/10/2018.
+//  Copyright © 2018 Codesquad Inc. All rights reserved.
+//
+
+import Foundation
+
+struct MyRect {
+    
+}
