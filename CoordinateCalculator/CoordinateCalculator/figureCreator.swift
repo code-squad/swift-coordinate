daa8938061fde88389a73664c280baf0a0061274
김장수 <jangsookim.me@gmail.com>
2018-10-26 11:17:34 +0900

    CreateFigure 구조체의 이름을 FigureCreator로 변경, 내부의 delieveFigure 메소드의 이름은 makeFigure로 변경

--- CoordinateCalculator/CoordinateCalculator/figureCreator.swift
+++ CoordinateCalculator/CoordinateCalculator/figureCreator.swift
@@ -11,7 +11,7 @@ import Foundation
 // ConvertInput 구조체의 역할
 // 입력 형태에 따라 정수형 좌표로 변환하고 전달
 
-struct CreateFigure {
+struct FigureCreator {
     private let rawPoints: String
     
     // 에러처리를 거친 입력이므로 correctInput으로 변수네이밍
@@ -49,16 +49,7 @@ struct CreateFigure {
     private func makeSinglePoint(_ rawPoint: [String]) -> MyPoint {
         return MyPoint(x: Int(rawPoint[0]) ?? -1, y: Int(rawPoint[1]) ?? -1)
     }
-    
-//    private func makeCoordinates(_ points: [MyPoint]) -> [MyPoint] {
-//        var coordinates: [MyPoint] = []
-//        for point in points {
-//            coordinates.append(point)
-//        }
-//
-//        return coordinates
-//    }
-    
+
     // 초기 입력값을 튜플로 좌표쌍으로 리턴하는 메소드
     // "(10,10)-(10,10)" -> [MyPoint(x: 10, y: 10),MyPoint(x: 10, y: 10)]
     private func makeMultiPoints(_ rawPoint: String) -> [MyPoint] {
@@ -77,7 +68,7 @@ struct CreateFigure {
     // "(10,10)"                    -> MyPoint
     // "(10,10)-(14,15)"            -> MyLine
     // "(10,10)-(14,15)-(20,21)"    -> MyTriangle
-    public func delieveFigure() -> Figure {
+    public func makeFigure() -> Figure {
         let separated = separate(rawPoints)
         let transformed = transform(rawPoints)
         
