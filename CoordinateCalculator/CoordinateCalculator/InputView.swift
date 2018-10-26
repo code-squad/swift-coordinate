7ca1d416b0ffee82c9465e0f008b0e0ea717d943
hngfu <djsl1018@naver.com>
2018-10-26 11:49:36 +0900

    [inputView] readCoordinate() 삭제
    대신 readInput() 사용

--- CoordinateCalculator/CoordinateCalculator/InputView.swift
+++ CoordinateCalculator/CoordinateCalculator/InputView.swift
@@ -9,15 +9,8 @@
 import Foundation
 
 struct InputView {
-    static private func readInput(ment:String) -> String {
+    static func readInput(ment:String) -> String {
         print(ment)
         return readLine() ?? ""
-    }
-    
-    static func readCoordinate(_ isRepeat:Bool) -> String {
-        let typingMent = "좌표를 입력하세요. //단, 숫자의 최대 크기는 24"
-        let retypeMent = "입력값을 확인하시고 다시 입력 해주세요."
-        let input = (isRepeat == true) ? readInput(ment: retypeMent) : readInput(ment: typingMent)
-        return input
     }
 }
--- CoordinateCalculator/CoordinateCalculator/main.swift
+++ CoordinateCalculator/CoordinateCalculator/main.swift
@@ -14,14 +14,15 @@ func main() {
 }
 
 func validCoordinate() -> Figure {
-    var isRepeat = false
+    var ment = "좌표를 입력하세요. //단, 숫자의 최대 크기는 24"
+    let retypeMent = "입력값을 확인하시고 다시 입력 해주세요."
     while true {
-        let inputCoordinate = InputView.readCoordinate(isRepeat)
-        guard inputCoordinate.isValidCharacter() else {isRepeat = true; continue}
+        let inputCoordinate = InputView.readInput(ment: ment)
+        guard inputCoordinate.isValidCharacter() else {ment = retypeMent; continue}
         
         let coordinates = FigureCreator.separateHyphen(inputCoordinate)
         let points = coordinates.map {FigureCreator.readPoint($0)}
-        guard Check.isValidSize(points) else {isRepeat = true; continue}
+        guard Check.isValidSize(points) else {ment = retypeMent; continue}
         
         return FigureCreator.createFigure(points)
     }
