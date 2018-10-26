f2f5c4f9ed99090dd7c454a9f18a0cb522d77ab4
김장수 <jangsookim.me@gmail.com>
2018-10-26 11:52:55 +0900

    makePoints() 메소드가 좌표의 갯수에 상관없이 동일한 역할을 수행하도록 수정

--- CoordinateCalculator/CoordinateCalculator/figureCreator.swift
+++ CoordinateCalculator/CoordinateCalculator/figureCreator.swift
@@ -12,11 +12,10 @@ import Foundation
 // 입력 형태에 따라 정수형 좌표로 변환하고 전달
 
 struct FigureCreator {
-    private let rawPoints: String
+    private let rawPoint: String
     
-    // 에러처리를 거친 입력이므로 correctInput으로 변수네이밍
-    init(rawPoints: String) {
-        self.rawPoints = rawPoints
+    init(rawPoint: String) {
+        self.rawPoint = rawPoint
     }
     
     // 초기 입력을 '-'을 기준으로 분리해주는 메소드
@@ -35,7 +34,7 @@ struct FigureCreator {
     // 분리된 배열의 요소들로 이중배열을 만들어주는 메소드
     // ["(10,10)", "(10,10)"] -> [["10","10"], ["10","10"]]
     // ["(10,10)", "(10,10)", "(14,15)"] -> [["10","10"], ["10","10"], ["14","15"]]
-    private func makeRawPointsUsing(_ separatedCoordinates: [String]) -> [[String]] {
+    private func makeRawPointsWith(_ separatedCoordinates: [String]) -> [[String]] {
         var rawPoints: [[String]] = []
         
         separatedCoordinates.forEach {
@@ -46,22 +45,22 @@ struct FigureCreator {
     
     // 배열의 요소들을 MyPoint에 구조체에 넣어 좌표를 리턴하는 메소드
     // ["10", "10"] -> MyPoint(x: 10, y: 10)
-    private func makeSinglePoint(_ rawPoint: [String]) -> MyPoint {
-        return MyPoint(x: Int(rawPoint[0]) ?? -1, y: Int(rawPoint[1]) ?? -1)
+    private func makeMyPointWith(rawPoints: [String]) -> MyPoint {
+        return MyPoint(x: Int(rawPoints[0]) ?? -1, y: Int(rawPoints[1]) ?? -1)
     }
-
+    
     // 초기 입력값을 튜플로 좌표쌍으로 리턴하는 메소드
-    // "(10,10)-(10,10)" -> [MyPoint(x: 10, y: 10),MyPoint(x: 10, y: 10)]
-    private func makeMultiPoints(_ rawPoint: String) -> [MyPoint] {
+    // "(10,10)-(14,15)" -> [MyPoint(x: 10, y: 10), MyPoint(x: 14, y: 15)]
+    private func makePointsWith(_ rawPoint: String) -> [MyPoint] {
         let separated = separate(rawPoint)
-        let rawPoints = makeRawPointsUsing(separated)    // ["(10,10)", "(10,10)"] -> [["10","10"], ["10","10"]]
+        let rawPoints = makeRawPointsWith(separated)        // ["(10,10)", "(10,10)"] -> [["10","10"], ["10","10"]]
         var points: [MyPoint] = []
         
         rawPoints.forEach {
-            points.append(makeSinglePoint($0))           // ["10", "10"]           -> MyPoint(x: 10, y: 10)
-        }
+            points.append(makeMyPointWith(rawPoints: $0))   // ["10", "10"]           -> MyPoint(x: 10, y: 10)
+        }                                                   // ["14", "15"]           -> MyPoint(x: 14, y: 15)
         
-        return points
+        return points                                       // [MyPoint(x: 10, y: 10), MyPoint(x: 14, y: 15)]
     }
     
     // 생성된 도형(Figure)을 전달해주는 메소드
@@ -69,18 +68,18 @@ struct FigureCreator {
     // "(10,10)-(14,15)"            -> MyLine
     // "(10,10)-(14,15)-(20,21)"    -> MyTriangle
     public func makeFigure() -> Figure {
-        let separated = separate(rawPoints)
-        let transformed = transform(rawPoints)
+        let separated = separate(rawPoint)
         
         switch separated.count {
         case 2:
-            let line = makeMultiPoints(rawPoints)
+            let line = makePointsWith(rawPoint)
             return MyLine(pointA: line[0], pointB: line[1]) // MyLine를 리턴
         case 3:
-            let triangle = makeMultiPoints(rawPoints)
+            let triangle = makePointsWith(rawPoint)
             return MyTriangle(pointA: triangle[0], pointB: triangle[1], pointC: triangle[2])
         default:
-            return makeSinglePoint(transformed)     // MyPoint를 리턴
+            let myPoint = makePointsWith(rawPoint)          // MyPoint를 리턴
+            return myPoint[0]
         }
     }
 }
