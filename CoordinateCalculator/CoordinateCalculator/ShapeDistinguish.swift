dce2d886669c82f5197ca1c1500e6501c727a571
Dongmin.yoon <ehdals8965@naver.com>
2018-10-29 16:13:02 +0900

    ENUM 타입 활용 ShpaeDistinguish

---
+++ CoordinateCalculator/CoordinateCalculator/ShapeDistinguish.swift
@@ -0,0 +1,17 @@
+//
+//  ShapeDistinguish.swift
+//  CoordinateCalculator
+//
+//  Created by 윤동민 on 29/10/2018.
+//  Copyright © 2018 Codesquad Inc. All rights reserved.
+//
+
+import Foundation
+
+enum ShapeDistinguish {
+    case point
+    case line
+    case triangle
+    case rect
+    case notSupport
+}
