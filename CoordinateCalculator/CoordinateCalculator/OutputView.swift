428396e103ac890bb396c32602baccaee1ba0a88
conyconydev <conyconydev@gmail.com>
2018-10-29 20:35:22 +0900

    좌표계산기 시작하기

---
+++ CoordinateCalculator/CoordinateCalculator/OutputView.swift
@@ -0,0 +1,17 @@
+//
+//  OutputView.swift
+//  CoordinateCalculator
+//
+//  Created by Elena on 29/10/2018.
+//  Copyright © 2018 Codesquad Inc. All rights reserved.
+//
+
+import Foundation
+
+struct OutputView {
+    
+    func drawAxis() {
+        print("\(ANSICode.clear)\(ANSICode.home)")
+        print("\(ANSICode.text.whiteBright)\(ANSICode.axis.draw())")
+    }
+}
