c92a357c0aed55ecd7b7be9ccbb9b21f2a58c9fb
김장수 <jangsookim.me@gmail.com>
2018-10-29 19:22:15 +0900

    사각형의 구조를 추상화해서 프로퍼티에 저장

--- CoordinateCalculator/CoordinateCalculator/myRect.swift
+++ CoordinateCalculator/CoordinateCalculator/myRect.swift
@@ -10,23 +10,20 @@ import Foundation
 
 struct MyRect : ComputableFigure {
     private var leftTop : MyPoint
-    private var rightTop : MyPoint
-    private var leftBottom : MyPoint
-    private var rightBottom : MyPoint
     private let size : CGSize
     
     init(origin: MyPoint, size: CGSize) {
-        let width = Int(size.width), height = Int(size.height)
-        let x = origin.xPoint(), y = origin.yPoint()
-
         self.size = size
         self.leftTop = origin
-        self.rightTop = MyPoint(x: x + width, y: y)
-        self.leftBottom = MyPoint(x: x, y: y - height)
-        self.rightBottom = MyPoint(x: x + width, y: y - height)
     }
     
     public var points: [MyPoint] {
+        let width = Int(size.width), height = Int(size.height)
+        let x = leftTop.xPoint(), y = leftTop.yPoint()
+        let rightTop = MyPoint(x: x + width, y: y)
+        let leftBottom = MyPoint(x: x, y: y - height)
+        let rightBottom = MyPoint(x: x + width, y: y - height)
+        
         return [leftTop, rightTop, rightBottom, leftBottom]
     }
     
