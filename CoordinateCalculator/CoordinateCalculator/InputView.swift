478e87994d5c97a052a3383f142efc2110dcfb57
conyconydev <conyconydev@gmail.com>
2018-11-01 13:26:59 +0900

    강제 옵셔널 처리 , Mypoint로 만들기 , Main 구조체 만듬

--- CoordinateCalculator/CoordinateCalculator/InputView.swift
+++ CoordinateCalculator/CoordinateCalculator/InputView.swift
@@ -7,25 +7,20 @@
 //
 
 import Foundation
-/*
- func splitByComma() -> [String] {
- return self.split(separator: ",").map{String($0)}
- }
- // 로 만들었을때는 readPlayerNames()
- checkPlayerName(commaName) ? commaName.compactMap {LadderPlayer(name: String($0))} : nil
- 이부분을 접근할때 문자 하나씩 접근하는데..
- 아래와 같이 SubSequence 로 접근하니깐 단어 하나씩 접근한다. 검색을 하고 이것저것 해보다보니 결론은 얻었으나?
- 왜? 그런지는 찾지 못하였다.
- */
 
 extension String {
-    // 문자의 공백을 제거하는것 만듬.
+    // 문자의 공백을 제거하는것 만듬. -0
     func removingWhitespaces() -> String {
         return components(separatedBy: .whitespaces).joined()
     }
 }
 
 struct InputView {
+    // MARK: Mypoint init
+    static func initMyPoint(_ number : (Int,Int)) -> MyPoint {
+        let point = MyPoint(x: number.0, y: number.1)
+        return point
+    }
  
     // MARK: 입력만 받는 함수 -1
     static private func readInput(ment: String) -> String {
@@ -33,51 +28,32 @@ struct InputView {
         return readLine() ?? ""
     }
     // MARK : 좌표 입력 받아서 처리하는 함수 -2
-    static func getUserCoordinate() -> Int {
-        let coordinates = readInput(ment: "좌표를 입력하세요.") // -1
-        //괄호 열고 닫는거 관리하려고
-        
-        let a = checkString(coordinates)
-        print(a)
-        // 문자열을 하나씩 참조해서 문자냐? 괄호냐? 숫자냐 접근
-//        for char in coordinates {
-//
-//            guard checkBracket(char) != true else {
-//                continue
-//            }
-//
-//            if char == "," {
-//                x = Int(number)!
-//                guard checkScope(x) else {
-//                    return 0
-//                }
-//                number = ""
-//            }else {
-//                number.append(char)
-//            }
-//        }
-//        y = Int(number)!
-//
-//        print(x,y)
-//
-        return 0
+    static func getUserCoordinate() -> MyPoint? {
+        let coordinate = readInput(ment: "좌표를 입력하세요.") // -1
+        let coordinates = coordinate.removingWhitespaces() // -0
+        let check : (Int,Int) = checkString(coordinates) // -5
+        guard check != (0,0) else {
+            return nil
+        }
+        let point = initMyPoint(check)
+        return point
     }
-    // (3,4) 숫자로 쪼개는 함수
-    static private func checkString(_ coordinates: String) -> (Int?,Int?){
-        var x = 0 , y = 0 , count = 0 // 맨 처음에 무조건 괄호를 검사하고 싶은데...
+    // (3,4) 숫자로 쪼개는 함수 // -5
+    static private func checkString(_ coordinates: String) -> (Int,Int){
+        //count : 반복문에서 인덱스와 문자 하나하나 접근하는 방법을 몰라서 , 첫번째 검사를 위해 만듬
+        var x = 0 , y = 0 , count = 0
         var number : String = ""
 
         for char in coordinates {
-            // 무조건 처음에 ( 여는 괄호 없음 그냥 멈춰.
-            if checkBracket(char) == true , count == 0  {
-                return (nil,nil)
+            // 예외처리 : 처음 괄호가 들어오지 않는다면 멈춰라.
+            if checkBracket(char) == true , count == 0  { //-3
+                return (0,0)
             }
             count = count + 1
+            
             if char == "," {
-                x = Int(number)!
-                guard checkScope(x) else {
-                    return (nil,nil)
-                }
+                x = changeNumber(number) //-5
+                if x == 0 { return (0,0) }
                 number = ""
             }else if char == "(" || char == ")" {
                 continue
@@ -86,9 +62,11 @@ struct InputView {
                 number.append(char)
             }
         }
-        y = Int(number)!
+        y = changeNumber(number) //-5
+        if y == 0 { return (0,0) }
         return (x,y)
     }
+    
     // MARK: 괄호를 체크하는 함수 -3
     static private func checkBracket(_ char : Character) -> Bool{
         if char == "(" {
@@ -99,13 +77,19 @@ struct InputView {
     // MARK: 1~24까지 범위, 입력초과시 재입력 // -4
     static private func checkScope(_ num :Int) -> Bool {
         if num <= 0 || num > 24 {
+            print("24 범위 안에 들어옴 \(num)")
             return false
         }
         return true
     }
-    // MARK: 괄호를 없세고 숫자로 반환해준다.
-    
-    
+    // MARK : 문자를 숫자로 변경하기 위해 (강제 옵셔널이 생겨서 함수로..) -5
+    static private func changeNumber(_ number : String) -> Int {
+        if let num = Int(number) , checkScope(num) == true {
+            print("강제 옵셔널 함수 안 \(num)")
+            return num
+        }
+        return 0
+    }
 }
 
 
--- CoordinateCalculator/CoordinateCalculator/main.swift
+++ CoordinateCalculator/CoordinateCalculator/main.swift
@@ -2,13 +2,24 @@
 //  main.swift
 //  CoordinateCalculator
 //
-//  Created by JK on 09/10/2017.
-//  Copyright © 2017 Codesquad Inc. All rights reserved.
+//  Created by Elena on 31/10/2018.
+//  Copyright © 2018 Codesquad Inc. All rights reserved.
 //
 
 import Foundation
 
-//let point : MyPoint = MyPoint.init(x: 3, y: 5)
-//
-//OutputView().drawPoint(inputPoints: point)
-InputView.getUserCoordinate()
+struct Main {
+     static func coordinateStart() {
+        var repetition = true
+        while(repetition) {
+            let point = InputView.getUserCoordinate()
+            if let pointPrint = point , point != nil {
+                repetition = false
+                OutputView().drawPoint(inputPoints: pointPrint)
+            }
+        }
+    }
+}
+
+Main.coordinateStart()
+
