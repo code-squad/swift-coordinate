d6de04b755543d7dd2d859997b8739d5dde71cc6
Jun ho Lee <june20516@gmail.com>
2018-11-01 12:26:43 +0900

    figure 종류별 출력 함수 추가, validator 강화
    printView
      - printPoint에 modify 메소드를 포함시킴
      - printPoint 사용하여 printLine 구현
      - figure 판단하여 알맞는 print함수 호출하는 printOnCoordSystem 구현
      - main함수에 반영
    
    Validator
      - incorrectCharactor 케이스 추가

--- CoordinateCalculator/CoordinateCalculator/main.swift
+++ CoordinateCalculator/CoordinateCalculator/main.swift
@@ -8,25 +8,36 @@
 
 import Foundation
 
+//좌표 개수에 따라 figure 구분하여 저장
+func classifyFigure(coordinates: [Int]) -> MyFigure {
+    switch coordinates.count {
+    case 4 :
+        return MyLine(coordinateArray: coordinates)
+    default : return MyPoint(x: coordinates[0], y: coordinates[1])
+    }
+}
+
 func main() {
     // 처음 값 받음
-    var inputedString = InputView.receiveInput(ment:"초기입력")
+    var inputedString = InputView.receiveInput(ment:"\(ANSICode.text.white)좌표를 입력하세요 (x,y)")
     // 검사 후 틀리면 재실행
     var inputValidity = Validator.validate(rawCoordinate: inputedString)
     while inputValidity != nil {
         inputValidity?.printMessage()
-        inputedString = InputView.receiveInput(ment:"초기입력")
+        inputedString = InputView.receiveInput(ment:"\(ANSICode.text.white)좌표를 입력하세요 (x,y)")
         inputValidity = Validator.validate(rawCoordinate: inputedString)
     }
     // 검사를 통과한 값을 배열로 변환
-    let inputedCoordinate = InputView.makeNumericArrayBy(rawInput: inputedString).compactMap { $0 }
+    let inputedCoordinates = InputView.makeNumericArrayBy(rawInput: inputedString).compactMap { $0 }
     // mypoint에 저장
-    let myPoint = MyPoint(x: inputedCoordinate[0], y: inputedCoordinate[1])
+    let myFigure = classifyFigure(coordinates: inputedCoordinates)
     // 화면 초기화
     PrintView.eraseDisplay()
     // 좌표계 위에 포인트 출력
-    PrintView.printOnCoordSystem(myPoint)
+    PrintView.printOnCoordSystem(myFigure)
 }
+
+
 
 main()
 
