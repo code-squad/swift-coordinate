9cdef018b3e54e4fef9132266b25becb50357ed2
Jun ho Lee <june20516@gmail.com>
2018-11-02 14:16:46 +0900

    CoordGenerator 구조체 생성, 매서드 재배치
    좌표로 변환하기 위한 메서드를 담는 CoordGenerator를 생성, Inputview의 makeNumericArray와 main파일의 classifyFigure 를 이동.

--- CoordinateCalculator/CoordinateCalculator/main.swift
+++ CoordinateCalculator/CoordinateCalculator/main.swift
@@ -8,14 +8,6 @@
 
 import Foundation
 
-//좌표 개수에 따라 figure 구분하여 저장
-func classifyFigure(coordinates: [Int]) -> MyFigure {
-    switch coordinates.count {
-    case 4 :
-        return MyLine(coordinateArray: coordinates)
-    default : return MyPoint(x: coordinates[0], y: coordinates[1])
-    }
-}
 
 func main() {
     // 처음 값 받음
@@ -23,14 +15,13 @@ func main() {
     // 검사 후 틀리면 재실행
     var inputValidity = Validator.validate(rawCoordinate: inputedString)
     while inputValidity != nil {
-        inputValidity?.printMessage()
         inputedString = InputView.receiveInput(ment:"\(ANSICode.text.white)좌표를 입력하세요 (x,y)")
         inputValidity = Validator.validate(rawCoordinate: inputedString)
     }
     // 검사를 통과한 값을 배열로 변환
-    let inputedCoordinates = InputView.makeNumericArrayBy(rawInput: inputedString).compactMap { $0 }
+    let inputedCoordinates = CoordGenerator.makeNumericArrayBy(rawInput: inputedString).compactMap { $0 }
     // mypoint에 저장
-    let myFigure = classifyFigure(coordinates: inputedCoordinates)
+    let myFigure = CoordGenerator.classifyFigure(coordinates: inputedCoordinates)
     // 화면 초기화
     PrintView.eraseDisplay()
     // 좌표계 위에 포인트 출력
