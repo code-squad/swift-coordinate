8c21753704263becd8d365cb0df9cfe91323e01e
Jun ho Lee <june20516@gmail.com>
2018-11-02 14:10:19 +0900

    validity 에 따른 메세지 출력 부분 재배치
    enum 안에서 validate로 print 함수 이동

---
+++ CoordinateCalculator/CoordinateCalculator/CoordGenerator.swift
@@ -0,0 +1,9 @@
+//
+//  CoordGenerator.swift
+//  CoordinateCalculator
+//
+//  Created by Jun ho Lee on 02/11/2018.
+//  Copyright © 2018 Codesquad Inc. All rights reserved.
+//
+
+import Foundation
