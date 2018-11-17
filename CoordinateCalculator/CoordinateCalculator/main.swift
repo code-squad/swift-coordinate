0bcce727c5df490b7042b392d5a493bcb08e0eb3
conyconydev <conyconydev@gmail.com>
2018-11-17 19:14:27 +0900

    modified step3

--- CoordinateCalculator/CoordinateCalculator/main.swift
+++ CoordinateCalculator/CoordinateCalculator/main.swift
@@ -8,18 +8,37 @@
 
 import Foundation
 
-struct Main {
-     static func coordinateStart() {
-        var repetition = true
-        while(repetition) {
-            let point = InputView.getUserCoordinate()
-            if let pointPrint = point[0] , point[0] != nil {
-                repetition = false
-                OutputView().drawPoint(inputPoints: pointPrint)
+func coordinateStart() {
+    var repetition = true
+    var pointResult: String = ""
+    var lineResult: String = ""
+    while(repetition) {
+        let point = InputView.getUserString()
+        //- 체크
+        let coordinatesCount = InputView.checkHyphenCoordinate(point)
+        //[point]
+        let points = InputView.selectResult(point,coordinatesCount)
+        
+        for i in 0..<points.count {
+            if points[i] == nil {
+                break
             }
+            repetition = false
+            pointResult += OutputView().resultPoint(inputPoints: points[i] ?? MyPoint.init(x: 0, y: 0))
+        }
+        // Line
+        if coordinatesCount == 2 {
+            let line = MyLine.init(points: points)
+            lineResult += OutputView().displayLine(line!)
         }
     }
+    OutputView().drawPoint(pointResult)
+    if lineResult != "" {
+        OutputView().drawLine(pointResult,lineResult)
+    }
+    
+    
 }
 
-Main.coordinateStart()
+coordinateStart()
 
