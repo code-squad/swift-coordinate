b4915d76b45a85d33f8acad564cbf26894e4ccfa
NORIKIM <jinagold.k@gmail.com>
2018-12-03 16:17:45 +0900

    Add main swift file to run code

---
+++ coordinateCalculator/coordinateCalculator/main.swift
@@ -0,0 +1,15 @@
+//
+//  main.swift
+//  coordinateCalculator
+//
+//  Created by JINA on 27/11/2018.
+//  Copyright © 2018 JINA KIM. All rights reserved.
+//
+
+import Foundation
+
+func main() {
+    let input = InputView.readInput()
+    outputView().printMyPoint(input)
+}
+main()
