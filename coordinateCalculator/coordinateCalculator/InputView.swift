6c436f681c450cba9bb439026b544e6b4c732474
NORIKIM <jinagold.k@gmail.com>
2018-12-04 14:51:25 +0900

    InputView에 맞지 않는 사용자의 입력에서 숫자를 받아오는 함수를 제외함

--- coordinateCalculator/coordinateCalculator/InputView.swift
+++ coordinateCalculator/coordinateCalculator/InputView.swift
@@ -8,25 +8,13 @@
 
 import Foundation
 
+//좌표를 입력받음
 struct InputView {
-    //좌표를 입력받고 콤마를 기준으로 나누어 문자열 배열을 만듦
-    static func readInput() -> [String] {
+    static func readInput() -> String {
         print("좌표를 입력하세요 (x,y)")
-        let input = readLine()?.components(separatedBy: ",")
-        return input!
-    }
-    
-    //콤마로 나눈 문자열 배열을 숫자와 문자를 분리 하고 숫자 배열을 만듦
-    static func separateValue(_ input: [String]) -> MyPoint {
-        var firVal = 0
-        var secVal = 0
-        if input[0].contains("(") && input[1].contains(")") {
-            firVal = Int(input[0].trimmingCharacters(in: CharacterSet.decimalDigits.inverted))!
-            secVal = Int(input[1].trimmingCharacters(in: CharacterSet.decimalDigits.inverted))!
-        } else {
-            print("(x,y) 형식으로 입력해주세요")
-            readInput()
+        guard let input = readLine() else{
+            return ""
         }
-        return MyPoint.init([firVal,secVal])
-    }    
+        return input
+    }
 }
