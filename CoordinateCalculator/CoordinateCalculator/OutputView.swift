367c5343995e5215bf270c3a3a09086bffe8cc91
conyconydev <conyconydev@gmail.com>
2018-12-07 12:15:45 +0900

    변수명 수정

--- CoordinateCalculator/CoordinateCalculator/OutputView.swift
+++ CoordinateCalculator/CoordinateCalculator/OutputView.swift
@@ -15,8 +15,8 @@ struct OutputView {
             resultPoint(inputPoints: point)
         }
         if shape.points.count >= 2 {
-            guard let shape: DistanceProtocol = shape as? DistanceProtocol else { return }
-            displayLine(shape)
+            guard let distance: DistanceProtocol = shape as? DistanceProtocol else { return }
+            displayLine(distance)
         }
         drawEnd()
     }
