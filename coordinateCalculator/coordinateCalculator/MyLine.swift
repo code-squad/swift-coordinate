99ef1ff804b78dd96bcd84d28592b4d79fa60fe9
NORIKIM <jinagold.k@gmail.com>
2018-12-13 17:46:59 +0900

    좌표를 하나 더 받고 두 점 사이의 거리를 계산하도록 MyLine객체 생성함

---
+++ coordinateCalculator/coordinateCalculator/MyLine.swift
@@ -0,0 +1,30 @@
+//
+//  MyLine.swift
+//  coordinateCalculator
+//
+//  Created by JINA on 04/12/2018.
+//  Copyright © 2018 JINA KIM. All rights reserved.
+//
+
+import Foundation
+
+struct MyLine: Figure, calculation {
+    private(set) var pointA: MyPoint
+    private(set) var pointB: MyPoint
+    
+    init(point: [Int]) {
+        self.pointA = MyPoint(x: point[0], y: point[1])
+        self.pointB = MyPoint(x: point[2], y: point[3])
+    }
+    
+    var point: [MyPoint] {
+        return [pointA, pointB]
+    }
+    
+    func calculate() -> String {
+        let pointX = Double(pointA.x - pointB.x)
+        let pointY = Double(pointA.y - pointB.y)
+        let calLine = sqrt((pointX * pointX) + (pointY * pointY))
+        return "두 점 사이의 거리는 : \(calLine)"
+    }
+}
