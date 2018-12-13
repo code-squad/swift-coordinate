826b4b9bc1d2ecf1d56f8ee0a2d966a503cb13bb
NORIKIM <jinagold.k@gmail.com>
2018-12-13 17:49:49 +0900

    두개 이상의 좌표값을 받고 두 점 사이의 거리를 계산하기 위한 프로토콜 Figure, calculation을 생성

---
+++ coordinateCalculator/coordinateCalculator/Procol.swift
@@ -0,0 +1,17 @@
+//
+//  Procol.swift
+//  coordinateCalculator
+//
+//  Created by JINA on 09/12/2018.
+//  Copyright © 2018 JINA KIM. All rights reserved.
+//
+
+import Foundation
+
+protocol Figure {
+    var point: [MyPoint] {get}
+}
+
+protocol calculation: Figure {
+    func calculate() -> String
+}
