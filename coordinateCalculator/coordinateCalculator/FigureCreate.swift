f4aa3c6db5c3063605ed4e8bba7189d9bdf736cd
NORIKIM <jinagold.k@gmail.com>
2018-12-19 14:44:36 +0900

    CreatFigure함수의 MyLine에서 Figure로 넘겨주는 방식 변경 후 MyTriangle 추가

--- coordinateCalculator/coordinateCalculator/FigureCreate.swift
+++ coordinateCalculator/coordinateCalculator/FigureCreate.swift
@@ -24,7 +24,9 @@ struct FigureCreate {
         
         switch num.count {
         case 4:
-            return MyLine(point: num)
+            return MyLine(pointA: MyPoint(x: num[0], y: num[1]), pointB: MyPoint(x: num[2], y: num[3]))
+        case 6:
+            return MyTriangle(pointA: MyPoint(x: num[0], y: num[1]), pointB: MyPoint(x: num[2], y: num[3]), pointC: MyPoint(x: num[4], y: num[5]))
         default:
             return MyPoint(x: num[0], y: num[1])
         }
