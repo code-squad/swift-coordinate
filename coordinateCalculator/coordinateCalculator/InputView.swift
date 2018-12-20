4c19656983b445e0637e8a65ff649317311701fe
NORIKIM <jinagold.k@gmail.com>
2018-12-20 15:28:34 +0900

    바뀐 사항 없음

--- coordinateCalculator/coordinateCalculator/InputView.swift
+++ coordinateCalculator/coordinateCalculator/InputView.swift
@@ -11,7 +11,7 @@ import Foundation
 //좌표를 입력받음
 struct InputView {
     static func readInput() -> String {
-        print("""
+        print( """
               좌표를 입력하세요.(범위:0~24)
               1. (x,y)
               2. (x,y)-(x,y)
