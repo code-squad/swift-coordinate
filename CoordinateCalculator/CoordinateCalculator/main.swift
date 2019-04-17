d97f6153fd5ca2cf1a1cd043ac338958af3662fd
Sunghyun Kim <sunghyun_k@icloud.com>
2019-04-17 21:06:37 +0900

    오류 처리 구문 생성 질문이 노란색으로 나오도록 변경

--- CoordinateCalculator/CoordinateCalculator/main.swift
+++ CoordinateCalculator/CoordinateCalculator/main.swift
@@ -4,6 +4,19 @@ func main() throws {
     OutputView.drawAxis()
     let coordinate = try InputView.readCoordinate()
     OutputView.drawPoint(x: coordinate.x, y: coordinate.y)
+    
 }
 
-try main()
+do {
+    try main()
+} catch InputError.cannotIdentifyNumbers {
+    print("오류: 숫자를 식별할 수 없음")
+} catch InputError.cannotIdentifyTwoValues {
+    print("오류: 두개의 숫자를 찾을 수 없음")
+} catch InputError.cannotRecognizeParentheses {
+    print("오류: 괄호를 찾을 수 없음")
+} catch InputError.invalidInput {
+    print("오류: 유효하지 않은 입력")
+} catch InputError.notEnoughCharacters {
+    print("오류: 글자 수가 충분하지 않음")
+}
