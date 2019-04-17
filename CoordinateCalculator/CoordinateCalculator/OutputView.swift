d330c66e1e921a231070b182064478a8f79f953e
Sunghyun Kim <sunghyun_k@icloud.com>
2019-04-17 20:40:42 +0900

    정확한 값을 출력하도록 수정

--- CoordinateCalculator/CoordinateCalculator/OutputView.swift
+++ CoordinateCalculator/CoordinateCalculator/OutputView.swift
@@ -3,32 +3,31 @@ import Foundation
 struct OutputView {
     
     //MARK: 비공개 정적 메소드
-    private static func moveCursorTo(row: Int, column: Int) {
-        let defaultRow = 3
-        let defaultColumn = 23
-        
-        let resultRow = move(row: defaultRow, move: row)
-        let resultColumn = move(column: defaultColumn, move: column)
-        
-        print("\(ANSICode.cursor.move(row: resultRow, column: resultColumn))")
+    private static func moveCursorTo(x: Int, y: Int) {
+        let defaultColumn = 3
+        let defaultRow = 25
+        let resultColumn = move(x: defaultColumn, by: x)
+        let resultRow = move(y: defaultRow, by: y)
+        print(ANSICode.cursor.move(row: resultRow, column: resultColumn), terminator: "")
     }
     
-    private static func move(row: Int, move rate: Int) -> Int {
-        return row + rate * 2
+    private static func move(x: Int, by rate: Int) -> Int {
+        return x + rate * 2
     }
-    private static func move(column: Int, move rate: Int) -> Int {
-        return column - rate
+    
+    private static func move(y: Int, by rate: Int) -> Int {
+        return y - rate
     }
     
     //MARK: 정적 메소드
     static func drawAxis() {
-        print(ANSICode.clear + ANSICode.home)
-        print(ANSICode.text.magenta + ANSICode.axis.draw())
+        print(ANSICode.clear)
+        print(ANSICode.text.cyanBright + ANSICode.axis.draw())
     }
     
-    static func drawPoint(row: Int, column: Int) {
-        moveCursorTo(row: row, column: column)
-        print("*")
+    static func drawPoint(x: Int, y: Int) {
+        moveCursorTo(x: x, y: y)
+        print(ANSICode.text.redBright + "O" + ANSICode.home)
     }
     
     
