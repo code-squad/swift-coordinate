776249e68bb66c4ef2bf9acc0703c080a54f2644
gukjin jang <>
2019-04-17 15:21:55 +0900

    Add : OutputView구조체 생성 및 drawAxis() 함수 생성

---
+++ CoordinateCalculator/CoordinateCalculator/OutputView.swift
@@ -0,0 +1,16 @@
+//
+//  OutputView.swift
+//  CoordinateCalculator
+//
+//  Created by jang gukjin on 17/04/2019.
+//  Copyright © 2019 Codesquad Inc. All rights reserved.
+//
+
+import Foundation
+
+struct OutputView{
+    func drawAxis(){
+        print("\(ANSICode.clear)\(ANSICode.home)")
+        print("\(ANSICode.text.whiteBright)\(ANSICode.axis.draw())")
+    }
+}
