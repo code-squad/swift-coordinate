3465902170ea7783f02470193a03abbfe0d8545d
kiljh <kiljh@me.com>
2019-04-17 13:43:12 +0900

    drawAxis 함수 구현

--- CoordinateCalculator/CoordinateCalculator/main.swift
+++ CoordinateCalculator/CoordinateCalculator/main.swift
@@ -9,10 +9,13 @@
 import Foundation
 
 struct OutputView {
-    
+    func drawAxis () {
+        print("\(ANSICode.clear)\(ANSICode.home)")
+        print("\(ANSICode.text.whiteBright)\(ANSICode.axis.draw())")
+    }
 }
 
 let outputView = OutputView()
-
+outputView.drawAxis()
 
 
