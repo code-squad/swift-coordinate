8b6c00fa0100bc40d9b2f059b1e730bee0f24a9f
kcushy <dev.kcushy@gmail.com>
2019-04-18 03:43:23 +0900

    feat: Add a OutputView struct and drawAxis()

---
+++ CoordinateCalculator/CoordinateCalculator/OutputView.swift
@@ -0,0 +1,17 @@
+//
+//  OutputView.swift
+//  CoordinateCalculator
+//
+//  Created by cushy k on 17/04/2019.
+//  Copyright © 2019 Codesquad Inc. All rights reserved.
+//
+
+import Foundation
+
+struct OutputView {
+    static func drawAxis() {
+        let quadrantOne = ANSICode.axis.draw()
+        print("\(ANSICode.clear)\(ANSICode.home)")
+        print("\(ANSICode.text.yellowBright)\(quadrantOne)")
+    }
+}
