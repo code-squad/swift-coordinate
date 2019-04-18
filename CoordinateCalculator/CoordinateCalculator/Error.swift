4ff5739ec943b834a9eb9b334fed01112e176100
Sunghyun Kim <sunghyun_k@icloud.com>
2019-04-18 17:02:37 +0900

    좌표의 속성들을 읽기 전용으로 변경함 오류 출력을 위해 InputError에 오류 문자열 반환 함수를 내장함

--- CoordinateCalculator/CoordinateCalculator/Error.swift
+++ CoordinateCalculator/CoordinateCalculator/Error.swift
@@ -7,4 +7,25 @@ enum InputError: Error {
     case cannotIdentifyNumbers
     case notEnoughCharacters
     case exceededAxisLimit
+    
+    func description() -> String {
+        var errorMessage = "입력 오류: "
+        switch self {
+        case .invalidInput:
+            errorMessage += "유효하지 않은 입력"
+        case .cannotIdentifyNumbers:
+            errorMessage += "숫자를 확인할 수 없음"
+        case .cannotIdentifyTwoValues:
+            errorMessage += "두 개의 숫자를 확인할 수 없음"
+        case .cannotRecognizeParentheses:
+            errorMessage += "괄호를 인식할 수 없음"
+        case .exceededAxisLimit:
+            errorMessage += "좌표의 제한을 초과함"
+        case .notEnoughCharacters:
+            errorMessage += "글자 수가 충분하지 않음"
+        }
+        return errorMessage
+    }
+    
 }
+
