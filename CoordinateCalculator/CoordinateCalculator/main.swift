1d21aaff9d15f82e9dbf720377721f29cae57296
gukjin jang <jgj455@gmail.com>
2019-04-18 20:42:00 +0900

    Add : main() 함수 생성

--- CoordinateCalculator/CoordinateCalculator/main.swift
+++ CoordinateCalculator/CoordinateCalculator/main.swift
@@ -8,4 +8,20 @@
 
 import Foundation
 
-OutputView().drawAxis()
+func main(){
+    
+    do{
+        try InputView().readInput()
+    }catch ErrorMessage.noValueError{
+        print("올바른 값을 입력하세요")
+    }catch ErrorMessage.outOfRangeError{
+        print("법위를 넘었습니다")
+    }catch ErrorMessage.typeMissError{
+        print("정수를 입력하세요")
+    }
+    
+    OutputView().drawAxis()
+    
+}
+
+main()
