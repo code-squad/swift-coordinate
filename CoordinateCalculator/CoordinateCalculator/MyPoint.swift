f78d6d722c24ce3d7be99b9ba9f9dce079a449b5
gukjin jang <jgj455@gmail.com>
2019-04-18 20:34:14 +0900

    Modifay : 코드를 원래대로 되롤림

--- CoordinateCalculator/CoordinateCalculator/MyPoint.swift
+++ CoordinateCalculator/CoordinateCalculator/MyPoint.swift
@@ -7,3 +7,33 @@
 //
 
 import Foundation
+
+struct MyPoint {
+    var x = 0
+    var y = 0
+    
+    mutating func distinctLocation() throws -> (x: Int, y: Int){
+        let locationText = InputView().readInput()
+        
+        if locationText != nil { throw ErrorMessage.noValueError }
+        else {
+            let locations = locationText!.split(separator: ",")
+            x = try distinctRangeAndValue(locations: locations).inputX
+            y = try distinctRangeAndValue(locations: locations).inputY
+        }
+        return (x,y)
+    }
+    
+    func distinctRangeAndValue(locations : [String.SubSequence]) throws -> (inputX: Int, inputY: Int) {
+        if locations.count != 2 { throw ErrorMessage.outOfRangeError}
+        else {
+            guard let inputX = Int(locations[0]) else {
+                throw ErrorMessage.typeMissError
+            }
+            guard let inputY = Int(locations[1]) else {
+                throw ErrorMessage.typeMissError
+            }
+            return (inputX,inputY)
+        }
+    }
+}
