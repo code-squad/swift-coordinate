e20f54abbd38d6e68924068a2670f071a6b9b50b
kiljh <kiljh@me.com>
2019-04-18 14:37:50 +0900

    오류 ENUM 구현

--- CoordinateCalculator/CoordinateCalculator/main.swift
+++ CoordinateCalculator/CoordinateCalculator/main.swift
@@ -8,6 +8,18 @@
 
 import Foundation
 
+enum InputError: Error {
+    case inputNonStandard
+    case coordinateNonStandard
+    
+    private var manual: String {
+        switch self {
+        case .inputNonStandard: return "소괄호로 둘러싸인 x,y 좌표를 정확히 입력해주세요. ex: (4,16)"
+        case .coordinateNonStandard: return "x,y 좌표는 0부터 24까지 입력해주세요."
+        }
+    }
+}
+
 //MARK: Struct
 struct OutputView {
     func drawAxis () {
