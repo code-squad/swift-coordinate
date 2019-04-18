3f9099ad78ec5148e7384cf83720e3504de248c5
kiljh <kiljh@me.com>
2019-04-18 17:50:12 +0900

    흐름을 제어하고 프린트하는 함수는 main에 구성

--- CoordinateCalculator/CoordinateCalculator/main.swift
+++ CoordinateCalculator/CoordinateCalculator/main.swift
@@ -11,10 +11,26 @@ import Foundation
 //MARK: Function
 func main () {
     let outputView = OutputView()
-    
     var inputView = InputView()
     var pointCoordinate = MyPoint()
-    pointCoordinate = inputView.inputLoop(pointCoordinate)
+    
+    while true {
+        inputView.readInput()
+        do {
+            try pointCoordinate.inputToCoordinate(inputView)
+            break
+        }
+        catch InputError.NonInputStandard {
+            print("소괄호로 둘러싸인 x,y 좌표를 정확히 입력해주세요. ex: (4,16)")
+        }
+        catch InputError.NonCoordinateStandard {
+            print("x,y 좌표는 0부터 24까지 입력해주세요.")
+        }
+        catch {
+            print("입력값을 확인해주세요.")
+        }
+    }
+    
     outputView.drawAxis()
     outputView.drawPoint(pointCoordinate)
 }
