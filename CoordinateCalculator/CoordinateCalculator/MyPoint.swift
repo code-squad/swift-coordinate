36bc0d59b86e9b7bf032a2d94ff5c205a4bb11c2
cmindy <minjidev@naver.com>
2019-04-19 15:19:45 +0900

    feat: Add MyPoint Struct to handling x, y point

---
+++ CoordinateCalculator/CoordinateCalculator/MyPoint.swift
@@ -0,0 +1,14 @@
+//
+//  MyPoint.swift
+//  CoordinateCalculator
+//
+//  Created by CHOMINJI on 19/04/2019.
+//  Copyright © 2019 Codesquad Inc. All rights reserved.
+//
+
+import Foundation
+
+struct MyPoint {
+    var x = 0
+    var y = 0
+}
