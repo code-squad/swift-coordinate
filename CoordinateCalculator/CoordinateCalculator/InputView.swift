ac1d419373ff28dfed110dc9e09e3df5ad763eab
kcushy <dev.kcushy@gmail.com>
2019-04-19 11:42:50 +0900

    feat: Add a InputView

---
+++ CoordinateCalculator/CoordinateCalculator/InputView.swift
@@ -0,0 +1,20 @@
+//
+//  InputView.swift
+//  CoordinateCalculator
+//
+//  Created by cushy k on 19/04/2019.
+//  Copyright © 2019 Codesquad Inc. All rights reserved.
+//
+
+import Foundation
+
+struct InputView {
+    private func deliver(message: String) {
+        print(message)
+    }
+
+    func readInput(from point: String) -> String {
+        deliver(message: "좌표를 입력하세요. ")
+        return readLine() ?? ""
+    }
+}
