bd69553097284ac5acfc09c2cf589e093d49d621
Unknown <yago0315@gmail.com>
2019-04-19 15:14:24 +0900

    OutputView 구조체 생성

---
+++ CoordinateCalculator/CoordinateCalculator/OutputView.swift
@@ -0,0 +1,16 @@
+//
+//  OutputView.swift
+//  CoordinateCalculator
+//
+//  Created by 이진영 on 19/04/2019.
+//  Copyright © 2019 Codesquad Inc. All rights reserved.
+//
+
+import Foundation
+
+struct OutputView {
+    static func drawAxis() {
+        print("\(ANSICode.clear)\(ANSICode.home)")
+        print("\(ANSICode.text.whiteBright)\(ANSICode.axis.draw())")
+    }
+}
