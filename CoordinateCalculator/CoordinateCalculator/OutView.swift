5b822c3e8db5ebbf40cc7c5becf81a3bf498c22c
kiljh <kiljh@me.com>
2019-04-19 14:49:19 +0900

    길이 출력하는 글자 색 흰색으로 변경

--- CoordinateCalculator/CoordinateCalculator/OutView.swift
+++ CoordinateCalculator/CoordinateCalculator/OutView.swift
@@ -17,7 +17,7 @@ struct OutputView {
         print("\(ANSICode.text.redBright)\(ANSICode.cursor.move(row:25-point.y, col: point.x*2+3))•\(ANSICode.cursor.move(row:26, col: 0))")
     }
     func drawLine (_ line: MyLine) {
-        print("\(ANSICode.text.redBright)\(ANSICode.cursor.move(row:25-line.pointA.y, col: line.pointA.x*2+3))•\(ANSICode.cursor.move(row:25-line.pointB.y, col: line.pointB.x*2+3))•\(ANSICode.cursor.move(row:26, col: 0))")
+        print("\(ANSICode.text.yellowBright)\(ANSICode.cursor.move(row:25-line.pointA.y, col: line.pointA.x*2+3))•\(ANSICode.cursor.move(row:25-line.pointB.y, col: line.pointB.x*2+3))•\(ANSICode.cursor.move(row:26, col: 0))")
         printLength(line)
         
 //        print(제곱근((A.x - B.x)^제곱 + (A.y - B.y)^제곱)
@@ -27,7 +27,7 @@ struct OutputView {
         let legB = pow(Double(line.pointA.y - line.pointB.y), 2)
         let length = sqrt(legA + legB)
         
-        print("두 점 사이의 거리는  \(length)")
+        print("\(ANSICode.text.whiteBright)두 점 사이의 거리는  \(length)")
     }
     
 }
