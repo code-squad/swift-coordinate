75da91ea86537e5c4f31f932c005aadd2cfd34a9
kiljh <kiljh@me.com>
2019-04-19 15:11:34 +0900

    직선의 길이를 계산하는 메소드를 MyLine으로 위치 변경

--- CoordinateCalculator/CoordinateCalculator/MyLine.swift
+++ CoordinateCalculator/CoordinateCalculator/MyLine.swift
@@ -11,4 +11,11 @@ import Foundation
 struct MyLine {
     var pointA = MyPoint(x: 0, y: 0)
     var pointB = MyPoint(x: 0, y: 0)
+
+    func calculateLength() -> Double {
+        let legA = pow(Double(pointA.x - pointB.x), 2)
+        let legB = pow(Double(pointA.y - pointB.y), 2)
+        
+        return sqrt(legA + legB)
+    }
 }
