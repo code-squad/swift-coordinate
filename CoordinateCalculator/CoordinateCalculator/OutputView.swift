cee543c0e4d418979c8cd5651194eff885f49919
cmindy <minjidev@naver.com>
2019-04-19 14:03:08 +0900

    feat: Add OutputView struct

---
+++ CoordinateCalculator/CoordinateCalculator/OutputView.swift
@@ -0,0 +1,16 @@
+//
+//  OutputView.swift
+//  CoordinateCalculator
+//
+//  Created by CHOMINJI on 19/04/2019.
+//  Copyright © 2019 Codesquad Inc. All rights reserved.
+//
+
+import Foundation
+
+struct OutputView {
+ 
+    func drawAxis() {
+  
+    }
+}
