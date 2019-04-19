5e4682c4d763714479097f4d268ceeb02d0e22b9
kiljh <kiljh@me.com>
2019-04-19 11:36:26 +0900

    Error 메시지 하드코딩 제거

--- CoordinateCalculator/CoordinateCalculator/InputError.swift
+++ CoordinateCalculator/CoordinateCalculator/InputError.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-enum InputError: Error {
-    case NonInputStandard
-    case NonCoordinateStandard
+enum InputError: String, Error {
+    case NonInputStandard = "소괄호로 둘러싸인 x,y 좌표를 정확히 입력해주세요. ex: (4,16)"
+    case NonCoordinateStandard = "x,y 좌표는 0부터 24까지 입력해주세요."
 }
