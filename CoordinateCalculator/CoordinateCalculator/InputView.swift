93b2ef301dae9589f8fda10da489d35589c4e214
cmindy <minjidev@naver.com>
2019-04-19 15:15:20 +0900

    feat: Add InputView Struct to read input

---
+++ CoordinateCalculator/CoordinateCalculator/InputView.swift
@@ -0,0 +1,16 @@
+//
+//  InputView.swift
+//  CoordinateCalculator
+//
+//  Created by CHOMINJI on 19/04/2019.
+//  Copyright © 2019 Codesquad Inc. All rights reserved.
+//
+
+import Foundation
+
+struct InputView {
+    
+    static func readInput() {
+        
+    }
+}
