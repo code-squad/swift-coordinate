bb71309584f8f2ab85629ad7f885a32b4c30299d
Sunghyun Kim <sunghyun_k@icloud.com>
2019-04-20 12:24:28 +0900

    Line 구조체 생성 Line이 초기화될 때 점 사이의 거리가 계산됨

--- CoordinateCalculator/CoordinateCalculator/Line.swift
+++ CoordinateCalculator/CoordinateCalculator/Line.swift
@@ -1,9 +1,23 @@
-//
-//  Line.swift
-//  CoordinateCalculator
-//
-//  Created by 김성현 on 20/04/2019.
-//  Copyright © 2019 Codesquad Inc. All rights reserved.
-//
-
 import Foundation
+
+private func distanceBetween(start: Point, end: Point) -> Double {
+    return sqrt(pow(Double(start.x - end.x), 2) + pow(Double(start.y - end.y), 2))
+}
+
+struct Line {
+    
+    let start: Point
+    let end: Point
+    let distance: Double
+    
+    init(start: Point, end: Point) {
+        self.start = start
+        self.end = end
+        distance = distanceBetween(start: start, end: end)
+    }
+    
+    func description() -> String {
+        return "두 점 사이의 거리는 \(distance)"
+    }
+    
+}
