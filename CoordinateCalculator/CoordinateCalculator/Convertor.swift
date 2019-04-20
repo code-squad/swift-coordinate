6f87d2a4654c754628d8dff429770a108c329c87
kcushy <dev.kcushy@gmail.com>
2019-04-20 15:31:56 +0900

    feat: Add a extension for converting string to int

--- CoordinateCalculator/CoordinateCalculator/Convertor.swift
+++ CoordinateCalculator/CoordinateCalculator/Convertor.swift
@@ -8,7 +8,13 @@
 
 import Foundation
 
-struct Converter {
+extension String {
+    var toInt: Int {
+        return Int(self) ?? 0
+    }
+}
+
+struct Convertor {
     static func remove(bracket: String) -> String {
         let result = bracket.components(separatedBy: ["(", ")"]).joined()
         return result
