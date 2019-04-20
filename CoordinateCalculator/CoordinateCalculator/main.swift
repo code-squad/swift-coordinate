25960523b1d732b8ba3b727312f5cf5b2397d7d8
Sunghyun Kim <sunghyun_k@icloud.com>
2019-04-20 12:24:53 +0900

    main() 함수 수정

--- CoordinateCalculator/CoordinateCalculator/main.swift
+++ CoordinateCalculator/CoordinateCalculator/main.swift
@@ -1,18 +1,32 @@
 import Foundation
 
 func main() {
-    OutputView.drawAxis()
+    let coordinateView = CoordinateView()
     var inputView = InputView()
+    var points: [Point]
+    
     while true {
         do {
-            let coordinate = try inputView.readCoordinate()
-            OutputView.drawPoint(coordinate)
+            points = try inputView.readCoordinates()
+            
+            for point in points {
+                try coordinateView.draw(point: point)
+            }
+            
+            switch points.count {
+            case 2:
+                let line = Line(start: points[0], end: points[1])
+                inputView.printText(line.description())
+            default:
+                break
+            }
+            
+        } catch let error as CoordinateViewError {
+            inputView.printText("좌표 표시기 오류: \(error.description())")
         } catch let error as InputError {
-            print(error.description())
-            inputView.nextLine()
+            inputView.printText("입력 오류: \(error.description())")
         } catch {
-            print("예상치 못한 오류: \(error)")
-            inputView.nextLine()
+            inputView.printText("예상치 못한 오류: \(error)")
         }
     }
     
