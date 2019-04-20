395b64072566b94d752c9b8e705ef379b9f29a91
Sunghyun Kim <sunghyun_k@icloud.com>
2019-04-20 12:21:20 +0900

    이제 Point 이니셜라이저는 좌표값만 받음

--- CoordinateCalculator/CoordinateCalculator/Point.swift
+++ CoordinateCalculator/CoordinateCalculator/Point.swift
@@ -1,23 +1,14 @@
 import Foundation
 
-struct Coordinate {
+struct Point {
     
     //MARK: 속성
-    private(set) var x: Int
-    private(set) var y: Int
+    let x, y: Int
     
-    //MARK: 초기화 함수
-    init?(x: Int, y: Int) {
-        guard
-            x <= ANSICode.axis.axisLimit,
-            y <= ANSICode.axis.axisLimit,
-            x >= 0,
-            y >= 0 else {
-            return nil
-        }
+    init(x: Int, y: Int) {
         self.x = x
         self.y = y
     }
     
-    
 }
+
