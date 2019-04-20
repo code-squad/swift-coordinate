5bd38bd54d8e2e45f3df614ab7ab4726869f5253
Sunghyun Kim <sunghyun_k@icloud.com>
2019-04-20 12:27:47 +0900

    줄바꿈 함수가 이제 출력 기능을 포함함

--- CoordinateCalculator/CoordinateCalculator/InputView.swift
+++ CoordinateCalculator/CoordinateCalculator/InputView.swift
@@ -1,69 +1,37 @@
 import Foundation
 
-extension String {
-    /// String이 Character를 몇개 가지고 있는지 확인합니다.
-    func count(of selectedCharacter: Character) -> Int {
-        var characterCount = 0
-        for character in self {
-            if selectedCharacter == character {
-                characterCount += 1
-            }
-        }
-        return characterCount
-    }
-}
-
 struct InputView {
     
-    //MARK: 속성
-    private var currentLine = 26
+    private var currentLine = ANSICode.CoordinateGrid.gridLimit + 3
     
     //MARK: 비공개 메소드
     mutating private func ask(about question: String) throws -> String {
-        print(ANSICode.cursor.move(row: currentLine, column: 1))
+        returnToCurrentLine()
         print(ANSICode.text.yellow + question)
         guard let input = readLine() else {
             throw InputError.invalidInput
         }
-        nextLine()
+        returnToCurrentLine()
         return input
     }
     
-    private func recognizeCoordinateFormat(input: String) throws -> (x: Int, y: Int) {
-        var userInput = input
-        guard input.count >= 2 else {
-            throw InputError.notEnoughCharacters
-        }
-        guard (userInput.removeFirst(), userInput.removeLast()) == ("(", ")") else {
-            throw InputError.cannotRecognizeParentheses
-        }
-        let separator: Character = ","
-        guard userInput.count(of: separator) == 1 else {
-            throw InputError.cannotIdentifyTwoValues
-        }
-        let separatedValues = userInput.split(separator: separator).map { Int($0) }
-        
-        if separatedValues.contains(nil) {
-            throw InputError.cannotIdentifyNumbers
-        }
-        return (separatedValues[0]!, separatedValues[1]!)
-    }
-    
-    //MARK: 메소드
-    mutating func readCoordinate() throws -> Coordinate {
-        let input = try ask(about: "좌표를 입력하세요.")
-        let coordinateValue = try recognizeCoordinateFormat(input: input)
-        guard let coordinate = Coordinate(x: coordinateValue.x, y: coordinateValue.y) else {
-            throw InputError.exceededAxisLimit
-        }
-        return coordinate
-    }
-    
-    mutating func nextLine() {
+    mutating private func returnToCurrentLine() {
+        print("\(ANSICode.cursor.move(row: currentLine, column: 1))", terminator: "")
         currentLine += 1
     }
     
+    //MARK: 메소드
+    mutating func readCoordinates() throws -> [Point] {
+        
+        let input = try ask(about: "좌표를 입력하세요.")
+        return try InputControl.points(input: input)
+        
+    }
     
+    mutating func printText(_ text: String) {
+        returnToCurrentLine()
+        print(text)
+    }
     
     
 }
