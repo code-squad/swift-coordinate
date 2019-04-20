de3742d7495dc5c84410f1f477dcc10559553439
Sunghyun Kim <sunghyun_k@icloud.com>
2019-04-20 18:10:42 +0900

    CoordinatePlane 구조체 추가

---
+++ CoordinateCalculator/CoordinateCalculator/CoordinatePlane.swift
@@ -0,0 +1,25 @@
+//
+//  CoordinatePlane.swift
+//  CoordinateCalculator
+//
+//  Created by 김성현 on 20/04/2019.
+//  Copyright © 2019 Codesquad Inc. All rights reserved.
+//
+
+import Foundation
+
+struct CoordinatePlane {
+    
+    let gridLimit = ANSICode.CoordinateGrid.gridLimit
+    var points = [Point]()
+    var shapes = [Shape]()
+    
+    func add(point: Point) {
+        
+    }
+    
+    func add(shape: Shape) {
+        
+    }
+    
+}
