8ddc8d98a8917b31b8b0f9fe5372a3253c9b711d
gukjin jang <jgj455@gmail.com>
2019-04-22 22:51:21 +0900

    Modify : InputVuew 구조체 내부에 있는 readInput() 함수에 입력받는 부분만 남기고 문자열 포함에 대한 검증은 containDistinct() 함수를 생성하여 구현

--- CoordinateCalculator/CoordinateCalculator/InputView.swift
+++ CoordinateCalculator/CoordinateCalculator/InputView.swift
@@ -12,8 +12,12 @@ struct InputView{
     func readInput() -> String?{
         print("두좌표를 입력하세요 (두 숫자는 ,로 구분해주고, 두 좌표 간에는 -로 구분해주세요)")
         let inputLocation = readLine()
-        let charactersInput = CharacterSet(charactersIn: "0123456789,()-").inverted
-        if ((inputLocation?.rangeOfCharacter(from: charactersInput)) == nil){ return inputLocation }
+        return inputLocation
+    }
+    
+    func distinctContain(inputLocation : String?) -> String?{
+        let notContainedInput = CharacterSet(charactersIn: "0123456789,()-").inverted
+        if ((inputLocation?.rangeOfCharacter(from: notContainedInput)) == nil){ return inputLocation }
         else { return nil }
     }
 }
