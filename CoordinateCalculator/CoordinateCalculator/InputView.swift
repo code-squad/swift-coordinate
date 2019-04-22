e4c44e0913674bea94a06f83b68a92c265716f6d
gukjin jang <jgj455@gmail.com>
2019-04-23 00:13:51 +0900

    Modify : Distinct 구조체 내부에 splitLocation() 함수를 입력된 좌표의 개수를 판단하는 역할만 남기고 twoLocations() 함수를 생성하여 두개의 좌표가 입력되었을때 값을 리턴할 수 있는 함수를 생성, OutputView 구조체 내부의 출력함수를 입력된 좌표의 개수에 따라 다르게 출력될 수 있도록 세분화, 기타 변수명 및 오타 수정, 주석 추가, main() 함수 수정

--- CoordinateCalculator/CoordinateCalculator/InputView.swift
+++ CoordinateCalculator/CoordinateCalculator/InputView.swift
@@ -9,12 +9,13 @@
 import Foundation
 
 struct InputView{
+    /// 입력을 받는 함수
     func readInput() -> String?{
-        print("두좌표를 입력하세요 (두 숫자는 ,로 구분해주고, 두 좌표 간에는 -로 구분해주세요)")
+        print("좌표를 입력하세요 (두 숫자는 ,로 구분해주고, 좌표 간에는 -로 구분해주세요)")
         let inputLocation = readLine()
         return inputLocation
     }
-    
+    /// 입력에 포함되지 않는 문자가 있는지 판단하는 함수
     func distinctContain(inputLocation : String?) -> String?{
         let notContainedInput = CharacterSet(charactersIn: "0123456789,()-").inverted
         if ((inputLocation?.rangeOfCharacter(from: notContainedInput)) == nil){ return inputLocation }
--- CoordinateCalculator/CoordinateCalculator/main.swift
+++ CoordinateCalculator/CoordinateCalculator/main.swift
@@ -13,17 +13,29 @@ func main(){
     var myPoint : MyPoint
     let distinct = Distinct()
     let input = InputView()
+    let output = OutputView()
     while true {
         let inputText = input.readInput()
-        let containDistinct = input.distinctContain(inputLocation: inputText)
-        if inputText != nil {
+        let distinctInput = input.distinctContain(inputLocation: inputText)
+        if distinctInput != nil {
             do{
-                myLine = try distinct.splitLocation(twoLocations: inputText)
-                OutputView().drawAxis(myLine: myLine)
+                let inputLocation = try distinct.splitLocation(inputTexts: distinctInput)
+                switch inputLocation.loctionCount {
+                case 2 : // 입력된 Location의 갯수가 2개인 경우
+                    myLine = try distinct.twoLocations(dividedLocations: inputLocation.dividedLocations)
+                    output.drawAxis()
+                    output.printMyLine(myLine: myLine)
+                case 1 : // 입력된 Location의 갯수가 1개인 경우
+                    myPoint = try distinct.location(locationText: inputLocation.dividedLocations[0])// 원소가 1개이지만 배열을 벗기기 위해 [0]을 사용 (해당 함수는 1개일때 뿐만 아니라 2개일때 각 좌표들에 값을 대입하기 위한 함수로도 쓰이기 때문에)
+                    output.drawAxis()
+                    output.printMyPoint(myPoint: myPoint)
+                default :
+                    throw ErrorMessage.outOfRangeError
+                }
             }catch ErrorMessage.noValueError {
                 print("올바른 값을 입력하세요")
             }catch ErrorMessage.outOfRangeError {
-                print("법위를 넘었습니다")
+                print("범위를 넘었습니다")
             }catch ErrorMessage.typeMissError {
                 print("정수를 입력하세요")
             }catch ErrorMessage.nonbracket {
