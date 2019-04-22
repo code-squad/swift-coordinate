749470e3d2e6f96129b97d710482c6ada23ee582
gukjin jang <jgj455@gmail.com>
2019-04-22 16:59:10 +0900

    Add : InputView 구조체 내에 readInput() 함수에 CharacterSet을 활용하여 입력할 수 없는 문자가 포함되어있는지 판단하는 구문 추가

--- CoordinateCalculator/CoordinateCalculator/InputView.swift
+++ CoordinateCalculator/CoordinateCalculator/InputView.swift
@@ -10,8 +10,14 @@ import Foundation
 
 struct InputView{
     func readInput() -> String?{
-        print("좌표를 입력하세요 (두 숫자는 ,로 구분해주세요)")
-        let inputLocation = readLine()
-        return inputLocation
+        while true {
+            print("두좌표를 입력하세요 (두 숫자는 ,로 구분해주고, 두 좌표 간에는 -로 구분해주세요)")
+            let inputLocation = readLine()
+            let charactersInput = CharacterSet(charactersIn: "0123456789,()-").inverted
+            if ((inputLocation?.rangeOfCharacter(from: charactersInput)) == nil){
+                return inputLocation
+                break
+            }
+        }
     }
 }
