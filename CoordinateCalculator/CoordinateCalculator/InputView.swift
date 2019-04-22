6087b6f28de45b2c03aa4043e186782f7ba8a3dc
gukjin jang <jgj455@gmail.com>
2019-04-22 22:42:00 +0900

    Modify : InputView 구조체 내부에 있는 readInput() 함수에 있는 while문을 main() 함수 최상위로 이동

--- CoordinateCalculator/CoordinateCalculator/InputView.swift
+++ CoordinateCalculator/CoordinateCalculator/InputView.swift
@@ -10,14 +10,10 @@ import Foundation
 
 struct InputView{
     func readInput() -> String?{
-        while true {
-            print("두좌표를 입력하세요 (두 숫자는 ,로 구분해주고, 두 좌표 간에는 -로 구분해주세요)")
-            let inputLocation = readLine()
-            let charactersInput = CharacterSet(charactersIn: "0123456789,()-").inverted
-            if ((inputLocation?.rangeOfCharacter(from: charactersInput)) == nil){
-                return inputLocation
-                break
-            }
-        }
+        print("두좌표를 입력하세요 (두 숫자는 ,로 구분해주고, 두 좌표 간에는 -로 구분해주세요)")
+        let inputLocation = readLine()
+        let charactersInput = CharacterSet(charactersIn: "0123456789,()-").inverted
+        if ((inputLocation?.rangeOfCharacter(from: charactersInput)) == nil){ return inputLocation }
+        else { return nil }
     }
 }
--- CoordinateCalculator/CoordinateCalculator/main.swift
+++ CoordinateCalculator/CoordinateCalculator/main.swift
@@ -12,20 +12,25 @@ func main(){
     var myLine : MyLine
     var myPoint : MyPoint
     let distinct = Distinct()
-    let input = InputView().readInput()
-    do{
-        myLine = try distinct.splitLocation(twoLocations: input)
-        OutputView().drawAxis(myLine: myLine)
-    }catch ErrorMessage.noValueError {
-        print("올바른 값을 입력하세요")
-    }catch ErrorMessage.outOfRangeError {
-        print("법위를 넘었습니다")
-    }catch ErrorMessage.typeMissError {
-        print("정수를 입력하세요")
-    }catch ErrorMessage.nonbracket {
-        print("소괄호를 씌워주세요")
-    }catch {
-        print("알수없는 에러입니다.")
+    while true {
+        let input = InputView().readInput()
+        if input != nil {
+            do{
+                myLine = try distinct.splitLocation(twoLocations: input)
+                OutputView().drawAxis(myLine: myLine)
+            }catch ErrorMessage.noValueError {
+                print("올바른 값을 입력하세요")
+            }catch ErrorMessage.outOfRangeError {
+                print("법위를 넘었습니다")
+            }catch ErrorMessage.typeMissError {
+                print("정수를 입력하세요")
+            }catch ErrorMessage.nonbracket {
+                print("소괄호를 씌워주세요")
+            }catch {
+                print("알수없는 에러입니다.")
+            }
+            break
+        }
     }
 }
 
