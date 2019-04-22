7866ae6cd4ec57344e58f85490445cbcc7307ff4
cmindy <minjidev@naver.com>
2019-04-22 19:54:27 +0900

    feat: Add Validator functions to validate user input
    기존 InputView에 있던 검증 함수들을 Validator로 이동하였습니다.

--- CoordinateCalculator/CoordinateCalculator/Validator.swift
+++ CoordinateCalculator/CoordinateCalculator/Validator.swift
@@ -10,13 +10,54 @@ import Foundation
 
 struct Validator {
     
-    static func hasComma(coordinates: [String]) -> Bool {
+    static func validateInput(of prompt: String) -> (x: Int, y: Int)? {
+        
+        guard let separatedValue = separateCoordinates(of: prompt) else {
+            return nil
+        }
+        
+        let numericValue = convertToInt(of: separatedValue)
+        
+        guard let coordinates = checkCoordinatesLimit(numericValue.x, numericValue.y) else {
+            return nil
+        }
+        
+        return coordinates
+    }
+    
+    static func separateCoordinates(of prompt: String) -> [String]? {
+        
+        let input = prompt.components(separatedBy: CharacterSet(charactersIn: "()")).joined()
+        let coordinates = input.components(separatedBy: ",")
         
         guard coordinates.count > 1 else {
             print("값을 쉼표(,)로 구분해주세요~!~!")
-            return false
+            return nil
         }
-        return true
         
+        return coordinates
+    }
+    
+    static private func convertToInt(of input: [String]) -> (x: Int, y: Int) {
+    
+        let inputNum = input.map { Int($0) ?? -1 }
+        
+        
+        return (inputNum[0], inputNum[1])
+    }
+    
+    static func checkCoordinatesLimit(_ x: Int, _ y: Int) -> (x: Int, y: Int)? {
+        
+        guard x < 25, y < 25 else {
+            print("x, y 값은 최대 24까지 입력할 수 있습니다.")
+            return nil
+        }
+        
+        guard x > 0 , y > 0 else {
+            print("숫자만 입력해주세요.")
+            return nil
+        }
+        
+        return (x, y)
     }
 }
