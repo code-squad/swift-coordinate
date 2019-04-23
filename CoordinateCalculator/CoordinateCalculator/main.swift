8f16ce92fa5eff982de1ca58e2e79be8a3690107
gukjin jang <jgj455@gmail.com>
2019-04-23 12:03:17 +0900

    Modify : ErrorMessage 열거형에 값을 넣어서 main() 함수에서 출력하도록 변경

--- CoordinateCalculator/CoordinateCalculator/main.swift
+++ CoordinateCalculator/CoordinateCalculator/main.swift
@@ -9,11 +9,8 @@
 import Foundation
 
 func main(){
-    var myLine : MyLine
-    var myPoint : MyPoint
     let distinct = Distinct()
     let input = InputView()
-    let output = OutputView()
     while true {
         let inputText = input.readInput()
         let distinctInput = input.distinctContain(inputLocation: inputText)
@@ -21,14 +18,8 @@ func main(){
             do{
                 let inputLocation = try distinct.splitLocation(inputTexts: distinctInput)
                 try distinct.callDependingCoordinates(locationCount: inputLocation.loctionCount, dividedLocations: inputLocation.dividedLocations)
-            }catch ErrorMessage.noValueError {
-                print("올바른 값을 입력하세요")
-            }catch ErrorMessage.outOfRangeError {
-                print("범위를 넘었습니다")
-            }catch ErrorMessage.typeMissError {
-                print("정수를 입력하세요")
-            }catch ErrorMessage.nonbracket {
-                print("소괄호를 씌워주세요")
+            }catch let error as ErrorMessage {
+                print(error.rawValue)
             }catch {
                 print("알수없는 에러입니다.")
             }
