fcd8092a5204b38c52a14e081750d80752d0a9ef
cmindy <minjidev@naver.com>
2019-04-23 12:20:19 +0900

    feat: Add validate function that input has possible characters

--- CoordinateCalculator/CoordinateCalculator/Validator.swift
+++ CoordinateCalculator/CoordinateCalculator/Validator.swift
@@ -11,10 +11,16 @@ import Foundation
 struct Validator {
     
     static let AxisLimit = ANSICode.axis.AxisLimit
+    static let possibleCharacters = CharacterSet(charactersIn: "(),-").union(CharacterSet.decimalDigits)
     
     static func validateInput(of prompt: String) -> (x: Int, y: Int)? {
         
         guard !prompt.isEmpty else {
+            return nil
+        }
+        
+        guard uncontainsPossibleCharacter(in: prompt) else {
+            print("사용할 수 없는 문자가 입력되었습니다. 숫자(0-9), 괄호(), 쉼표(,), 대시(-)만을 사용해주세요.")
             return nil
         }
         
@@ -29,6 +35,18 @@ struct Validator {
         }
         
         return coordinates
+    }
+    
+    /// prompt에 입력된 문자열 중에 입력할 수 없는 문자가 포함되어 있는지 확인합니다.
+    ///
+    /// prompt 문자열 Set에 대한 possibleCharater Set의 차집합을 구하고 차집합이 비어있으면 true를 반환합니다.
+    /// - Returns: 입력할 수 없는 문자가 포함되어있지 않으면 true를 반환하고, 포함되어 있으면 false를 반환합니다.
+    static private func uncontainsPossibleCharacter(in prompt: String) -> Bool {
+        
+        let promptCharacters = CharacterSet(charactersIn: prompt)
+        let differenceCharacters = promptCharacters.subtracting(possibleCharacters)
+        
+        return differenceCharacters.isEmpty
     }
     
     /// prompt에 입력된 값을 쉼표로 구분해 반환합니다.
