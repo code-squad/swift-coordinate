99490b5d2512218266fcb7861ad55ea0cb710015
cmindy <minjidev@naver.com>
2019-04-23 11:33:21 +0900

    feat: Add MyLine Struct to handling two coordinates

---
+++ CoordinateCalculator/CoordinateCalculator/MyLine.swift
@@ -0,0 +1,14 @@
+//
+//  MyLine.swift
+//  CoordinateCalculator
+//
+//  Created by CHOMINJI on 23/04/2019.
+//  Copyright © 2019 Codesquad Inc. All rights reserved.
+//
+
+import Foundation
+
+struct MyLine {
+    private(set) var pointA = MyPoint(x: 0, y: 0)
+    private(set) var pointB = MyPoint(x: 0, y: 0)
+}
