843409716c44b1190ca59eb7dc2b59cc5cda32fc
cmindy <minjidev@naver.com>
2019-04-23 16:53:01 +0900

    feat: Add Converter struct to convert to MyPoint

---
+++ CoordinateCalculator/CoordinateCalculator/Converter.swift
@@ -0,0 +1,13 @@
+//
+//  Converter.swift
+//  CoordinateCalculator
+//
+//  Created by CHOMINJI on 23/04/2019.
+//  Copyright © 2019 Codesquad Inc. All rights reserved.
+//
+
+import Foundation
+
+struct Converter {
+    
+}
