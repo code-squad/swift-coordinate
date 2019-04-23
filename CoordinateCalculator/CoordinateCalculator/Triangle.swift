18047c1b66dc670d753cec603e611769fc05b454
Sunghyun Kim <sunghyun_k@icloud.com>
2019-04-23 13:29:23 +0900

    삼각형의 넓이를 구하는 연산속성 추가

--- CoordinateCalculator/CoordinateCalculator/Triangle.swift
+++ CoordinateCalculator/CoordinateCalculator/Triangle.swift
@@ -16,7 +16,7 @@ struct Triangle: Drawable {
             ANSICode.CoordinateGrid.gridRange.contains(lineAB.end.x),
             ANSICode.CoordinateGrid.gridRange.contains(lineAB.end.y),
             ANSICode.CoordinateGrid.gridRange.contains(lineAC.end.x),
-            ANSICode.CoordinateGrid.gridRange.contains(lineAC.end.x){
+            ANSICode.CoordinateGrid.gridRange.contains(lineAC.end.x) {
             return true
         }
         return false
@@ -28,13 +28,17 @@ struct Triangle: Drawable {
     
     var description: String {
         //TODO: 삼각형 설명 입력하기
-        return ""
+        return "삼각형의 넓이: \(size)"
     }
     
     var lineAB: Line
     var lineAC: Line
     var lineBC: Line
     
+    var size: Double {
+        let s = (lineAB.distance + lineAC.distance + lineBC.distance) / 2
+        return sqrt(s * (s - lineAB.distance) * (s - lineAC.distance) * (s - lineBC.distance))
+    }
     
     init(pointA: Point, pointB: Point, pointC: Point) {
         lineAB = Line(start: pointA, end: pointB)
