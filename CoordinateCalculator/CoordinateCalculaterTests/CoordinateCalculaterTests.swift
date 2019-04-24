c910289875aa16a7e68c19787372cf4b29e6bb46
O-O-wl <ldcpaul@hanmail.net>
2019-04-24 20:12:06 +0900

    Modified: 정규표현식을 이용한 좌표 값 추출

--- CoordinateCalculator/CoordinateCalculaterTests/CoordinateCalculaterTests.swift
+++ CoordinateCalculator/CoordinateCalculaterTests/CoordinateCalculaterTests.swift
@@ -10,24 +10,27 @@ import XCTest
 
 class CoordinateCalculaterTests: XCTestCase {
 
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
+    /*
+     메소드가 너무 많은 일을 하지 않도록 분리하기 위해 노력해 본다.
+     각 객체의 역할과 책임을 구분하도록 노력해 본다.
+     입력을 처리하는 InputView 구조체를 추가하고, 아래 실행 결과처럼 좌표값 입력하는 메뉴를 구현한다.
+     main에서 InputView 구조체 readInput() 함수를 호출한다.
+     입력받은 좌표값 X, Y 정보를 처리하기 위한 MyPoint 구조체를 구현한다.
+     MyPoint 구조체는 다음과 같은 형태를 가진다.
+     
+     좌표를 입력하세요.
+     (10,10)
+     
+     struct MyPoint {
+     var x = 0
+     var y = 0
+     }
+     MyPoint 구조체 인스턴스를 OutputView에 넘겨서 출력하는 함수를 추가한다.
+     화면 처리 유의사항
+     터미널 화면은 뒤집힌 1사분면으로 동작한다. 위쪽이 0이고 아래로 내려올수록 커진다.
+     X좌표축은 화면 비율상 2를 곱해서 표현해야 한다.
+     X좌표축 범례는 Y축으로 25만큼 떨어진 공간에 그려진다.
+     Y좌표축은 터미널 좌표상 3만큼 떨어져서 그려진다.
+    */
 
 }
--- CoordinateCalculator/CoordinateCalculator/InputView.swift
+++ CoordinateCalculator/CoordinateCalculator/InputView.swift
@@ -8,15 +8,19 @@
 
 import Foundation
 
+typealias StringPoint = (String,String)
 
 struct InputView{
     
     enum Question : String{
         case aboutCoordinate  = "좌표를 입력하세요."
     }
-
+    
     func readInput() throws {
         let anwser = try ask(Question.aboutCoordinate)
+        anwser.isCorrectFormat()
+        let point = try anwser.getPointValue()
+        print(point)
     }
     
     func ask(_ question:Question) throws -> String {
@@ -27,6 +31,27 @@ struct InputView{
         return anwser
     }
     
-   
+    
 }
 
+extension String{
+    func isCorrectFormat() throws ->(Bool){
+        guard let regex = try? NSRegularExpression.init(pattern: "^([0-9,]+)", options: []) else {
+            return false
+        }
+        
+       return true
+    }
+    
+    func getPointValue() throws -> StringPoint {
+        guard let regex = try? NSRegularExpression.init(pattern: "[0-9]+", options: []) else {
+            throw Exception.ErrorType.wrongFormat
+        }
+        let matchs = regex.matches(in: self, options: [], range: NSRange.init(location: 0, length: self.count))
+        let x = NSString.init(string:self).substring(with: (matchs[0].range))
+        let y = NSString.init(string:self).substring(with: (matchs[1].range))
+        return (x,y)
+    }
+    
+    func checkRange(){}
+}
