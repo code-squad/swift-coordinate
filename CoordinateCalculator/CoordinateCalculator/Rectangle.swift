b70e08504ff5e1be2827867e30fc3944f479f1ea
Sunghyun Kim <sunghyun_k@icloud.com>
2019-04-24 09:55:46 +0900

    Rectangle이 points 배열과 size 연산 속성을 가지도록 작성

--- CoordinateCalculator/CoordinateCalculator/Rectangle.swift
+++ CoordinateCalculator/CoordinateCalculator/Rectangle.swift
@@ -1,14 +1,22 @@
 import Foundation
 
-struct Rectangle {
+struct Rectangle: Drawable {
+    
+    var points: [Point] {
+        return [leftTop, rightBottom, Point(x: leftTop.x, y: rightBottom.y), Point(x: rightBottom.x, y: leftTop.y)]
+    }
     
     var leftTop: Point
     var rightBottom: Point
     
+    var size: Int {
+        return (rightBottom.x - leftTop.x) * (leftTop.y - rightBottom.y)
+    }
+    
     init(origin: Point, size: CGSize) {
         
         leftTop = origin
-        rightBottom = Point(x: origin.x + Int(size.width), y: origin.y + Int(size.height))
+        rightBottom = Point(x: origin.x + Int(size.width), y: origin.y - Int(size.height))
         
     }
     
