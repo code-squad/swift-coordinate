19ba1f7f3f1693d2b4e4005ca7b78f88ab044d54
Daheen Lee <allwhite.dev@gmail.com>
2019-04-25 21:31:57 +0900

    feat: Add new error type in CoordinateError - impossibleToConvert

--- CoordinateCalculator/CoordinateCalculator/CoordinateError.swift
+++ CoordinateCalculator/CoordinateCalculator/CoordinateError.swift
@@ -9,8 +9,9 @@
 import Foundation
 
 enum CoordinateError: String, Error {
-    case invalidInputFormat = "잘못된 형식의 입력입니다. 다시 입력해주세요."
-    case impossibleToParse  = "변환 불가능한 입력입니다. 다시 입력해주세요. "
-    case outOfAxisRange     = "좌표 범위를 벗어난 입력입니다. 다시 입력해주세요. "
+    case invalidInputFormat  = "잘못된 형식의 입력입니다. 다시 입력해주세요."
+    case impossibleToParse   = "변환 불가능한 입력입니다. 다시 입력해주세요. "
+    case outOfAxisRange      = "좌표 범위를 벗어난 입력입니다. 다시 입력해주세요. "
+    case impossibleToConvert = "정수로 변환할 수 없습니다. 다시 입력해주세요."
 }
 
