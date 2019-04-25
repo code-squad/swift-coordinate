776685d45487d446eb5e0574197bea670b45e4a2
kcushy <dev.kcushy@gmail.com>
2019-04-26 02:58:11 +0900

    feat: Add a function to draw

--- CoordinateCalculator/CoordinateCalculator/OutputView.swift
+++ CoordinateCalculator/CoordinateCalculator/OutputView.swift
@@ -10,13 +10,42 @@ import Foundation
 
 struct OutputView {
 
-    static func drawAxis() {
-        let quadrantOne = ANSICode.axis.draw()
+    /**
+    draw a Axix.
+    */
+    private static func drawAxis() {
         formatScreen()
-        print("\(ANSICode.text.yellow)\(quadrantOne)")
+        let quadrantOne = ANSICode.axis.draw()
+        print("\(ANSICode.text.whiteBright)\(quadrantOne)")
     }
 
+    /**
+     clear a screen
+    */
     private static func formatScreen() {
         print("\(ANSICode.clear)\(ANSICode.home)")
+    }
+
+    /**
+     mark a sign at axix.
+
+     - parameters:
+         - point: MyPoint
+    */
+    static func mark(at point: MyPoint) {
+        print(point)
+        let standardRow = 2 * point.positionX
+        let axisBound = ANSICode.axis.AxisLimit + 1
+        let sign = "📌"
+        drawAxis()
+        print("\(ANSICode.cursor.move(row: axisBound - point.positionY, col: standardRow + 3))\(sign)")
+        // for showing full axis. If not, broken screen.
+        print("\(ANSICode.cursor.move(row: axisBound + 3, col: axisBound + 3))")
+
+    }
+
+    static func showError(message: Validation) -> Bool {
+        print(message)
+        return true
     }
 }
