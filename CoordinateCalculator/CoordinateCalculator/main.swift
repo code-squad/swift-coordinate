f2dcdff842654fe4ab70f1b9854760e3707ed074
Daheen Lee <allwhite.dev@gmail.com>
2019-04-25 16:27:29 +0900

    refactor: Modify main.swift to have main process

--- CoordinateCalculator/CoordinateCalculator/main.swift
+++ CoordinateCalculator/CoordinateCalculator/main.swift
@@ -8,4 +8,28 @@
 
 import Foundation
 
-CoordinateCalulator.run()
+//CoordinateCalulator.run()
+
+while true {
+    var numbers: (Int, Int)!
+    do {
+        let input = try InputView.readInput()
+        numbers  = try Parser.parse(using: input)
+        if CoordinateValidator.isOutOfAxis(using: numbers) {
+            throw CoordinateError.outOfAxisRange
+        }
+    } catch let e as CoordinateError {
+        print(e.rawValue)
+        continue
+    } catch {
+        print("Other Unexpected Error")
+        break
+    }
+    
+    let point = MyPoint(x: numbers.0, y: numbers.1)
+    OutputView.drawAxis()
+    OutputView.drawYellowDot(at: point)
+    break;
+}
+
+
