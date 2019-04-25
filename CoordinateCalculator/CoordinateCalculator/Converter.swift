41cdc400b82561a60df0cfaa67b07b634596c361
cmindy <minjidev@naver.com>
2019-04-25 16:02:47 +0900

    feat: Add Converter struct for convert input to mypoint array

--- CoordinateCalculator/CoordinateCalculator/Converter.swift
+++ CoordinateCalculator/CoordinateCalculator/Converter.swift
@@ -10,4 +10,19 @@ import Foundation
 
 struct Converter {
     
+    static func toMyPoints(using input: String) -> [MyPoint] {
+
+        var myPoints : [MyPoint] = []
+        
+        let coordinates = input.components(separatedBy: "-").map { $0.trimmingCharacters(in: CharacterSet(charactersIn: "()")).components(separatedBy: ",") }
+        
+        for coordinate in coordinates {
+            
+            let point = coordinate.map { Int($0)! }
+            
+            myPoints.append(MyPoint(x:point[0], y: point[1]))
+        }
+        
+        return myPoints
+    }
 }
