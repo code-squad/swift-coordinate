aac09108efb1b533850d8920dd23775299238a00
kiljh <kiljh@me.com>
2019-04-25 21:32:19 +0900

    Refactoring FigureFactory

--- CoordinateCalculator/CoordinateCalculator/FigureFactory.swift
+++ CoordinateCalculator/CoordinateCalculator/FigureFactory.swift
@@ -9,85 +9,56 @@
 import Foundation
 
 struct FigureFactory {
-    static func getFigure(_ values: [String]) throws -> Figure {
-        switch values.count {
-        case 1: return inputToPoint(values[0])
-        case 2: return MyLine(pointA: inputToPoint(values[0]), pointB: inputToPoint(values[1]))
-        case 3: return MyTriangle(pointA: inputToPoint(values[0]), pointB: inputToPoint(values[1]), pointC: inputToPoint(values[2]))
+    static func getFigure(_ coordinates: [(x: Int, y: Int)]) throws -> Figure {
+        switch coordinates.count {
+        case 1: return MyPoint(x: coordinates[0].x, y: coordinates[0].y)
+        case 2: return MyLine(pointA: MyPoint(x: coordinates[0].x, y: coordinates[0].y), pointB: MyPoint(x: coordinates[1].x, y: coordinates[1].y))
+        case 3: return MyTriangle(pointA: MyPoint(x: coordinates[0].x, y: coordinates[0].y), pointB: MyPoint(x: coordinates[1].x, y: coordinates[1].y), pointC: MyPoint(x: coordinates[2].x, y: coordinates[2].y))
         case 4:
-            let rectangleInfo = try verifyRectangle(values)
+            let rectangleInfo = try verifyRectangle(coordinates)
             return MyRect(origin: rectangleInfo.0, size: rectangleInfo.1)
         default: throw InputError.UndefinedFigure
         }
-    }
-
-    static private func inputToPoint (_ valueEntered: String) -> MyPoint {
-        var value = valueEntered
-        
-        value.removeFirst()
-        value.removeLast()
-        
-        let coordinate = value.split(separator: ",")
-        let coordinateX: Int = Int(String(coordinate[0])) ?? 0
-        let coordinateY: Int = Int(String(coordinate[1])) ?? 0
-        
-        return MyPoint(x: coordinateX, y: coordinateY)
     }
     
     static private func inputToRectangle (_ point: MyPoint, _ size: CGSize) -> MyRect {
         return MyRect(origin: point, size: size)
     }
     
-    static private func verifyRectangle (_ valueEntered: [String]) throws -> (MyPoint, CGSize) {
-        let minMaxCoordinates = getMinMaxCoordinate(valueEntered)
-        try countCheckMinMaxCoordinate(valueEntered, minMaxCoordinates)
+    static private func verifyRectangle (_ coordinates: [(x: Int, y: Int)]) throws -> (MyPoint, CGSize) {
+        let minMax = getMinMax(coordinates)
+        try countCheckMinMax(coordinates, minMax)
         
-        let minX = minMaxCoordinates[0]
-        let maxX = minMaxCoordinates[1]
-        let minY = minMaxCoordinates[2]
-        let maxY = minMaxCoordinates[3]
+        let minX = minMax[0]
+        let maxX = minMax[1]
+        let minY = minMax[2]
+        let maxY = minMax[3]
         
         return (MyPoint(x: minX, y: minY), CGSize.init(width: maxX-minX, height: maxY-minY))
     }
     
-    static private func getMinMaxCoordinate (_ valueEntered: [String]) -> [Int] {
+    static private func getMinMax (_ coordinates: [(x: Int, y: Int)]) -> [Int] {
         var maxX = 0; var minX = 24; var minY = 24; var maxY = 0
-        var coordinate: [Substring]; var coordinateX: Int; var coordinateY: Int
         
-        for valueOrigin in valueEntered {
-            var value = valueOrigin
-            value.removeFirst()
-            value.removeLast()
-            coordinate = value.split(separator: ",")
-            coordinateX = Int(String(coordinate[0])) ?? 0
-            coordinateY = Int(String(coordinate[1])) ?? 0
-            
-            if coordinateX > maxX { maxX = coordinateX }
-            if coordinateX < minX { minX = coordinateX }
-            if coordinateY > maxY { maxY = coordinateY }
-            if coordinateY < minY { minY = coordinateY }
+        for coordinate in coordinates {
+            if coordinate.x > maxX { maxX = coordinate.x }
+            if coordinate.x < minX { minX = coordinate.x }
+            if coordinate.y > maxY { maxY = coordinate.y }
+            if coordinate.y < minY { minY = coordinate.y }
         }
         
         return [minX, maxX, minY, maxY]
     }
     
-    static private func countCheckMinMaxCoordinate (_ valueEntered: [String], _ minMaxCoordinates: [Int]) throws
+    static private func countCheckMinMax (_ coordinates: [(x: Int, y: Int)], _ minMaxCoordinates: [Int]) throws
     {
         var minXCount = 0; var maxXCount = 0; var minYCount = 0; var maxYCount = 0
-        var coordinate: [Substring]; var coordinateX: Int; var coordinateY: Int
     
-        for valueOrigin in valueEntered {
-            var value = valueOrigin
-            value.removeFirst()
-            value.removeLast()
-            coordinate = value.split(separator: ",")
-            coordinateX = Int(String(coordinate[0])) ?? 0
-            coordinateY = Int(String(coordinate[1])) ?? 0
-            
-            if coordinateX == minMaxCoordinates[0] { minXCount += 1 }
-            if coordinateX == minMaxCoordinates[1] { maxXCount += 1 }
-            if coordinateY == minMaxCoordinates[2] { minYCount += 1 }
-            if coordinateY == minMaxCoordinates[3] { maxYCount += 1 }
+        for coordinate in coordinates {
+            if coordinate.x == minMaxCoordinates[0] { minXCount += 1 }
+            if coordinate.x == minMaxCoordinates[1] { maxXCount += 1 }
+            if coordinate.y == minMaxCoordinates[2] { minYCount += 1 }
+            if coordinate.y == minMaxCoordinates[3] { maxYCount += 1 }
         }
         
         if maxXCount != 2 || maxYCount != 2 || minXCount != 2 || minYCount != 2 {
