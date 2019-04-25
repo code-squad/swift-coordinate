9d0946d5419c3723554c01fec351c22a54e092b5
kiljh <kiljh@me.com>
2019-04-25 17:16:36 +0900

    Create MyRect struct

---
+++ CoordinateCalculator/CoordinateCalculator/MyRect.swift
@@ -0,0 +1,15 @@
+//
+//  MyRect.swift
+//  CoordinateCalculator
+//
+//  Created by joon-ho kil on 4/25/19.
+//  Copyright © 2019 Codesquad Inc. All rights reserved.
+//
+
+import Foundation
+
+struct MyRect {
+    var leftTop = MyPoint()
+    var rightBottom = MyPoint()
+    
+}
