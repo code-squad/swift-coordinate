ea42e8859fcc11167132238a6048ad3a32e4ca39
Kroisttt <yago0315@gmail.com>
2019-04-25 16:40:56 +0900

    Drawable 프로토콜 추가

---
+++ CoordinateCalculator/CoordinateCalculator/Drawable.swift
@@ -0,0 +1,13 @@
+//
+//  Drawable.swift
+//  CoordinateCalculator
+//
+//  Created by 이진영 on 25/04/2019.
+//  Copyright © 2019 Codesquad Inc. All rights reserved.
+//
+
+import Foundation
+
+protocol Drawable {
+    
+}
