b8c6ed5fa56005ec45a176f9903081af965041c4
gukjin jang <jgj455@gmail.com>
2019-04-27 02:50:10 +0900

    Modify : MyRect 구조체 수정 및 Distinct 구조체 내부에 좌표가 4개 입력받았을떄 직사각형이 되는지 판단하고 MyRect에 값을 입력하는 함수 구현

--- CoordinateCalculator/CoordinateCalculator/ErrorMessage.swift
+++ CoordinateCalculator/CoordinateCalculator/ErrorMessage.swift
@@ -13,4 +13,5 @@ enum ErrorMessage : String, Error {
     case typeMissError = "정수를 입력하세요"
     case outOfRangeError = "범위를 넘었습니다"
     case nonbracket = "소괄호를 씌워주세요"
+    case noRectangle = "네 점이 직사각형이 되지 않습니다"
 }
