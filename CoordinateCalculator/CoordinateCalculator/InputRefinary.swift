b0396bf29ec0ed0b9bc37aeb74ca96c6ffb54f62
hwj0623 <hwj0623@naver.com>
2019-04-26 10:20:07 +0900

    STEP 5-3 (3) Line 좌표 입력 기능 추가 * 좌표구분 익스텐션 함수 추가 * 허용되지 않는 입력문자에 대한 정규식 체크 함수 추가 * 단일좌표 대신 복수의 좌표를 입력 받을 수 있도록 이니셜라이저 재설계

--- CoordinateCalculator/CoordinateCalculator/InputRefinary.swift
+++ CoordinateCalculator/CoordinateCalculator/InputRefinary.swift
@@ -8,28 +8,74 @@
 
 import Foundation
 
+extension String {
+    
+    func seperatePointByPoint(for regex: String, in inputString: String) -> [String] {
+        do {
+            if let regex = try? NSRegularExpression(pattern: regex, options: .caseInsensitive){
+                let test = regex.matches(in: inputString, options: [], range: NSRange(location:0, length: inputString.count))
+                let result : [String] = test.map{ String(inputString[Range($0.range, in: inputString)!])}
+                return result
+            }
+        }
+        return []
+    }
+    
+    func checkInvalidCharacterSet(for regex: String, in inputString: String) -> Bool {
+        do {
+            if let regex = try? NSRegularExpression(pattern: regex, options: .caseInsensitive){
+                let test = regex.matches(in: inputString, options: [], range: NSRange(location:0, length: inputString.count))
+                let result : [String] = test.map{ String(inputString[Range($0.range, in: inputString)!])}
+                return result.count == 0 ? true : false
+            }
+        }
+        return true
+    }
+    
+}
 
 struct InputRefinary {
     static private let MaxRange: Int = 24
     static private let MinRange: Int = 0
-    private (set) var pointPair: Pair
+    private (set) var pointPair: [Pair] = [Pair]()
     
     init(_ input : String) throws{
-        let (preprocessedX,preprocessedY) = try InputRefinary.splitPoints(input)
-        let (xPointDigit,yPointDigit) = try InputRefinary.convertStringToNumber(x: preprocessedX, y: preprocessedY)
-        
-        if !InputRefinary.isInRange(x: xPointDigit, y: yPointDigit) {
-            throw ErrorCode.InvalidRange
+        try InputRefinary.checkLineHasValidCharacterSet(input)
+        let pointList: [String] = try InputRefinary.splitPointByPoint(input)
+        for point in pointList {
+            let (preprocessedX,preprocessedY) = try InputRefinary.splitPointXFromY(point)
+            let (xPointDigit,yPointDigit) = try InputRefinary.convertStringToNumber(x: preprocessedX, y: preprocessedY)
+            if !InputRefinary.isInRange(x: xPointDigit, y: yPointDigit) {
+                throw ErrorCode.InvalidRange
+            }
+            pointPair.append (Pair(xPointDigit, yPointDigit))
         }
-        pointPair = Pair(xPointDigit, yPointDigit)
     }
     
-    static private func splitPoints (_ point: String) throws -> ( String, String) {
+    static private func splitPointByPoint(_ input: String) throws -> [String] {
+        let numberFilter: String = "[0-9]{1,2}"
+        let pointfilter: String = "(\\()\(numberFilter)(,)\(numberFilter)(\\))"
+        let result = input.seperatePointByPoint(for: pointfilter, in: input)
+            if result.count == 0 {
+                throw ErrorCode.SplitStringError
+            }
+        return result
+    }
+
+    static private func checkLineHasValidCharacterSet(_ input: String) throws -> Void { 
+        let invalidCharacterSetFilter: String = "[a-zA-Z `!@#$%^&*_+`=\\[\\]\\{\\}\\\\|]+"
+        let invalidCharacterSet = input.checkInvalidCharacterSet(for: invalidCharacterSetFilter, in: input)
+        if !invalidCharacterSet {
+            throw ErrorCode.InvalidCharacter
+        }
+    }
+    
+    static private func splitPointXFromY (_ point: String) throws -> (String, String) {
         let splitAPairOfPoint: [String] = point.trimmingCharacters(in: ["(",")"])
             .split(separator: ",")
             .map { (value) in return String(value)}
         if splitAPairOfPoint.count != 2 {
-            throw ErrorCode.SplitStringError
+            throw ErrorCode.SplitCoordinatorError
         }
         return (splitAPairOfPoint[0], splitAPairOfPoint[1])
     }
