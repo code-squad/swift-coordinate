0de27866e41db297c249e6599f81fcc1884e2a6e
Kroisttt <yago0315@gmail.com>
2019-04-26 15:57:43 +0900

    Measurable 프로토콜 수정

--- CoordinateCalculator/CoordinateCalculator/Measurable.swift
+++ CoordinateCalculator/CoordinateCalculator/Measurable.swift
@@ -7,3 +7,13 @@
 //
 
 import Foundation
+
+enum MeasurableUnit: String {
+    case length = "두 점 사이 거리는 "
+}
+
+protocol Measurable {
+    var unit: String { get }
+    
+    func calculate() -> Double
+}
