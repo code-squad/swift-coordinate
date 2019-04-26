256363ef432ecd055a95acc5f44d2b777a5a32f0
Kroisttt <yago0315@gmail.com>
2019-04-26 15:59:32 +0900

    MyLine 구조체 수정
    Drawable 프로토콜 수정과 Measurable 프로토콜 채택에 따른 수정

--- CoordinateCalculator/CoordinateCalculator/MyLine.swift
+++ CoordinateCalculator/CoordinateCalculator/MyLine.swift
@@ -8,12 +8,32 @@
 
 import Foundation
 
-struct MyLine: Drawable {
+struct MyLine: Drawable, Measurable {
+    func calculate() -> Double {
+        let x = pow(Double(pointA.x - pointB.x), 2)
+        let y = pow(Double(pointA.y - pointB.y), 2)
+        
+        return sqrt(x + y)
+    }
+    
+    func point() -> [MyPoint] {
+        let linePoint = [pointA, pointB]
+        
+        return linePoint
+    }
+    
     private(set) var pointA: MyPoint
     private(set) var pointB: MyPoint
+    private(set) var length: Double
+    var unit: String
     
     init(pointA: MyPoint, pointB: MyPoint) {
         self.pointA = pointA
         self.pointB = pointB
+        length = 0
+        unit = ""
+            
+        length = calculate()
+        unit = MeasurableUnit.length.rawValue
     }
 }
