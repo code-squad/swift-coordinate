8d973548960d9aa8fbade7297987a176693e7f92
Kroisttt <yago0315@gmail.com>
2019-04-26 16:09:51 +0900

    Converter 구조체 수정
    여러 값 입력에 따른 구조 변경

--- CoordinateCalculator/CoordinateCalculator/Converter.swift
+++ CoordinateCalculator/CoordinateCalculator/Converter.swift
@@ -9,7 +9,25 @@
 import Foundation
 
 struct Converter {
-    static func convertCoordinates(coordinatesText: String) throws -> MyPoint {
+    static func convertCoordinates(coordinatesText: [String]) throws -> Drawable {
+        switch coordinatesText.count {
+        case 1:
+            return try convertPoint(coordinatesText: coordinatesText[0])
+        case 2:
+            return try convertLine(coordinatesTexts: coordinatesText)
+        default:
+            throw InputError.invalidInput
+        }
+    }
+    
+    private static func convertLine(coordinatesTexts: [String]) throws -> MyLine {
+        let pointA = try convertPoint(coordinatesText: coordinatesTexts[0])
+        let pointB = try convertPoint(coordinatesText: coordinatesTexts[1])
+        
+        return MyLine(pointA: pointA, pointB: pointB)
+    }
+    
+    private static func convertPoint(coordinatesText: String) throws -> MyPoint {
         let numbersText = coordinatesText.components(separatedBy: ["(", ")"]).joined().split(separator: ",")
         var numbers: [Int] = []
         
