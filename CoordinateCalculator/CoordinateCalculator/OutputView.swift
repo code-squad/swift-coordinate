a95a7bdc5dd9750b012dae92cb417e5843114ef4
O-O-wl <ldcpaul@hanmail.net>
2019-04-28 03:25:46 +0900

    Modified: OutputView 프로퍼티 타입을 Drawable 로 재정의

--- CoordinateCalculator/CoordinateCalculator/OutputView.swift
+++ CoordinateCalculator/CoordinateCalculator/OutputView.swift
@@ -10,26 +10,17 @@ import Foundation
 
 struct OutputView{
     
-    let point:MyPoint
+    let target:Drawable
     
-    init(_ point:MyPoint) {
-        self.point = point
+    init(_ target:Drawable) {
+        self.target = target
     }
-   
-    
     func drawAxis(){
         print("\(ANSICode.clear)\(ANSICode.home)")
         print("\(ANSICode.text.cyanBright)\(ANSICode.axis.draw())")
     }
+    func output(){
+        self.target.draw()
+    }
     
-    func drawPoint(){
-        let row = ANSICode.convertX(self.point.x)
-        let col = ANSICode.convertY(self.point.y)
-        let color = ANSICode.text.colorFrom(R:255,G:127,B:0)
-        print("\(ANSICode.cursor.move(row:row , col: col ))\(color)\(ANSICode.dot)")
-        moveCurser()
-    }
-    func moveCurser(){
-         print("\(ANSICode.cursor.move(row: ANSICode.axis.AxisLimit+2, col: 2))")
-    }
 }
