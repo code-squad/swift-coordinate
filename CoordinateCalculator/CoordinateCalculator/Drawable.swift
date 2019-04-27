d2cb865c27e9d10722e079af10f0f817b21e69cc
O-O-wl <ldcpaul@hanmail.net>
2019-04-28 01:55:52 +0900

    Created: Drawable 프로토콜정의

---
+++ CoordinateCalculator/CoordinateCalculator/Drawable.swift
@@ -0,0 +1,13 @@
+//
+//  Drawable.swift
+//  CoordinateCalculator
+//
+//  Created by 이동영 on 28/04/2019.
+//  Copyright © 2019 Codesquad Inc. All rights reserved.
+//
+
+import Foundation
+
+protocol Drawable{
+    func draw()
+}
