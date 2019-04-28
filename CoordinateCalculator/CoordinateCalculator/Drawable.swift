3eee19efb885889671e311bd6a9edf48562911cf
cmindy <minjidev@naver.com>
2019-04-28 21:25:21 +0900

    feat: Add Drawable Protocol for drawing mypoints

---
+++ CoordinateCalculator/CoordinateCalculator/Drawable.swift
@@ -0,0 +1,15 @@
+//
+//  Drawable.swift
+//  CoordinateCalculator
+//
+//  Created by CHOMINJI on 28/04/2019.
+//  Copyright © 2019 Codesquad Inc. All rights reserved.
+//
+
+import Foundation
+
+protocol Drawable {
+    
+    var points: [MyPoint] { get }
+    
+}
