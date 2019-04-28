9b837b82fad669cff8e8e3ea982ecc387df2d667
cmindy <minjidev@naver.com>
2019-04-28 21:31:25 +0900

    feat: Add conform Drawable protocol in Mypoint, MyLine

--- CoordinateCalculator/CoordinateCalculator/MyLine.swift
+++ CoordinateCalculator/CoordinateCalculator/MyLine.swift
@@ -8,7 +8,12 @@
 
 import Foundation
 
-struct MyLine {
+struct MyLine: Drawable {
+    
     private(set) var pointA = MyPoint(x: 0, y: 0)
     private(set) var pointB = MyPoint(x: 0, y: 0)
+    
+    var points: [MyPoint] {
+        return [pointA, pointB]
+    }
 }
