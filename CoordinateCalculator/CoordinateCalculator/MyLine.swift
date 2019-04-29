c8c6cee4be319dedb694efa9acc0354361e9904d
O-O-wl <ldcpaul@hanmail.net>
2019-04-30 04:05:54 +0900

    Modified: 줄간격 정리

--- CoordinateCalculator/CoordinateCalculator/MyLine.swift
+++ CoordinateCalculator/CoordinateCalculator/MyLine.swift
@@ -12,16 +12,16 @@ struct MyLine : Figure{
     private(set) var type: FigureType = .line
     private(set) var pointA : MyPoint
     private(set) var pointB : MyPoint
-    
     var distanceOfPoints: Double {
         let xd = pow(Double(pointA.x - pointB.x),2)
         let yd = pow(Double(pointA.y - pointB.y),2)
         return sqrt(xd+yd)
     }
+    
     init(_ pointA:MyPoint,_ pointB:MyPoint) {
         self.pointA = pointA
         self.pointB = pointB
     }
-
+    
 }
 
