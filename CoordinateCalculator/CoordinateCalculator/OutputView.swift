097b0c79bf47bef2572f448d8af0c37ba7cdd7e8
kimjieun <zieunv@gmail.com>
2019-04-29 15:16:32 +0900

    점 찍는 코드 구현

--- CoordinateCalculator/CoordinateCalculator/OutputView.swift
+++ CoordinateCalculator/CoordinateCalculator/OutputView.swift
@@ -10,7 +10,12 @@ import Foundation
 
 public struct OutputView {
     static func drawAxis(){
+        // 출력 화면을 지우는 ANSI코드
         print("\(ANSICode.clear)\(ANSICode.home)")
-        print("\(ANSICode.text.whiteBright)\(ANSICode.axis.draw())")
+        print("\(ANSICode.text.redBright)\(ANSICode.axis.draw())")
+        print("\(ANSICode.cursor.move(row:10, col: 10))*")
+        print("\(ANSICode.text.redBright)\(ANSICode.axis.draw())")
+    
+    
     }
 }
