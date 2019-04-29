9f087ab95cecad76502c4ea1d7111c662ba54ac7
O-O-wl <ldcpaul@hanmail.net>
2019-04-30 02:36:49 +0900

    Modified:MyLine  출력하는함수 분리

--- CoordinateCalculator/CoordinateCalculator/MyLine.swift
+++ CoordinateCalculator/CoordinateCalculator/MyLine.swift
@@ -10,29 +10,17 @@ import Foundation
 
 struct MyLine : Figure{
    
-    private(set) let pointA : MyPoint
-    private(set) let pointB : MyPoint
-    
-    init(_ pointA:MyPoint,_ pointB:MyPoint) {
-        self.pointA = pointA
-        self.pointB = pointB
-    }
-    
-    func getDistancebetweenPoints()->Double{
+    private(set) var pointA : MyPoint
+    private(set) var pointB : MyPoint
+    var distanceOfPoints: Double {
         let xd = pow(Double(pointA.x - pointB.x),2)
         let yd = pow(Double(pointA.y - pointB.y),2)
         return sqrt(xd+yd)
     }
-    
-    private func printDistancebetweenPoints(){
-        print("두 점 사이 거리는 \(self.getDistancebetweenPoints())")
+    init(_ pointA:MyPoint,_ pointB:MyPoint) {
+        self.pointA = pointA
+        self.pointB = pointB
     }
-    
-    func draw(line:MyLine) {
-        draw(point:line.pointA)
-        draw(point:line.pointB)
-        self.printDistancebetweenPoints()
-    }
-    
+
 }
 
