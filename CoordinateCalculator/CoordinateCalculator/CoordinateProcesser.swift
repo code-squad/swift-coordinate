c69db6e51f21413c38efa0e19b1877147c172258
O-O-wl <ldcpaul@hanmail.net>
2019-04-30 04:02:11 +0900

    Modified: 입력받은 값을 처리하는 CoordinateProcesser 에 처리하는 함수 정의

--- CoordinateCalculator/CoordinateCalculator/CoordinateProcesser.swift
+++ CoordinateCalculator/CoordinateCalculator/CoordinateProcesser.swift
@@ -7,3 +7,31 @@
 //
 
 import Foundation
+
+struct CoordinateProcesser {
+    
+    
+    private let factory:FigureFactory = FigureFactory()
+    
+    private func check(point:PointTuple) throws{
+        let possibleRange = 0...ANSICode.axis.AxisLimit
+        guard possibleRange.contains(point.x) && possibleRange.contains(point.y) else {
+            throw Exception.ErrorType.outOfRange
+        }
+    }
+    
+    private func check(answer:Answer) throws{
+        guard answer.isCoordinateFormat() else {
+            throw Exception.ErrorType.wrongFormat
+        }
+        _ = try answer.getPointTuples().map{
+            pointTuple in
+            try check(point:pointTuple)
+        }
+    }
+    
+    func convertToFigure(answer:Answer) throws -> Figure{
+        try check(answer:answer)
+        return try factory.createFigure(type: answer.getFormat(), points: answer.getPointTuples())
+    } 
+}
