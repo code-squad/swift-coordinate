11032f5c01c45e06bbcc3bdcc6c88840bd101095
cmindy <minjidev@naver.com>
2019-04-29 12:09:30 +0900

    feat: Add Calculator struct for calculate distance of points

---
+++ CoordinateCalculator/CoordinateCalculator/Calculator.swift
@@ -0,0 +1,14 @@
+//
+//  Calculator.swift
+//  CoordinateCalculator
+//
+//  Created by CHOMINJI on 29/04/2019.
+//  Copyright © 2019 Codesquad Inc. All rights reserved.
+//
+
+import Foundation
+
+struct Calculator {
+    
+   
+}
