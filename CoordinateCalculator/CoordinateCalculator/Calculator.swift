02e71e4349d82f0b5c63d29879b30d8e2fcb4aec
cmindy <minjidev@naver.com>
2019-04-29 12:10:24 +0900

    feat: Add calculateDistance() for calculate distance of points

--- CoordinateCalculator/CoordinateCalculator/Calculator.swift
+++ CoordinateCalculator/CoordinateCalculator/Calculator.swift
@@ -10,5 +10,13 @@ import Foundation
 
 struct Calculator {
     
-   
+    static func calculateDistance(of points: MyLine) -> Double {
+        
+        let distanceX = points.pointA.x - points.pointB.x
+        let distanceY = points.pointA.y - points.pointB.y
+        
+        let distance = sqrt(Double(distanceX*distanceX + distanceY*distanceY))
+        
+        return distance
+    }
 }
