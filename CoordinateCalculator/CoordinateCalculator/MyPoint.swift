e50ed72beefc9ddb18139c6c6d218f1afe8d16f1
Daheen Lee <allwhite.dev@gmail.com>
2019-04-30 12:25:46 +0900

    refactor: Modify access level of stored property in MyPoint

--- CoordinateCalculator/CoordinateCalculator/MyPoint.swift
+++ CoordinateCalculator/CoordinateCalculator/MyPoint.swift
@@ -13,8 +13,8 @@ protocol AxisDrawable {
 }
 
 struct MyPoint: AxisDrawable {
-    private var x = 0
-    private var y = 0
+    public private(set) var x: Int = 0
+    public private(set) var y: Int = 0
     
     init(x: Int, y: Int) {
         self.x = x
