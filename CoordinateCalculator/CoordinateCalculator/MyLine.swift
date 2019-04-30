445b240a1855071b2721f42ce0c1420b45615091
Daheen Lee <allwhite.dev@gmail.com>
2019-04-30 16:59:14 +0900

    refactor: Modify MyLine struct to conform ValueComputable protocol

--- CoordinateCalculator/CoordinateCalculator/MyLine.swift
+++ CoordinateCalculator/CoordinateCalculator/MyLine.swift
@@ -8,10 +8,13 @@
 
 import Foundation
 
-struct MyLine: AxisDrawable {
-    private var pointA = MyPoint(x: 0, y: 0) 
+struct MyLine: AxisDrawable, ValueComputable {
+    private var pointA = MyPoint(x: 0, y: 0)
     private var pointB = MyPoint(x: 0, y: 0)
     
+    var description: String {
+        return "두 점 사이의 거리는 "
+    }
     
     init(pairA: Pair, pairB: Pair) {
         pointA = MyPoint(x: pairA.x, y: pairA.y)
@@ -22,7 +25,11 @@ struct MyLine: AxisDrawable {
         return [pointA, pointB]
     }
     
-    func getLength() -> Double {
+    func getComputedValue() -> Double {
+        return getLength()
+    }
+    
+    private func getLength() -> Double {
         let distanceX = pow(Double(pointA.x - pointB.x), 2.0)
         let distanceY = pow(Double(pointA.y - pointB.y), 2.0)
         return sqrt(distanceX + distanceY)
