cc5277f2e9ed833eccd2163929ef2e6cb2b4ac39
Daheen Lee <allwhite.dev@gmail.com>
2019-04-30 11:28:37 +0900

    refactor: Separate struct MyLine from MyPoint.swift

--- CoordinateCalculator/CoordinateCalculator/MyPoint.swift
+++ CoordinateCalculator/CoordinateCalculator/MyPoint.swift
@@ -32,23 +32,3 @@ struct MyPoint: AxisDrawable {
 }
 
 
-struct MyLine: AxisDrawable {
-    var pointA = MyPoint(x: 0, y: 0)
-    var pointB = MyPoint(x: 0, y: 0)
-    
-    
-    init(pairs: [Pair]) {
-        pointA = MyPoint(x: pairs[0].x, y: pairs[0].y)
-        pointB = MyPoint(x: pairs[1].x, y: pairs[1].y)
-    }
-    
-    func getPoints() -> [MyPoint] {
-        return [pointA, pointB]
-    }
-    
-    func getLength() -> Double {
-        let distanceX = pow(Double(pointA.x - pointB.x), 2.0)
-        let distanceY = pow(Double(pointA.y - pointB.y), 2.0)
-        return sqrt(distanceX + distanceY)
-    }
-}
