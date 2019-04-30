857e9055c67f5b0c18d27106c062c01f16ee35e3
Kroisttt <yago0315@gmail.com>
2019-04-30 11:50:11 +0900

    MyTriangle 구조체 추가

---
+++ CoordinateCalculator/CoordinateCalculator/MyTriangle.swift
@@ -0,0 +1,42 @@
+//
+//  MyTriangle.swift
+//  CoordinateCalculator
+//
+//  Created by 이진영 on 30/04/2019.
+//  Copyright © 2019 Codesquad Inc. All rights reserved.
+//
+
+import Foundation
+
+struct MyTriangle: Drawable, Measurable {
+    private(set) var lineAB: MyLine
+    private(set) var lineBC: MyLine
+    private(set) var lineAC: MyLine
+    
+    private let trianglePoint: [MyPoint]
+    
+    func point() -> [MyPoint] {
+        return trianglePoint
+    }
+    
+    var unit: String {
+        return MeasurableUnit.triangle.rawValue
+    }
+    
+    var value: Double {
+        let cos = (pow(lineBC.value, 2) + pow(lineAB.value, 2) - pow(lineAC.value, 2)) / ( 2 * lineBC.value * lineAB.value)
+        let sin = sqrt(1 - pow(cos, 2))
+        
+        let result = 0.5 * lineBC.value * lineAB.value * sin
+        
+        return result
+    }
+    
+    init(pointA: MyPoint, pointB: MyPoint, pointC: MyPoint) {
+        lineAB = MyLine(pointA: pointA, pointB: pointB)
+        lineBC = MyLine(pointA: pointB, pointB: pointC)
+        lineAC = MyLine(pointA: pointA, pointB: pointC)
+        
+        trianglePoint = [pointA, pointB, pointC]
+    }
+}
