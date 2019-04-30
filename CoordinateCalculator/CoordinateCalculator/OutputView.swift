9312169907d86d929a26ac5234d9fdb628023ccb
gukjin jang <jgj455@gmail.com>
2019-04-30 14:27:08 +0900

    Modify : printPoint() 함수 내부의 for문을 index 대신 배열요소로 받게 수정

--- CoordinateCalculator/CoordinateCalculator/OutputView.swift
+++ CoordinateCalculator/CoordinateCalculator/OutputView.swift
@@ -16,8 +16,8 @@ struct OutputView{
     }
     /// MyPoint, MyLine, MyTriangle, MyRect을 받아와서 출력하는 함수
     func printPoint(figure : Figure){
-        for index in 0...figure.distinctIndex().locationIndex-1 {
-            print("\(ANSICode.text.whiteBright)\(ANSICode.cursor.move(row: 25-figure.distinctIndex().myPoints[index].y, col: (figure.distinctIndex().myPoints[index].x*2)+2))*")
+        for myPoint in figure.distinctIndex().myPoints {
+            print("\(ANSICode.text.whiteBright)\(ANSICode.cursor.move(row: 25-myPoint.y, col: (myPoint.x*2)+2))*")
         }
         print("\(ANSICode.cursor.move(row: 27, col: 25))")
         if figure.distinctIndex().locationIndex == 2 { print("두점을 잇는 직선의 길이는 \(figure.distinctIndex().guideMent)입니다") }
