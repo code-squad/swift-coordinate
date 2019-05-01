44d9a920dcd970c6bfeb0984129b27a811ed1359
O-O-wl <ldcpaul@hanmail.net>
2019-05-01 20:31:28 +0900

    Modified: Factory 에 삼각형 만들수 있게 수정

--- CoordinateCalculator/CoordinateCalculator/FigureFactory.swift
+++ CoordinateCalculator/CoordinateCalculator/FigureFactory.swift
@@ -10,18 +10,45 @@ import Foundation
 
 struct FigureFactory{
     
-     func createFigure(type:FigureType,points:[PointTuple]) throws ->Figure{
+    func createFigure(type:FigureType,points:[PointTuple]) throws ->Figure{
         switch type {
         case .none:
             throw Exception.ErrorType.wrongFormat
         case .point:
-            return  MyPoint.init(points[0])
+            return createPoint(points: points)
         case .line :
-            return MyLine.init( MyPoint(points[0]),  MyPoint(points[1]))
+            return createLine(points: points)
         case .triangle:
-            
-            return MyTriangle.init(lineAB: <#T##MyLine#>, lineBC: <#T##MyLine#>, lineAC: <#T##MyLine#>)
-            
+            return createTriangle(points: points)
         }
     }
+    
+    func createPoint(points:[PointTuple])->MyPoint{
+        return MyPoint.init(points[0])
+    }
+    
+    func createLine(points:[PointTuple])->MyLine{
+        let pointA = createPoint(points:[points[0]])
+        let pointB = createPoint(points:[points[1]])
+        return MyLine.init(pointA,pointB)
+    }
+    
+    func createTriangle(points:[PointTuple])->MyTriangle{
+        let pairAB =  pair(points: points, indexA: 0, indexB: 1)
+        let pairBC =  pair(points: points, indexA: 1, indexB: 2)
+        let pairAC =  pair(points: points, indexA: 0, indexB: 2)
+        let lineAB = createLine(points: pairAB)
+        let lineBC = createLine(points: pairBC)
+        let lineAC = createLine(points: pairAC)
+        return MyTriangle.init(lineAB: lineAB, lineBC: lineBC, lineAC: lineAC)
+        
+    }
+    
+    func pair(points:[PointTuple],indexA:Int,indexB:Int)->[PointTuple]{
+        var pairedPoints = [PointTuple]()
+        pairedPoints.append(points[indexA])
+        pairedPoints.append(points[indexB])
+        return pairedPoints
+    }
+    
 }
