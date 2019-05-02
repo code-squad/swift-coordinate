4bb63cbcddb274041217dc181c2299a2910dcc84
cmindy <minjidev@naver.com>
2019-05-02 14:17:58 +0900

    feat: Add MyTriangle struct

---
+++ CoordinateCalculator/CoordinateCalculator/MyTriangle.swift
@@ -0,0 +1,20 @@
+//
+//  MyTriangle.swift
+//  CoordinateCalculator
+//
+//  Created by CHOMINJI on 02/05/2019.
+//  Copyright © 2019 Codesquad Inc. All rights reserved.
+//
+
+import Foundation
+
+struct MyTriangle {
+    
+    var lineAB = MyLine(pointA: MyPoint(), pointB: MyPoint())
+    var lineBC = MyLine(pointA: MyPoint(), pointB: MyPoint())
+    var lineAC = MyLine(pointA: MyPoint(), pointB: MyPoint())
+    
+    init(pointA: MyPoint, pointB: MyPoint, pointC: MyPoint) {
+        //로직 구현
+    }
+}
