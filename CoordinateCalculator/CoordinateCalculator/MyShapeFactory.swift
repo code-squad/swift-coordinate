4708e8c3b9aaddb39f4e1e9e1784594b664dfd55
hwj0623 <hwj0623@naver.com>
2019-05-02 16:09:04 +0900

    STEP 5-5 (2) 팩토리 구조체에 사각형 구조체 형성 로직 추가

--- CoordinateCalculator/CoordinateCalculator/MyShapeFactory.swift
+++ CoordinateCalculator/CoordinateCalculator/MyShapeFactory.swift
@@ -20,6 +20,9 @@ struct MyShapeFactory: ShapeCreatable {
         case 3:
             let myShape: Shapable = try createTriangle(pointList)
             return myShape
+        case 4:
+            let myShape: Shapable = try createRectangle(pointList)
+            return myShape
         default:
             throw ErrorCode.ShapeCreationError
         }
@@ -45,5 +48,40 @@ struct MyShapeFactory: ShapeCreatable {
         }
         let myShape: MyTriangle = MyTriangle.init(pointA: myPointA, pointB: myPointB, pointC: myPointC)
         return myShape
+    }
+    
+    static private func createRectangle(_ pointList: [Pair]) throws -> Shapable {
+        let checkRectangleResult = try checkRectangle(pointList)
+        if checkRectangleResult.isValid {
+            let pairOfleftTopAndRightBottom = decideLeftTopAndRightBottom(sortedX: checkRectangleResult.sortedX, sortedY: checkRectangleResult.sortedY)
+            let myShape: MyRect = MyRect.init(leftTop: pairOfleftTopAndRightBottom.leftTop, rightBottom: pairOfleftTopAndRightBottom.rightBottom)
+            return myShape
+        }
+        throw ErrorCode.ShapeCreationError
+    }
+    
+    static private func decideLeftTopAndRightBottom(sortedX: [Int], sortedY: [Int]) -> (leftTop: MyPoint, rightBottom: MyPoint){
+        let leftTopX: Int = sortedX[0]
+        let leftTopY: Int = sortedY[0]
+        let rightBottomX: Int = sortedX[1]
+        let rightBottomY: Int = sortedY[1]
+        let leftTop = MyPoint.init(x: leftTopX, y: leftTopY)
+        let rightBottom = MyPoint.init(x: rightBottomX, y: rightBottomY)
+        return (leftTop, rightBottom)
+    }
+    
+    static private func checkRectangle(_ pointList: [Pair]) throws -> (isValid: Bool, sortedX: [Int], sortedY: [Int]) {
+        guard pointList.count == 4 else{
+            throw ErrorCode.ShapeCreationError
+        }
+        var xPointSet: Set<Int> = Set<Int>()
+        var yPointSet: Set<Int> = Set<Int>()
+        for point in pointList {
+            xPointSet.insert(point.x)
+            yPointSet.insert(point.y)
+        }
+        let sortedX = xPointSet.sorted(by: >)   // descending order
+        let sortedY = yPointSet.sorted()        // ascending order
+        return (xPointSet.count == 2 && yPointSet.count == 2 ) ? (true, sortedX, sortedY) : (false, sortedX, sortedY)
     }
 }
