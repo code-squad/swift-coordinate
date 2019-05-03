477e0b50d44056210b0c791d80ec5b3d7a184e8f
kimjieun <zieunv@gmail.com>
2019-05-03 12:21:51 +0900

    1. main함수 조합

--- CoordinateCalculator/CoordinateCalculator/main.swift
+++ CoordinateCalculator/CoordinateCalculator/main.swift
@@ -8,6 +8,10 @@
 
 import Foundation
 
-let question = InputView.readInput(question: "좌표를 입력하세요. ex(10,10)")
+func main() {
+    let input = InputView.readInput(question: "좌표를 입력하세요. ex.(10,10)")
+    let point = InputControl.convertStringtoInt(input: input)
+    OutputView.drawAxis(point: point)
+}
 
-
+main()
