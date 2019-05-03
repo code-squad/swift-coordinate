ac16a9ef953e03702f1af887f2710dd9adeab17c
Daheen Lee <allwhite.dev@gmail.com>
2019-05-03 21:01:36 +0900

    Add MyRect.swift to add struct MyRect

---
+++ CoordinateCalculator/CoordinateCalculator/MyRect.swift
@@ -0,0 +1,18 @@
+//
+//  MyRect.swift
+//  CoordinateCalculator
+//
+//  Created by Daheen Lee on 03/05/2019.
+//  Copyright © 2019 Codesquad Inc. All rights reserved.
+//
+
+import Foundation
+
+struct MyRect {
+    var leftTop = MyPoint()
+    var rightBottom = MyPoint()
+    
+    init(origin: MyPoint, size: CGSize) {
+        //로직구현
+    }
+}
