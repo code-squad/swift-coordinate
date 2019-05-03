9886efce013192281a6d4390ea137fb4b23fe20d
Daheen Lee <allwhite.dev@gmail.com>
2019-05-03 18:03:29 +0900

    feat: Add makePoint() method which makes MyPoint instance using pair

--- CoordinateCalculator/CoordinateCalculator/FigureFactory.swift
+++ CoordinateCalculator/CoordinateCalculator/FigureFactory.swift
@@ -12,13 +12,22 @@ struct FigureFactory {
     static public func makeFigure(pairs: [Pair]) throws -> AxisDrawable {
         switch pairs.count {
         case 1:
-            return MyPoint(pair: pairs[0])
+            return makePoint(using: pairs[0])
         case 2:
-            return MyLine(pairA: pairs[0], pairB: pairs[1])
+            let startPoint = makePoint(using: pairs[0])
+            let endPoint = makePoint(using: pairs[1])
+            return MyLine(pointA: startPoint, pointB: endPoint)
         case 3:
-            return MyTriangle(pairA: pairs[0], pairB: pairs[1], pairC: pairs[2])
+            let pointA = makePoint(using: pairs[0])
+            let pointB = makePoint(using: pairs[1])
+            let pointC = makePoint(using: pairs[2])
+            return MyTriangle(pointA: pointA, pointB: pointB, pointC: pointC)
         default:
             throw FactoryError.impossibleToCreateInstance
         }
+    }
+    
+    static private func makePoint(using pair: Pair) -> MyPoint {
+        return MyPoint(x: pair.x, y: pair.y)
     }
 }
