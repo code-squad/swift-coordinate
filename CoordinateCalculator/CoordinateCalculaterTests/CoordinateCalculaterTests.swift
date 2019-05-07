a4d44a936413178a89696a1df2f6229d16078b95
O-O-wl <ldcpaul@hanmail.net>
2019-05-07 21:02:43 +0900

    Modified: 테스트함수 실패와 성공을 별개의 메소드로 분리

--- CoordinateCalculator/CoordinateCalculaterTests/CoordinateCalculaterTests.swift
+++ CoordinateCalculator/CoordinateCalculaterTests/CoordinateCalculaterTests.swift
@@ -37,7 +37,7 @@ class CoordinateCalculaterTests: XCTestCase {
         XCTAssertEqual(pointTuple.1,try anwser.getPointTuples()[0].y,"y 좌표 추출 테스트합니다")
     }
     
-    func testCheckRange(){
+    func testCheckRangeToFail(){
         //Given
         let excess = Answer.init("(25,10)")
         let under = Answer.init("(-1,-1)")
@@ -47,47 +47,66 @@ class CoordinateCalculaterTests: XCTestCase {
         XCTAssertThrowsError(try coordinateProcesser.convertToFigure(answer: under),"범위를 초과한 입력입니다.")
     }
     
-    func testCheckLineFormat(){
+    func testCheckRangeToSuccess(){
         //Given
-        let testAnwser = Answer.init("(10,10]-(20,20]")
-        let testAnwser2 = Answer.init("(10,10)-(20,20]")
-        let testAnwser3 = Answer.init("(5,5)-(10,10)")
+        let answer = Answer.init("(20,20)")
+        let coordinateProcesser = CoordinateProcesser()
         //Then
-        XCTAssertNotEqual(try testAnwser.getPointTuples().count,2, "라인 형태를 제대로 추출합니다")
+        XCTAssertNoThrow(try coordinateProcesser.convertToFigure(answer: answer),"범위를 초과한 입력입니다.")
+    }
+  
+    func testCheckLineFormatToSuccess(){
+        //Given
+        let answerToSuccess = Answer.init("(5,5)-(10,10)")
+        //Then
+        XCTAssertEqual(try answerToSuccess.getPointTuples().count,2, "라인 형태를 제대로 추출합니다")
+   }
+    
+    func testCheckLineFormatToFail(){
+        //Given
+        let answerToFail = Answer.init("(10,10]-(20,20]")
+        let answerToFail2 = Answer.init("(10,10)-(20,20]")
+        //Then
+        XCTAssertNotEqual(try answerToFail.getPointTuples().count,2, "라인 형태를 제대로 추출합니다")
+        XCTAssertNotEqual(try answerToFail2.getPointTuples().count,2, "라인 형태를 제대로 추출합니다")
+    }
+    
+    func testCheckTriangleFormatToSuccess(){
+        //Given
+        let answerToSuccess = Answer.init("(5,5)-(10,10)-(20,20)")
+        //Then
+        XCTAssertEqual(try answerToSuccess.getPointTuples().count,3,"()-()-() 형식이 이면 성공합니다")
+    }
+    
+    func testCheckTriangleFormatToFail(){
+        //Given
+        let answerToFail = Answer.init("(10,10]-(20,20]-(10,20)")
+        let answerToFail2 = Answer.init("(10,10)-(20,20]-(20,20]")
         
-        XCTAssertNotEqual(try testAnwser2.getPointTuples().count,2, "라인 형태를 제대로 추출합니다")
-        
-        XCTAssertEqual(try testAnwser3.getPointTuples().count,2, "라인 형태를 제대로 추출합니다")
-    }
-    
-    func testCheckTriangleFormat(){
-        //Given
-        let testAnwser = Answer.init("(10,10]-(20,20]-(10,20)")
-        let testAnwser2 = Answer.init("(10,10)-(20,20]-(20,20]")
-        let testAnwser3 = Answer.init("(5,5)-(10,10)-(20,20)")
         //Then
-        XCTAssertNotEqual(try testAnwser.getPointTuples().count,3,"()-()-() 형식이 아니면 에러를 던집니다")
-        XCTAssertNotEqual(try testAnwser2.getPointTuples().count,3,"()-()-() 형식이 아니면 에러를 던집니다")
-        XCTAssertEqual(try testAnwser3.getPointTuples().count,3,"()-()-() 형식이 이면 성공합니다")
+        XCTAssertNotEqual(try answerToFail.getPointTuples().count,3,"()-()-() 형식이 아니면 에러를 던집니다")
+        XCTAssertNotEqual(try answerToFail2.getPointTuples().count,3,"()-()-() 형식이 아니면 에러를 던집니다")
     }
     
-    func testCheckRectFormat(){
+    func testCheckRectFormatToSuccess(){
         //Given
-        let testAnwser = Answer.init("(10,10]-(20,20]-(10,20)-(10,10)")
-        let testAnwser2 = Answer.init("(10,10)-(20,20]-(20,20]-(10,10)")
-        let testAnwser3 = Answer.init("(5,10)-(5,20)-(10,10)-(20,20)")
+       let anwserToSuccess = Answer.init("(5,10)-(5,20)-(10,10)-(20,20)")
         //Then
-        XCTAssertNotEqual(try testAnwser.getPointTuples().count,4,"()-()-()-() 형식이 아니면 에러를 던집니다")
-        XCTAssertNotEqual(try testAnwser2.getPointTuples().count,4,"()-()-()-() 형식이 아니면 에러를 던집니다")
-        XCTAssertEqual(try testAnwser3.getPointTuples().count,4,"()-()-()-() 형식이 이면 성공합니다")
+        XCTAssertEqual(try anwserToSuccess.getPointTuples().count,4,"()-()-()-() 형식이 이면 성공합니다")
     }
     
-    func testRectInit(){
+    func testRectInitToSuccess(){
         //Given
         let pointsToSuccess = [(10,10),(22,10),(22,18),(10,18)]
-        let pointsToFail = [(10,11),(22,10),(22,18),(10,18)]
         //Then
         XCTAssertNoThrow(try factory.createFigure(points: pointsToSuccess))
+    }
+    
+    func testRectInitToFail(){
+        //Given
+        let pointsToFail = [(10,11),(22,10),(22,18),(10,18)]
+        
+        //Then
         XCTAssertThrowsError(try factory.createFigure(points: pointsToFail))
         
     }
@@ -99,12 +118,12 @@ class CoordinateCalculaterTests: XCTestCase {
         XCTAssertEqual(testLine.getCalculatedValue(), 6.4031242374328485)
     }
     
-    func testAreaOfTriangle()throws {
+    func testAreaOfTriangle() throws {
         //Given
         let points = [(10,10),(14,15),(20,8)]
         
         //When
-        guard let triangle = (try factory.createFigure( points: points)) as? MyTriangle else {return}
+        guard let triangle = (try factory.createFigure( points: points)) as? MyTriangle else { return }
         
         //Then
         XCTAssertEqual(triangle.getCalculatedValue(), 29)
