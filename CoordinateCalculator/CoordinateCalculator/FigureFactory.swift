72868c76766635001c8069f33b6cbe4248a4bb4a
Daheen Lee <allwhite.dev@gmail.com>
2019-05-07 12:14:40 +0900

    feat: Add MyRect creation

--- CoordinateCalculator/CoordinateCalculator/FigureFactory.swift
+++ CoordinateCalculator/CoordinateCalculator/FigureFactory.swift
@@ -14,14 +14,11 @@ struct FigureFactory {
         case 1:
             return makePoint(using: pairs[0])
         case 2:
-            let startPoint = makePoint(using: pairs[0])
-            let endPoint = makePoint(using: pairs[1])
-            return MyLine(pointA: startPoint, pointB: endPoint)
+            return makeLine(using: pairs)
         case 3:
-            let pointA = makePoint(using: pairs[0])
-            let pointB = makePoint(using: pairs[1])
-            let pointC = makePoint(using: pairs[2])
-            return MyTriangle(pointA: pointA, pointB: pointB, pointC: pointC)
+            return makeTriangle(using: pairs)
+        case 4:
+            return try makeRect(using: pairs)
         default:
             throw FactoryError.impossibleToCreateInstance
         }
@@ -29,5 +26,39 @@ struct FigureFactory {
     
     static private func makePoint(using pair: Pair) -> MyPoint {
         return MyPoint(x: pair.x, y: pair.y)
+    }
+    
+    static private func makeLine(using pairs:[Pair]) -> MyLine {
+        let startPoint = makePoint(using: pairs[0])
+        let endPoint = makePoint(using: pairs[1])
+        return MyLine(pointA: startPoint, pointB: endPoint)
+    }
+    
+    static private func makeTriangle(using pairs: [Pair]) -> MyTriangle {
+        let pointA = makePoint(using: pairs[0])
+        let pointB = makePoint(using: pairs[1])
+        let pointC = makePoint(using: pairs[2])
+        return MyTriangle(pointA: pointA, pointB: pointB, pointC: pointC)
+    }
+
+    static private func makeRect(using pairs: [Pair]) throws -> MyRect {
+        let setX = Set<Int>(pairs.map { (x, _) in x })
+        let setY = Set<Int>(pairs.map { (_, y) in y })
+
+        if CoordinateValidator.isNotRectangle(setX: setX, setY: setY) {
+            throw FactoryError.isNotRectangle
+        }
+        let (minX, maxX) = try getMinMax(of: setX)
+        let (minY, maxY) = try getMinMax(of: setY)
+        let leftTop = MyPoint(x: minX, y: maxY)
+        let rightBottom = MyPoint(x: maxX, y: minY)
+        return MyRect(leftTop: leftTop, rightBottom: rightBottom)
+    }
+    
+    static private func getMinMax(of set: Set<Int>) throws -> (Int, Int) {
+        guard let min = set.min(), let max = set.max() else {
+            throw FactoryError.invalidRectXY
+        }
+        return (min, max)
     }
 }
