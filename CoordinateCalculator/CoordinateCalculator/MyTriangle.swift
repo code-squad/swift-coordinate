959a68c53a3e0dff845810ad8d2bd12037ac66e3
cmindy <minjidev@naver.com>
2019-05-07 12:03:02 +0900

    refactor: Refactor MyTriangle, MyLine struct

--- CoordinateCalculator/CoordinateCalculator/MyTriangle.swift
+++ CoordinateCalculator/CoordinateCalculator/MyTriangle.swift
@@ -18,24 +18,21 @@ struct MyTriangle: Drawable {
         return lineAB.points + lineBC.points + lineAC.points
     }
     
-    init(pointA: MyPoint, pointB: MyPoint, pointC: MyPoint) {
-        self.lineAB = MyLine(pointA: pointA, pointB: pointB)
-        self.lineBC = MyLine(pointA: pointB, pointB: pointC)
-        self.lineAC = MyLine(pointA: pointA, pointB: pointC)
-    }
-    
     var area: Double {
-        
         let powA = lineBC.distance * lineBC.distance
         let powB = lineAC.distance * lineAC.distance
         let powC = lineAB.distance * lineAB.distance
-        
         let cosB = (powA + powC - powB) / (2 * lineBC.distance * lineAB.distance)
-        
         let sinB = sqrt(Double(1 - cosB * cosB))
         
         let result = lineBC.distance * lineAB.distance * sinB / 2
         
         return result
+    }
+    
+    init(pointA: MyPoint, pointB: MyPoint, pointC: MyPoint) {
+        self.lineAB = MyLine(pointA: pointA, pointB: pointB)
+        self.lineBC = MyLine(pointA: pointB, pointB: pointC)
+        self.lineAC = MyLine(pointA: pointA, pointB: pointC)
     }
 }
