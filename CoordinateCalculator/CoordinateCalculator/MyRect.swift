18d2e4c69331e8b32181a037375a31f9e440d9ab
Daheen Lee <allwhite.dev@gmail.com>
2019-05-07 12:10:16 +0900

    feat: Add initializer of MyRect

--- CoordinateCalculator/CoordinateCalculator/MyRect.swift
+++ CoordinateCalculator/CoordinateCalculator/MyRect.swift
@@ -11,8 +11,21 @@ import Foundation
 struct MyRect {
     var leftTop = MyPoint()
     var rightBottom = MyPoint()
+    var size = CGSize()
+    
+    init(leftTop: MyPoint, rightBottom: MyPoint) {
+        self.leftTop = leftTop
+        self.rightBottom = rightBottom
+        let width = rightBottom.x - leftTop.x
+        let height = leftTop.y - rightBottom.y
+        size = CGSize(width: width, height: height)
+    }
     
     init(origin: MyPoint, size: CGSize) {
-        //로직구현
+        self.size = size
+        let leftTopY = origin.y + Int(size.height)
+        let rightBottomX = origin.x + Int(size.width)
+        leftTop = MyPoint(x: origin.x, y: leftTopY)
+        rightBottom = MyPoint(x: rightBottomX, y: origin.y)
     }
 }
