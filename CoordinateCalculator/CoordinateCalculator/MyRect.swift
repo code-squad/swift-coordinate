cb0b7defee5817b44eb04eee3f80a0b882592fe7
cmindy <minjidev@naver.com>
2019-05-09 14:07:02 +0900

    feat: Add MyRect struct

---
+++ CoordinateCalculator/CoordinateCalculator/MyRect.swift
@@ -0,0 +1,16 @@
+//
+//  MyRect.swift
+//  CoordinateCalculator
+//
+//  Created by CHOMINJI on 09/05/2019.
+//  Copyright © 2019 Codesquad Inc. All rights reserved.
+//
+
+import Foundation
+
+struct MyRect {
+    
+    private let leftTop: MyPoint
+    private let rightBottom: MyPoint
+    
+}
