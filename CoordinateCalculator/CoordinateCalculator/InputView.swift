013def3f34e89e6ad5367c55c2e852f7a973c690
junyng <pom0894@naver.com>
2019-05-09 19:41:47 +0900

    입력 구조체 문자열 읽는 함수 이름 변경 및 접근 제한자 지정, 점 및 좌표 변환 구조체 추가

--- CoordinateCalculator/CoordinateCalculator/InputView.swift
+++ CoordinateCalculator/CoordinateCalculator/InputView.swift
@@ -10,12 +10,12 @@ import Foundation
 
 struct InputView {
 
-    static func ask(question: String) -> String {
+    static private func readText(question: String) -> String {
         print(question)
         return readLine() ?? ""
     }
 
     static func readInput() -> String {
-        return ask(question: "좌표를 입력하세요.")
+        return readText(question: "좌표를 입력하세요.")
     }
 }
