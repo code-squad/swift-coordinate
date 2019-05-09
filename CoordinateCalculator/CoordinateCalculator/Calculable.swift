4d0da2a44b95ac30bd108836b17a82f5934a4077
cmindy <minjidev@naver.com>
2019-05-09 13:55:51 +0900

    feat: Add Calculable protocol to print calculated value

---
+++ CoordinateCalculator/CoordinateCalculator/Calculable.swift
@@ -0,0 +1,17 @@
+//
+//  Calculable.swift
+//  CoordinateCalculator
+//
+//  Created by CHOMINJI on 09/05/2019.
+//  Copyright © 2019 Codesquad Inc. All rights reserved.
+//
+
+import Foundation
+
+protocol Calculable {
+  
+  var description: String { get }
+  
+  func getCalcultedValue() -> Double
+  
+}
