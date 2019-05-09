b8545809aa30d2296e6dca7169165cd77ef38584
junyng <pom0894@naver.com>
2019-05-09 19:41:47 +0900

    입력 구조체 문자열 읽는 함수 이름 변경 및 접근 제한자 지정, 점 및 좌표 변환 구조체 추가

---
+++ CoordinateCalculator/CoordinateCalculator/CoordinateFormatter.swift
@@ -0,0 +1,13 @@
+//
+//  CoordinateFormatter.swift
+//  CoordinateCalculator
+//
+//  Created by BLU on 2019. 5. 9..
+//  Copyright © 2019년 Codesquad Inc. All rights reserved.
+//
+
+import Foundation
+
+struct CoordinateFormatter {
+    
+}
---
+++ CoordinateCalculator/CoordinateCalculator/MyPoint.swift
@@ -0,0 +1,14 @@
+//
+//  MyPoint.swift
+//  CoordinateCalculator
+//
+//  Created by BLU on 2019. 5. 9..
+//  Copyright © 2019년 Codesquad Inc. All rights reserved.
+//
+
+import Foundation
+
+struct MyPoint {
+    var x = 0
+    var y = 0
+}
