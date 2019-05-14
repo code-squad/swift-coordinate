61b9cda97d39ec11d157efecd623e3f41bc12e3a
chan <clown4014@gmail.com>
2019-05-15 05:07:06 +0900

    validator구조체내 함수가   단위테스틀 모두 통과하게 구현 UserInput관련 Error를 정의하는 열거형 생성

---
+++ CoordinateCalculator/CoordinateCalculator/UserInputError.swift
@@ -0,0 +1,32 @@
+//
+//  UserInputError.swift
+//  CoordinateCalculator
+//
+//  Created by 이희찬 on 15/05/2019.
+//  Copyright © 2019 Codesquad Inc. All rights reserved.
+//
+
+import Foundation
+
+enum UserInputError:Error {
+    case isNotString
+    case inCorrectFormat
+    case inCorrectItemNumber
+    case canNotConvertTOInt
+    case outOfRange
+    
+    var description: String{
+        switch self{
+        case .isNotString:
+            return "잘못된 입력입니다."
+        case .inCorrectFormat:
+            return "잘못된 형식의 입력입니다."
+        case .inCorrectItemNumber:
+            return "좌표 값은 두개를 입력하세요."
+        case .canNotConvertTOInt:
+            return "정수를 입력하세요."
+        case .outOfRange:
+            return "표시할수 있는 좌표값을 벗어났습니다. "
+        }
+    }
+}
