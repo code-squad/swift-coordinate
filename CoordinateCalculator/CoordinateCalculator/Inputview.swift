f3335034e6cc9556a55f26b0e33bb543ad0029ed
chan <clown4014@gmail.com>
2019-05-15 04:43:19 +0900

    InputView구조체생성

---
+++ CoordinateCalculator/CoordinateCalculator/Inputview.swift
@@ -0,0 +1,12 @@
+//
+//  Inputview.swift
+//  CoordinateCalculator
+//
+//  Created by 이희찬 on 15/05/2019.
+//  Copyright © 2019 Codesquad Inc. All rights reserved.
+//
+
+import Foundation
+struct InputView {
+    <#fields#>
+}
