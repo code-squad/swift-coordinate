c0dc0066fb2d2716bdc681d9f653f944419f9ab9
chan <clown4014@gmail.com>
2019-05-14 18:04:32 +0900

    좌표축 출력

---
+++ CoordinateCalculator/CoordinateCalculator/OutputView..swift
@@ -0,0 +1,19 @@
+//
+//  OutputView..swift
+//  CoordinateCalculator
+//
+//  Created by 이희찬 on 14/05/2019.
+//  Copyright © 2019 Codesquad Inc. All rights reserved.
+//
+
+import Foundation
+
+struct OutputView {
+    
+    static func drawAxis() {
+        
+        print("\(ANSICode.clear)\(ANSICode.home)")
+        print("\(ANSICode.text.whiteBright)\(ANSICode.axis.draw())")
+    }
+    
+}
