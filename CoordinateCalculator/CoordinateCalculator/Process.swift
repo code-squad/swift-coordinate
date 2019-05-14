c31c015eca3482898477354d82775dfccfec66b1
박준희 <junibibi04@gmail.com>
2019-05-14 19:28:07 +0900

    Process구조체 파일 생성, InputView구조체 메소드명 수정
    입력받은 값을 가공할 구조체 Process를 생성.
    입력한값에서 사용할 값만 추려내는 메소드 생성
    값의 자료형을 변환하는 메소드 생성

---
+++ CoordinateCalculator/CoordinateCalculator/Process.swift
@@ -0,0 +1,61 @@
+//
+//  Process.swift
+//  CoordinateCalculator
+//
+//  Created by JH on 10/05/2019.
+//  Copyright © 2019 Codesquad Inc. All rights reserved.
+//
+
+import Foundation
+
+enum ErrorMessage: Error {
+    case noInput
+    case incorrectInput
+    case rangeOver
+    
+    var description: String {
+        switch self {
+        case .noInput:
+            return "입력값이 없습니다"
+        case .incorrectInput:
+            return "숫자를 입력해주세요"
+        case .rangeOver:
+            return "입력범위를 초과했습니다"
+        }
+    }
+}
+
+struct Process {
+    
+    static func trimAndSplit(inputValue: String) -> [String] {
+        let trimmedValue = inputValue.trimmingCharacters(in: ["(",")"])
+        let splitedValues = trimmedValue.components(separatedBy: ",")
+        
+        return splitedValues
+    }
+
+    
+    static func makeCoordinateValueFrom(inputs: [String]) -> (Int,Int) {
+        guard let xValue = Int(inputs[0]), let yValue = Int(inputs[1]) else {
+            return (0,0)
+        }
+        
+        return (xValue, yValue)
+    }
+    
+    
+    static func point(from input: (Int,Int)) -> MyPoint {
+        let formattedInput = input
+
+        return MyPoint(x: formattedInput.0, y: formattedInput.1)
+
+    }
+    
+    
+    
+    
+
+
+}
+
+
--- CoordinateCalculator/CoordinateCalculator/main.swift
+++ CoordinateCalculator/CoordinateCalculator/main.swift
@@ -8,10 +8,23 @@
 
 import Foundation
 
-func main() {
+func main() throws {
     
+    //입력
+    let question = "좌표를 입력해주세요(예:(10,10))"
+    let input = try InputView.ask(about: question)
+    
+    //좌표그리기
     OutputView.drawAxis()
     OutputView.drawDot()
+    
+    //값가공하기
+    let str = Process.trimAndSplit(inputValue: input)
+    let coordinate = Process.makeCoordinateValueFrom(inputs: str)
+    let pointValue = Process.point(from: coordinate)
+    
+    dump(pointValue)
 }
 
-main()
+try main()
+
