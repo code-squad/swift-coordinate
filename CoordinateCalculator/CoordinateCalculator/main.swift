175efd548bb1484891071cc77700716e9d4030e9
박준희 <junibibi04@gmail.com>
2019-05-16 02:03:46 +0900

    오류객체 잡아내기
    main파일에서 오류객체 잡아내는 구문 작성
    오류를 발생시키고 좌표값을 다시 입력받게끔 함

--- CoordinateCalculator/CoordinateCalculator/main.swift
+++ CoordinateCalculator/CoordinateCalculator/main.swift
@@ -10,13 +10,31 @@ import Foundation
 
 func main() throws {
     
-    //입력
-    let question = "좌표를 입력해주세요(예:(10,10))"
-    let input = try InputView.ask(about: question)
-    
-    //입력값 가공
-    let processingValue = try Process.checkFormat(inputValue: input)
-    let coordinateValue = try Process.convertForm(checkValue: processingValue)
+    var coordinateValue = (x: 0, y: 0)
+
+    while true {
+        do {
+            let question = "좌표를 입력해주세요(예:(10,10))"
+            let input = try InputView.ask(about: question)
+            
+            let processingValue = try Process.checkFormat(inputValue: input)
+            coordinateValue = try Process.convertForm(checkValue: processingValue)
+            break
+            
+        } catch ErrorMessage.noInputValue {
+            print(ErrorMessage.noInputValue.description)
+        } catch ErrorMessage.incorrectFormet {
+            print(ErrorMessage.incorrectFormet.description)
+        } catch ErrorMessage.incorrectInputValue {
+            print(ErrorMessage.incorrectInputValue.description)
+        } catch ErrorMessage.incorrectInputValueCount {
+            print(ErrorMessage.incorrectInputValueCount.description)
+        } catch ErrorMessage.rangeOver {
+            print(ErrorMessage.rangeOver.description)
+        }catch {
+            print("알수없는오류발생")
+        }
+   }
     
     //좌표값 저장
     let dot = MyPoint(x: coordinateValue.x, y: coordinateValue.y)
