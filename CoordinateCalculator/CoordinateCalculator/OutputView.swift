de040c10f4fd04caec9036ad7ff4b55a88bb574d
chan <clown4014@gmail.com>
2019-05-16 08:59:12 +0900

    출력 가능한 좌표로 바꾸는 함수 outputView구조체로 병합

--- CoordinateCalculator/CoordinateCalculator/OutputView.swift
+++ CoordinateCalculator/CoordinateCalculator/OutputView.swift
@@ -10,16 +10,23 @@ import Foundation
 
 struct OutputView {
     
-    static func drawAxis(_ point:MyPoint) {
+    static func drawAxis(_ point:(Int,Int)) {
         print("\(ANSICode.clear)\(ANSICode.home)")
         print("\(ANSICode.text.whiteBright)\(ANSICode.axis.draw())")
-        draw(point)
+        let convertedCoordinate = convertToCoordinate(point)
+        draw(convertedCoordinate)
     }
     
     private static func draw(_ point:MyPoint) {
 
         print("\(ANSICode.cursor.move(row: point.y, col: point.x))😼\(ANSICode.cursor.move(row: ANSICode.axis.AxisLimit+2, col: ANSICode.axis.AxisLimit+2))")
     
+    }
+    
+    private static func convertToCoordinate(_ point:(Int,Int)) -> MyPoint {
+        let convertedX = point.0 * 2 + 3
+        let convertedY = ANSICode.axis.AxisLimit - point.1 + 1
+        return MyPoint(x: convertedX, y: convertedY)
     }
     
 }
