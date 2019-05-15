86d38b223233f00eb2e150a4670cb018ebb8a40f
kimjieun <zieunv@gmail.com>
2019-05-15 17:14:07 +0900

    converte하는 기능을 하는 함수들 모아놓기

--- CoordinateCalculator/CoordinateCalculator/Converter.swift
+++ CoordinateCalculator/CoordinateCalculator/Converter.swift
@@ -10,7 +10,6 @@ import Foundation
 
 struct Converter {
     
-    
     static func makePoint (pointArray: [Int]) -> MyPoint {
         return MyPoint(x: pointArray[0], y: pointArray[1])
     }
@@ -18,6 +17,28 @@ struct Converter {
     static func makeLine (pointArray: [MyPoint]) -> MyLine {
         return MyLine(pointA: pointArray[0], pointB: pointArray[1])
     }
+    
+    
+    static func validateFormat(input: String) throws -> [Int] {
+        var input = input
+        if try Validator.validateFormatChecker(input: input) {
+            input.removeFirst()
+            input.removeLast()
+            let points = input.split(separator: ",").map {String($0)}
+            let convertPointstoInt = try points.map { (point: String) -> Int in
+                guard let point = Int(point) else {
+                    throw Error.notIntValue
+                }
+                return point
+            }
+            return convertPointstoInt
+        }
+        throw Error.wrongFormat
+    }
+    
+    
+    
+    
     
 }
     
