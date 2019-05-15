28d5c80c99bb24da262a94b52ae12d7a83c210ce
kimjieun <zieunv@gmail.com>
2019-05-15 21:04:56 +0900

    main문에서 있던 기능 구현 코드들을 Converter쪽으로 옮김

--- CoordinateCalculator/CoordinateCalculator/main.swift
+++ CoordinateCalculator/CoordinateCalculator/main.swift
@@ -11,22 +11,12 @@ import Foundation
 func main() {
     
     while true {
-        
+    
         let inputs = InputView.readInput(Question.request)
         do {
-            let coordinates = try inputs.map { try Converter.validateFormat(input: $0) }
+            let coordinates = try inputs.map { try Converter.ConvertInput(input: $0) }
             let points = coordinates.map { Converter.makePoint(pointArray: $0) }
-            switch points.count {
-            case 1:
-                OutputView.drawPoint(points[0])
-            case 2:
-                let line = Converter.makeLine(pointArray: points)
-                OutputView.drawLine(line)
-                
-            default:
-                break
-            }
-            
+            Converter.classifyInput(points: points)
         } catch {
             print(error)
         }
