57dbd126a2d2ef3bb56b8ec44d0532214ff95663
chan <clown4014@gmail.com>
2019-05-18 03:16:18 +0900

    검증과 변환 분리 입력 처리 출력 최상위 함수에서 처리

--- CoordinateCalculator/CoordinateCalculator/OutputView.swift
+++ CoordinateCalculator/CoordinateCalculator/OutputView.swift
@@ -10,22 +10,25 @@ import Foundation
 
 struct OutputView {
     
-    static func drawAxis(_ point:(Int,Int)) {
-        print("\(ANSICode.clear)\(ANSICode.home)")
-        print("\(ANSICode.text.whiteBright)\(ANSICode.axis.draw())")
-        let convertedCoordinate = convertToCoordinate(point)
-        draw(convertedCoordinate)
+    func drawAxis(_ point: MyPoint) {
+        drawBoard()
+        drawDot(point)
     }
     
-    private static func draw(_ point:MyPoint) {
+    private func drawBoard() {
+        print("\(ANSICode.clear)\(ANSICode.home)")
+        print("\(ANSICode.text.whiteBright)\(ANSICode.axis.draw())")
+    }
+    
+    private func drawDot(_ point:MyPoint) {
 
         print("\(ANSICode.cursor.move(row: point.y, col: point.x))😼\(ANSICode.cursor.move(row: ANSICode.axis.AxisLimit+2, col: ANSICode.axis.AxisLimit+2))")
     
     }
     
-    private static func convertToCoordinate(_ point:(Int,Int)) -> MyPoint {
-        let convertedX = point.0 * 2 + 3
-        let convertedY = ANSICode.axis.AxisLimit - point.1 + 1
+    private func convertToCoordinate(_ point:MyPoint) -> MyPoint {
+        let convertedX = point.x * 2 + 3
+        let convertedY = ANSICode.axis.AxisLimit - point.y + 1
         return MyPoint(x: convertedX, y: convertedY)
     }
     
