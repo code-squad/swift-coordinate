0c070a7b8b933e1e1e1d92dd994efab0d104a8b9
junyng <pom0894@naver.com>
2019-05-17 22:02:56 +0900

    Message 구조체 열거형으로 변경 및 CustomStringConvertible 프로토콜 적용

--- CoordinateCalculator/CoordinateCalculator/Message.swift
+++ CoordinateCalculator/CoordinateCalculator/Message.swift
@@ -8,6 +8,16 @@
 
 import Foundation
 
-struct Message {
-    static let unexpectedError = "예상치 못한 에러가 발생하였습니다"
+enum Message: CustomStringConvertible {
+    case unexpectedError(error: Error)
+    case distanceBetweenPoints(distance: Double)
+    
+    var description: String {
+        switch self {
+        case .unexpectedError(let error):
+            return "예상치 못한 에러가 발생하였습니다.\(error)"
+        case .distanceBetweenPoints(let distance):
+            return "점과 점사이의 거리는 \(distance) 입니다."
+        }
+    }
 }
