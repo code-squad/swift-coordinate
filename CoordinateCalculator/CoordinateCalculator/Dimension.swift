c62aeb756636c623e50408e2b0d3ded7119d5625
junyng <pom0894@naver.com>
2019-05-17 23:55:00 +0900

    Dimension 프로토콜 Measurable 로 이름 변경, Measurable 프로토콜 파일 분리

---
+++ CoordinateCalculator/CoordinateCalculator/Dimension.swift
@@ -0,0 +1,13 @@
+//
+//  Measurable.swift
+//  CoordinateCalculator
+//
+//  Created by BLU on 2019. 5. 17..
+//  Copyright © 2019년 Codesquad Inc. All rights reserved.
+//
+
+import Foundation
+
+protocol Measurable {
+    func area() -> Double
+}
