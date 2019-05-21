90a0941b34daed5097d09f7e4f34510d06462160
chan <clown4014@gmail.com>
2019-05-21 12:57:46 +0900

    MyLine구조체 추가

---
+++ CoordinateCalculator/CoordinateCalculator/Myline.swift
@@ -0,0 +1,14 @@
+//
+//  Myline.swift
+//  CoordinateCalculator
+//
+//  Created by 이희찬 on 21/05/2019.
+//  Copyright © 2019 Codesquad Inc. All rights reserved.
+//
+
+import Foundation
+
+struct MyLine {
+    var pointA = MyPoint(x: 0, y: 0)
+    var pointB = MyPoint(x: 0, y: 0)
+}
