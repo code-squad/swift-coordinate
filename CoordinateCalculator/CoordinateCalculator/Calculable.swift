f24e0432ce3cf51d88c8bfe6892a098656622370
kimjieun <zieunv@gmail.com>
2019-05-22 22:14:13 +0900

    Calculatable 프로토콜 구현 및 책임 분리 (팩토리 패턴 적용)

---
+++ CoordinateCalculator/CoordinateCalculator/Calculable.swift
@@ -0,0 +1,14 @@
+//
+//  Calculable.swift
+//  CoordinateCalculator
+//
+//  Created by JieunKim on 22/05/2019.
+//  Copyright © 2019 Codesquad Inc. All rights reserved.
+//
+
+import Foundation
+
+protocol Calculable {
+    var explanation : String { get }
+    var value : Double { get }
+}
---
+++ CoordinateCalculator/CoordinateCalculator/Factory.swift
@@ -0,0 +1,27 @@
+//
+//  Factory.swift
+//  CoordinateCalculator
+//
+//  Created by JieunKim on 22/05/2019.
+//  Copyright © 2019 Codesquad Inc. All rights reserved.
+//
+
+import Foundation
+
+struct Factory {
+static func makePoint (pointArray: [Int]) throws -> MyPoint {
+    guard pointArray.count == 2 else {
+        throw Error.wrongFormat
+    }
+    return MyPoint(x: pointArray[0], y: pointArray[1])
+}
+
+static func makeLine (pointA: MyPoint, pointB: MyPoint) -> Drawable {
+    return MyLine(pointA: pointA, pointB: pointB)
+}
+
+static func makeTriangle (points:[MyPoint]) -> Drawable {
+    return MyTriangle(lineAB: MyLine(pointA: points[0], pointB: points[1]), lineBC:  MyLine(pointA: points[1], pointB: points[2]),lineAC: MyLine(pointA: points[0], pointB: points[2]))
+}
+     }
+    
