3efca84a99feef75644fa1a35ea57f35d2597210
junyng <pom0894@naver.com>
2019-05-24 10:37:29 +0900

    두 점 사이의 거리 케이스명 변경

--- CoordinateCalculator/CoordinateCalculator/Message.swift
+++ CoordinateCalculator/CoordinateCalculator/Message.swift
@@ -10,14 +10,14 @@ import Foundation
 
 enum Message: CustomStringConvertible {
     case unexpectedError(error: Error)
-    case distanceBetweenPoints(distance: Double)
+    case distanceBetweenTwoPoints(distance: Double)
     case triangleArea(_ area: Double)
     
     var description: String {
         switch self {
         case .unexpectedError(let error):
             return "예상치 못한 에러가 발생하였습니다.\(error)"
-        case .distanceBetweenPoints(let distance):
+        case .distanceBetweenTwoPoints(let distance):
             return "점과 점사이의 거리는 \(distance) 입니다."
         case .triangleArea(let area):
             return "삼각형 넓이는 \(area) 입니다."
--- CoordinateCalculator/CoordinateCalculator/Shapes/MyLine.swift
+++ CoordinateCalculator/CoordinateCalculator/Shapes/MyLine.swift
@@ -16,7 +16,7 @@ struct MyLine: Shape, Measurable, Equatable {
         return sqrt(pow(Double(pointA.x - pointB.x), 2) + pow(Double(pointA.y - pointB.y), 2))
     }
     var areaDescription: String {
-        return Message.distanceBetweenPoints(distance: area).description
+        return Message.distanceBetweenTwoPoints(distance: area).description
     }
     
     init?(pointA: MyPoint, pointB: MyPoint) {
