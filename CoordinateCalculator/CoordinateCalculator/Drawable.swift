797e075ba369506496c978022c3e4f05b42deda9
박준희 <junibibi04@gmail.com>
2019-05-25 15:14:26 +0900

    포인트를 출력하는 프로토콜 생성

---
+++ CoordinateCalculator/CoordinateCalculator/Drawable.swift
@@ -0,0 +1,13 @@
+//
+//  Drawable.swift
+//  CoordinateCalculator
+//
+//  Created by JH on 24/05/2019.
+//  Copyright © 2019 Codesquad Inc. All rights reserved.
+//
+
+import Foundation
+
+protocol Drawable {
+    func drawablePoint() -> [MyPoint]
+}
