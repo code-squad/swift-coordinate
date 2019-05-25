4e5320bdadee1a15e6eedf8f71c7dc5cf5650152
chan <clown4014@gmail.com>
2019-05-25 23:37:09 +0900

    Drawable protocol 생성

---
+++ CoordinateCalculator/CoordinateCalculator/Drawable.swift
@@ -0,0 +1,13 @@
+//
+//  Drawable.swift
+//  CoordinateCalculator
+//
+//  Created by 이희찬 on 25/05/2019.
+//  Copyright © 2019 Codesquad Inc. All rights reserved.
+//
+
+import Foundation
+
+protocol Drawable {
+    var pointNumber:Int { get }
+}
