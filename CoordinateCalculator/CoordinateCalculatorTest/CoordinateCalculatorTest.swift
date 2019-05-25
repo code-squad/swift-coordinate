33b5b57fa2923466826d350f3d778e1c09234586
chan <clown4014@gmail.com>
2019-05-25 13:04:37 +0900

    test case 추가

--- CoordinateCalculator/CoordinateCalculatorTest/CoordinateCalculatorTest.swift
+++ CoordinateCalculator/CoordinateCalculatorTest/CoordinateCalculatorTest.swift
@@ -15,42 +15,50 @@ class CoordinateCalculatorTest: XCTestCase {
     }
     
     func testCorrectFormat() {
-        XCTAssertNoThrow(try converter.convertInput("(0,0)",Validator()))
+        XCTAssertNoThrow(try converter.convertInput("(0,0)"))
+    }
+    
+    func testCorrectFormatOfLine() {
+        XCTAssertNoThrow(try converter.convertInput("(0,0)-(0,0)"))
     }
     
     func testInCorrectFormat() {
-        XCTAssertThrowsError(try converter.convertInput("0,0",Validator()))
+        XCTAssertThrowsError(try converter.convertInput("0,0"))
+    }
+    
+    func testInCorrectFormatOfLine() {
+        XCTAssertThrowsError(try converter.convertInput("(0,0)(0,0)"))
     }
     
     func testConsistOfTwoItem() {
-        XCTAssertNoThrow(try converter.convertInput("(0,0)",Validator()))
+        XCTAssertNoThrow(try converter.convertInput("(0,0)"))
     }
     
     func testNotConsistOfTwoItem() {
-        XCTAssertThrowsError(try converter.convertInput("(0,0,0)",Validator()))
+        XCTAssertThrowsError(try converter.convertInput("(0,0,0)"))
     }
     
     func testCanConvertToInt() {
-        XCTAssertNoThrow(try converter.convertInput("(0,0)",Validator()))
+        XCTAssertNoThrow(try converter.convertInput("(0,0)"))
     }
     
     func testCanNotConvertToInt() {
-        XCTAssertThrowsError(try converter.convertInput("(zero,zero)",Validator()))
+        XCTAssertThrowsError(try converter.convertInput("(zero,zero)"))
     }
     
     func testMoreThanZero() {
-        XCTAssertNoThrow(try converter.convertInput("(0,0)",Validator()))
+        XCTAssertNoThrow(try converter.convertInput("(0,0)"))
     }
     
     func testBelowZero() {
-        XCTAssertThrowsError(try converter.convertInput("(-1,-1)",Validator()))
+        XCTAssertThrowsError(try converter.convertInput("(-1,-1)"))
     }
     
     func testUnderTwentyFour() {
-        XCTAssertNoThrow(try converter.convertInput("(24,24)",Validator()))
+        XCTAssertNoThrow(try converter.convertInput("(24,24)"))
     }
     
     func testExcessTwentyFour() {
-        XCTAssertThrowsError(try converter.convertInput("(25,25)",Validator()))
+        XCTAssertThrowsError(try converter.convertInput("(25,25)"))
     }
 }
