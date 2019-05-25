bbf87a5f36ced1c21e1bb512a13d97f770358227
chan <clown4014@gmail.com>
2019-05-26 00:07:42 +0900

    Mypoint 초기화 함수 추가

--- CoordinateCalculator/CoordinateCalculator/Myline.swift
+++ CoordinateCalculator/CoordinateCalculator/Myline.swift
@@ -10,6 +10,6 @@ import Foundation
 
 struct MyLine:Drawable {
     var pointNumber: Int = 2
-    var pointA = MyPoint()
-    var pointB = MyPoint()
+    var pointA = MyPoint(x: 0, y: 0)
+    var pointB = MyPoint(x: 0, y: 0)
 }
