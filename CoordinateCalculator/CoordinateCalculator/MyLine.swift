1f41bf78c76faa3de12c4c51d7a5c4db6790eb63
박준희 <junibibi04@gmail.com>
2019-05-27 00:12:57 +0900

    두 좌표의 거리를 계산하는 메소드 생성

--- CoordinateCalculator/CoordinateCalculator/MyLine.swift
+++ CoordinateCalculator/CoordinateCalculator/MyLine.swift
@@ -16,4 +16,15 @@ struct MyLine: DrawableShape {
     func drawablePoints() -> [MyPoint] {
         return [pointA, pointB]
     }
+    
+    
+    func distance() -> Double {
+        return sqrt(pow(Double(pointA.x - pointB.x), 2) + pow(Double(pointA.y - pointB.y), 2))
+    }
+    
 }
+/*
+ pow(<#T##Double#>, <#T##Double#>)
+ pow(제곱당하는 수, 하려고하는 제곱의 수)
+ */
+
