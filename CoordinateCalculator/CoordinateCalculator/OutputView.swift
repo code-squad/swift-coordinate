c5c0dc560acef1b0db5c06851ea61942cce30c84
박준희 <junibibi04@gmail.com>
2019-05-27 00:17:53 +0900

    도형(좌표점의갯수)에 따라 좌표를 출력하는 메소드 생성 내부에서 호출되는 메소드 private 해줌 두좌표의 거리를 출력하는 메소드 생성 좌표의 색을 바꿔보았음

--- CoordinateCalculator/CoordinateCalculator/OutputView.swift
+++ CoordinateCalculator/CoordinateCalculator/OutputView.swift
@@ -10,23 +10,30 @@ import Foundation
 
 struct OutputView {
     
-    static func draw(shape: Drawable) {
+    static func draw(shape: DrawableShape) {
         for point in shape.drawablePoints() {
-            drawDot(at: point)
+            draw(point: point)
         }
     }
 
     static func drawAxis(){
         print("\(ANSICode.clear)\(ANSICode.home)")
-        print("\(ANSICode.text.whiteBright)\(ANSICode.axis.draw())")
+        print("\(ANSICode.text.redBright)\(ANSICode.axis.draw())")
     }
     
-    static func drawDot(at value: MyPoint) {
+    private static func draw(point: MyPoint) {
         
         let startPoint = ANSICode.axis.AxisLimit + 1
-        let x = value.x
-        let y = value.y
+        let x = point.x
+        let y = point.y
 
         print("\(ANSICode.cursor.move(row: startPoint-y, col: (x*2)+2))😆\(ANSICode.cursor.move(row: startPoint+1, col: (startPoint*2)+2))")
     }
+    
+    static func printDistance(line: MyLine) {
+        
+        print(line.distance())
+        
+    }
+    
 }
