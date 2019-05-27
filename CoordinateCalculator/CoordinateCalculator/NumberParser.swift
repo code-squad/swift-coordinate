9fe7fa9f28a758b8a8052cf5759e7dfb26888e96
junyng <pom0894@naver.com>
2019-05-27 22:19:42 +0900

    ShapeConverter 구조체에서 숫자 파싱 구조체로 파싱 에러를 갖도록 분리

--- CoordinateCalculator/CoordinateCalculator/NumberParser.swift
+++ CoordinateCalculator/CoordinateCalculator/NumberParser.swift
@@ -10,9 +10,24 @@ import Foundation
 
 struct NumberParser {
     
-    func parseNumbers(_ input: String) -> [Int] {
+    enum Error: Swift.Error {
+        case parseFailed
+        
+        var localizedDescription: String {
+            switch self {
+            case .parseFailed:
+                return "숫자 파싱에 실패하였습니다."
+            }
+        }
+    }
+    
+    func parseNumbers(_ input: String) throws -> [Int] {
         let regex = "-?[0-9]+"
         let numbers = input.matches(for: regex)
-        return numbers.compactMap { Int($0) }
+        let parsedNumbers = numbers.compactMap { Int($0) }
+        guard parsedNumbers.count == numbers.count else {
+            throw Error.parseFailed
+        }
+        return parsedNumbers
     }
 }
