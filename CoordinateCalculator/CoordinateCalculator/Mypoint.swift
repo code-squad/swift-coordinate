ff4d1367f658f2b16a875350aee3dae2c16ef772
chan <clown4014@gmail.com>
2019-05-27 14:13:38 +0900

    접근제어

--- CoordinateCalculator/CoordinateCalculator/Mypoint.swift
+++ CoordinateCalculator/CoordinateCalculator/Mypoint.swift
@@ -9,9 +9,9 @@
 import Foundation
 
 struct MyPoint:Drawable {
-    var pointNumber: Int = 1
-    var x = 0
-    var y = 0
+    private(set) var pointNumber: Int = 1
+    private(set) var x = 0
+    private(set) var y = 0
     
     init(x:Int,y:Int) {
         self.x = x
