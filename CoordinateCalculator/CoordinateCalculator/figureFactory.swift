08427c953dcf31f6deabb025390909b4e4772f3b
chan <clown4014@gmail.com>
2019-05-28 20:48:06 +0900

    도형을 만드는 함수 전체적인 구조 수정

--- CoordinateCalculator/CoordinateCalculator/figureFactory.swift
+++ CoordinateCalculator/CoordinateCalculator/figureFactory.swift
@@ -13,11 +13,21 @@ struct figureFactory {
     private static var converter = Converter()
     
     static func makeFigure(_ points:[coordinate],_ validator:Validatable) throws -> Drawable {
-        if points.count == 2 {
+        switch points.count {
+        case 1:
+            return MyPoint(x: points[0].0, y: points[0].1)
+        case 2:
             let pointA = MyPoint(x: points[0].0, y: points[0].1)
             let pointB = MyPoint(x: points[1].0, y: points[1].1)
             return MyLine(pointA: pointA, pointB: pointB)
+        case 3:
+            let pointA = MyPoint(x: points[0].0, y: points[0].1)
+            let pointB = MyPoint(x: points[1].0, y: points[1].1)
+            let pointC = MyPoint(x: points[2].0, y: points[2].1)
+            return MyTriangle(pointA: pointA, pointB: pointB, pointC: pointC)
+        default:
+            throw UserInputError.isNotFigure
         }
-        return MyPoint(x: points[0].0, y: points[0].1)
     }
+    
 }
