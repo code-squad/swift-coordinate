dc7ccb289a0e89fae10dde05f34c3f0645008983
박준희 <junibibi04@gmail.com>
2019-05-29 01:29:06 +0900

    MyTriangle 구조체 생성
    프로퍼티를 선언
    점3개로 직선3개를 구현하도록 이니셜라이저 추가
    DrawableShape프로토콜을 채택하고, 점3개를 반환하는 메소드코드 추가
    Myline의 프로퍼티명이 사용함에 불편함이 있어(헷갈려서) start / end로 수정함

---
+++ CoordinateCalculator/CoordinateCalculator/MyTriangle.swift
@@ -0,0 +1,33 @@
+//
+//  ㅎㅎ.swift
+//  CoordinateCalculator
+//
+//  Created by JH on 29/05/2019.
+//  Copyright © 2019 Codesquad Inc. All rights reserved.
+//
+
+import Foundation
+
+struct MyTriangle: DrawableShape {
+    
+    var lineAB: MyLine
+    var lineBC: MyLine
+    var lineAC: MyLine
+    
+    //    var lineAB = MyLine(startPoint: MyPoint(), endPoint: MyPoint())
+    //    var lineBC = MyLine(startPoint: MyPoint(), endPoint: MyPoint())
+    //    var lineAC = MyLine(startPoint: MyPoint(), endPoint: MyPoint())
+    
+    init(pointA: MyPoint, pointB: MyPoint, pointC: MyPoint) {
+        self.lineAB = MyLine(startPoint: pointA, endPoint: pointB)
+        self.lineBC = MyLine(startPoint: pointB, endPoint: pointC)
+        self.lineAC = MyLine(startPoint: pointA, endPoint: pointC)
+    }
+    
+    
+    func drawablePoints() -> [MyPoint] {
+        return [lineAB.startPoint, lineBC.startPoint, lineAC.endPoint]
+    }
+    
+    
+}
