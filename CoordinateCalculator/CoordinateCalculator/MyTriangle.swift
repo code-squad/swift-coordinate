14da84573dfa08e477ebf271d1c1ed5726b4deaf
chan <clown4014@gmail.com>
2019-05-28 20:45:47 +0900

    MyTriangle 구조체 추가

--- CoordinateCalculator/CoordinateCalculator/MyTriangle.swift
+++ CoordinateCalculator/CoordinateCalculator/MyTriangle.swift
@@ -7,3 +7,27 @@
 //
 
 import Foundation
+
+struct MyTriangle:Drawable {
+    
+    var lineA = MyLine(pointA: MyPoint(x: 0, y: 0), pointB: MyPoint(x: 0, y: 0))
+    var lineB = MyLine(pointA: MyPoint(x: 0, y: 0), pointB: MyPoint(x: 0, y: 0))
+    var lineC = MyLine(pointA: MyPoint(x: 0, y: 0), pointB: MyPoint(x: 0, y: 0))
+    
+    var points:[MyPoint] {
+        return [lineA.pointA,lineB.pointA,lineC.pointA]
+    }
+    
+    var trianglearea:Double {
+        let a = (lineA.pointA.x * lineB.pointA.y)+(lineB.pointA.x * lineC.pointA.y)+(lineC.pointA.x * lineA.pointA.y)
+        let b = (lineA.pointA.x * lineC.pointA.y)+(lineC.pointA.x * lineB.pointA.y)+(lineB.pointA.x * lineA.pointA.y)
+        return Double(abs(a-b)) / 2.0
+    }
+    
+    init(pointA: MyPoint, pointB: MyPoint, pointC: MyPoint) {
+        self.lineA = MyLine(pointA: pointA, pointB: pointB)
+        self.lineB = MyLine(pointA: pointB, pointB: pointC)
+        self.lineC = MyLine(pointA: pointC, pointB: pointA)
+    }
+    
+}
