5c71e7dd884fa461ecf33af68df2de4797574266
박준희 <junibibi04@gmail.com>
2019-05-29 01:36:39 +0900

    좌표값을 받아서 도형을 만드는 메소드
    점3개일때 삼각형을 그리는 경우를 추가

--- CoordinateCalculator/CoordinateCalculator/ShapeMaker.swift
+++ CoordinateCalculator/CoordinateCalculator/ShapeMaker.swift
@@ -16,6 +16,8 @@ struct ShapeMaker {
             return MyPoint(x: coordinates[0].x, y: coordinates[0].y)
         case 2:
             return MyLine(startPoint: MyPoint(x: coordinates[0].x, y: coordinates[0].y), endPoint: MyPoint(x: coordinates[1].x, y: coordinates[1].y))
+        case 3:
+            return MyTriangle(pointA: MyPoint(x: coordinates[0].x, y: coordinates[0].y), pointB: MyPoint(x: coordinates[1].x, y: coordinates[1].y), pointC: MyPoint(x: coordinates[2].x, y: coordinates[2].y))
         default:
             return nil
         }
