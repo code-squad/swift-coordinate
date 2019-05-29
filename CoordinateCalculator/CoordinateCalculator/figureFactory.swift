82fb9af20c15249ed44025bd78b90d90cd59a15c
chan <clown4014@gmail.com>
2019-05-29 23:59:44 +0900

    검증하고 객체를 생성하도록 수정

--- CoordinateCalculator/CoordinateCalculator/figureFactory.swift
+++ CoordinateCalculator/CoordinateCalculator/figureFactory.swift
@@ -12,7 +12,7 @@ struct figureFactory {
     
     private static var converter = Converter()
     
-    static func makeFigure(_ points:[coordinate],_ validator:Validatable) throws -> Drawable {
+    static func makeFigure(_ points:[coordinate]) throws -> Drawable {
         
         let myPoints:[MyPoint] = points.map{MyPoint(x: $0.0, y: $0.1)}
         
@@ -20,10 +20,15 @@ struct figureFactory {
         case 1:
             return myPoints[0]
         case 2:
-            return MyLine(pointA: myPoints[0], pointB: myPoints[1])
+            let myLine = MyLine(pointA: myPoints[0], pointB: myPoints[1])
+            try Validator.canDrawLine(line: myLine)
+            return myLine
         case 3:
-            return MyTriangle(pointA: myPoints[0], pointB: myPoints[1], pointC: myPoints[2])
+            let triangle = MyTriangle(pointA: myPoints[0], pointB: myPoints[1], pointC: myPoints[2])
+            try Validator.canDrawTriangle(triangle: triangle)
+            return triangle
         case 4:
+            try Validator.canDrawRect(points: points)
             let RectInitItem = convertMyRectInitFormat(points: myPoints)
             return MyRect(origin: RectInitItem.0, size: RectInitItem.1, points: myPoints)
         default:
