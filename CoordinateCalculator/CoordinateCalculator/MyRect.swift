4d2291e25772d7cb1ddf5a5b9a072791aca6cc0f
박준희 <junibibi04@gmail.com>
2019-06-01 16:57:53 +0900

    drawablePoints 구현

--- CoordinateCalculator/CoordinateCalculator/MyRect.swift
+++ CoordinateCalculator/CoordinateCalculator/MyRect.swift
@@ -8,13 +8,17 @@
 
 import Foundation
 
-struct MyRect {
+struct MyRect: DrawableShape {
     var leftTop: MyPoint
     var rightBottom: MyPoint
 
     init(origin: MyPoint, size: CGSize) {
         self.leftTop = MyPoint(x: origin.x, y: origin.y)
         self.rightBottom = MyPoint(x: origin.x + Int(size.width), y: origin.y - Int(size.height))
+    }
+    
+    var drawablePoints: [MyPoint] {
+        return [leftTop, rightBottom, MyPoint(x: leftTop.x, y: rightBottom.y), MyPoint(x: rightBottom.x, y: leftTop.y)]
     }
 
 }
