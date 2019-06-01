658b655f9a2ce4486516b3e2a91bc37698ce08fb
박준희 <junibibi04@gmail.com>
2019-06-01 16:51:59 +0900

    ShapeMaker 파일 수정
    입력받은 좌표값을 셋타입으로 만들어주는 메소드 생성
    최대최소값을 구하는 메소드 생성
    makeShape에 점이4개일때 케이스코드 생성

--- CoordinateCalculator/CoordinateCalculator/ShapeMaker.swift
+++ CoordinateCalculator/CoordinateCalculator/ShapeMaker.swift
@@ -19,29 +19,45 @@ struct ShapeMaker {
         case 3:
             return MyTriangle(pointA: MyPoint(x: coordinates[0].x, y: coordinates[0].y), pointB: MyPoint(x: coordinates[1].x, y: coordinates[1].y), pointC: MyPoint(x: coordinates[2].x, y: coordinates[2].y))
         case 4:
-            return MyRect(origin: <#T##MyPoint#>, size: <#T##CGSize#>)
+            let coordinateSet = makeCoordinateSet(from: coordinates)
+            let xStat = calculateStatistics(numbers: coordinateSet.xValues)
+            let yStat = calculateStatistics(numbers: coordinateSet.yValues)
+        
+            return MyRect(origin: MyPoint(x: xStat.min y: yStat.max), size: CGSize(width: xStat.max - xStat.min, height: yStat.max - yStat.min))
         default:
             return nil
         }
     }
     
-    
-    private static func makeOrigin(coordinates: [(x: Int, y: Int)]) -> (x: Int, y: Int) {
-        // 받을값중에 임의로 최소최대값의 초기값을 설정해줌
-        var minX = coordinates[0].x
-        var maxY = coordinates[0].y
+    /// 최대 최소값 구하는 메소드
+    private static func calculateStatistics(numbers: Set<Int>) -> (min: Int, max: Int) {
+        var maxNum = Int.min
+        var minNum = Int.max
         
-        // 받은 좌표값의 원소 하나씩 돌면서 최대 최소값을 찾아서 리턴한다.
-        for coordinate in coordinates {
-            if minX > coordinate.x {
-                minX = coordinate.x
+        for num in numbers {
+            if maxNum < num {
+                maxNum = num
             }
-            if maxY < coordinate.y {
-                maxY = coordinate.y
+            if minNum > num {
+                minNum = num
             }
-        return (x: minX, y: maxY)
         }
+        return (min: minNum, max: maxNum)
     }
     
+    /// 입력받은 튜플값을 셋으로 만들어주는 메소드
+    private static func makeCoordinateSet(from coordinates: [(x: Int, y: Int)]) -> (xValues: Set<Int>, yValues: Set<Int>) {
+       
+        var xValues = Set<Int>()
+        var yValues = Set<Int>()
+        
+        //좌표값배열요소 하나씩 접근해서 set타입의 변수에 넣어준다.
+        for coordinate in coordinates {
+            xValues.insert(coordinate.x)
+            yValues.insert(coordinate.y)
+        }
+        return (xValues, yValues)
+    }
+
     
 }
