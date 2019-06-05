1215866a8788067ad7538ea545ad9219dba0340f
Jung Kim <godrm77@gmail.com>
2019-06-04 23:05:09 -0700

    Revert "출력 가능한 좌표로 바꾸는 함수 outputView구조체로 병합"
    This reverts commit de040c10f4fd04caec9036ad7ff4b55a88bb574d.

--- CoordinateCalculator/CoordinateCalculator/OutputView.swift
+++ CoordinateCalculator/CoordinateCalculator/OutputView.swift
@@ -10,23 +10,16 @@ import Foundation
 
 struct OutputView {
     
-    static func drawAxis(_ point:(Int,Int)) {
+    static func drawAxis(_ point:MyPoint) {
         print("\(ANSICode.clear)\(ANSICode.home)")
         print("\(ANSICode.text.whiteBright)\(ANSICode.axis.draw())")
-        let convertedCoordinate = convertToCoordinate(point)
-        draw(convertedCoordinate)
+        draw(point)
     }
     
     private static func draw(_ point:MyPoint) {
 
         print("\(ANSICode.cursor.move(row: point.y, col: point.x))😼\(ANSICode.cursor.move(row: ANSICode.axis.AxisLimit+2, col: ANSICode.axis.AxisLimit+2))")
     
-    }
-    
-    private static func convertToCoordinate(_ point:(Int,Int)) -> MyPoint {
-        let convertedX = point.0 * 2 + 3
-        let convertedY = ANSICode.axis.AxisLimit - point.1 + 1
-        return MyPoint(x: convertedX, y: convertedY)
     }
     
 }
