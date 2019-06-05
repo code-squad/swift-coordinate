6013185ad63b1b493706d7eca32cdf7831e374a2
Jung Kim <godrm77@gmail.com>
2019-06-04 23:05:09 -0700

    Revert "validator구조체내 함수가   단위테스틀 모두 통과하게 구현 UserInput관련 Error를 정의하는 열거형 생성"
    This reverts commit 61b9cda97d39ec11d157efecd623e3f41bc12e3a.

--- CoordinateCalculator/CoordinateCalculator/Validator.swift
+++ CoordinateCalculator/CoordinateCalculator/Validator.swift
@@ -8,58 +8,5 @@
 
 import Foundation
 struct Validator {
-    var userInput = ""
-    
-    mutating func verifyInput(_ userInputOptionalString:String?)throws ->MyPoint {
-        try convertString(userInputOptionalString)
-        try isCorrectFormat()
-        let seperatedUserInput = seperate()
-        try hasTwoItem(seperatedUserInput)
-        let point = try convertInt(seperatedUserInput)
-        try isInRange(point.x)
-        try isInRange(point.y)
-        return point
-    }
-    
-    
-    private mutating func convertString(_ optionalString:String?)throws {
-        guard let convertedString = optionalString else{
-            throw UserInputError.isNotString
-        }
-        userInput = convertedString
-    }
-    
-    private func isCorrectFormat()throws {
-        guard userInput.first == "(" && userInput.last == ")" else{
-            throw UserInputError.inCorrectFormat
-        }
-    }
-    
-    private mutating func seperate()->[String] {
-        userInput = userInput.trimmingCharacters(in: ["(",")"])
-        return userInput.components(separatedBy: ",")
-    }
-    
-    private mutating func hasTwoItem(_ seperatedUserInput:[String])throws {
-        guard seperatedUserInput.count == 2 else{
-            throw UserInputError.inCorrectItemNumber
-        }
-    }
-    
-    private mutating func convertInt(_ seperatedUserInput:[String])throws ->MyPoint{
-        guard let x = Int(seperatedUserInput[0]) else{
-            throw UserInputError.canNotConvertTOInt
-        }
-        guard let y = Int(seperatedUserInput[1]) else{
-            throw UserInputError.canNotConvertTOInt
-        }
-        return MyPoint(x: x, y: y)
-    }
-    
-    private func isInRange(_ point: Int)throws {
-        let range = 0...ANSICode.axis.AxisLimit
-        guard range.contains(point) else {
-            throw UserInputError.outOfRange
-        }
-    }
+    <#fields#>
 }
