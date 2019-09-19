ca36d339d7320e8c8ab80bbf95893122a5578824
Jake YOU <jake.you@kinemaster.com>
2019-09-20 03:08:54 +0900

    Remove dependency between constants and MyPoint initializer

--- CoordinateCalculator/CoordinateCalculator/MyPoint.swift
+++ CoordinateCalculator/CoordinateCalculator/MyPoint.swift
@@ -12,15 +12,3 @@ struct MyPoint: Equatable {
     var x = 0
     var y = 0
 }
-
-extension MyPoint {
-    init?(coordinates: String) {
-        let userInput = coordinates.trimmingCharacters(in: ["(",")"]).components(separatedBy: ",")
-        guard let x = userInput.first, let y = userInput.last,
-                let coordinateX = Int(x), let coordinateY = Int(y) else {
-            return nil
-        }
-        self.x = (coordinateX + CoordinateConstants.xAxisStartPos) * CoordinateConstants.coordinatesMagnifyingFactor.x
-        self.y = CoordinateConstants.yAxisEndPos - (coordinateY * CoordinateConstants.coordinatesMagnifyingFactor.y)
-    }
-}
