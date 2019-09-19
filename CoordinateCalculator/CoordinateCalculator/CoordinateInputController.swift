62598f449beca3efad0d5da11605b24836670113
Jake YOU <jake.you@kinemaster.com>
2019-09-20 03:08:54 +0900

    Remove dependency between constants and MyPoint initializer

--- CoordinateCalculator/CoordinateCalculator/CoordinateInputController.swift
+++ CoordinateCalculator/CoordinateCalculator/CoordinateInputController.swift
@@ -19,10 +19,12 @@ struct CoordinateInputController {
                 userInput = CoordinateInputView.read(with: .retry(error: "잘못된 형식입니다:[입력:\(userInput)]"))
                 continue
             }
-            guard let point = MyPoint(coordinates: userInput) else {
+            guard let coordinate = convert(userInput: userInput) else {
                 userInput = CoordinateInputView.read(with: .retry(error: "내부 에러가 발생했습니다."))
                 continue
             }
+
+            let point = MyPoint(x: coordinate.x, y: coordinate.y)
             guard coordinates.contains(point) == false else {
                 userInput = CoordinateInputView.read(with: .retry(error: "동일한 데이터가 존재합니다."))
                 continue
@@ -39,6 +41,16 @@ struct CoordinateInputController {
     func isValid(userInput: String) -> Bool {
         let regex = "\\(([0-1]{0,1}[0-9]|[2][0-4]),\\s{0,1}([0-1]{0,1}[0-9]|[2][0-4])\\)"
         return userInput.range(of: regex, options: .regularExpression, range: nil, locale: nil) != nil
+    }
+    
+    func convert(userInput: String) -> (x: Int, y: Int)? {
+        let userInputs = userInput.trimmingCharacters(in: ["(",")"]).components(separatedBy: ",")
+        guard let x = userInputs.first, let y = userInputs.last,
+            let coordinateX = Int(x), let coordinateY = Int(y) else {
+                return nil
+        }
+        return ((coordinateX + CoordinateConstants.xAxisStartPos) * CoordinateConstants.coordinatesMagnifyingFactor.x,
+                CoordinateConstants.yAxisEndPos - (coordinateY * CoordinateConstants.coordinatesMagnifyingFactor.y))
     }
 }
 
