b0a0a4a9bbd8c87f2be070d7caad545d6c43a6b4
Changhee-Byun <changhee.byun@kinemaster.com>
2019-09-20 04:38:21 +0900

    Add MyPoint, InputView, OutputView structs.. Implementation of the requirements1.

---
+++ CoordinateCalculator/CoordinateCalculator/OutputView.swift
@@ -0,0 +1,37 @@
+//
+//  OutputView.swift
+//  CoordinateCalculator
+//
+//  Created by temphee.Reid on 20/09/2019.
+//  Copyright © 2019 Codesquad Inc. All rights reserved.
+//
+
+import Foundation
+
+struct OutputView {
+    static private let xLegend = 2 //원래 3이지만 특수문자의 경우 2가 눈에 맞아보임.
+    static private let yLegend = 25
+    static private let xPosScale = 2
+    static private let descriptionPos = MyPoint(x: 0, y: 29)
+    
+    //static private let explodingHead = "\u{1F92F}"
+    //static private let coffee = "\u{2615}"
+    //static private let babyChick = "\u{1F424}"
+    //static private let roundPushPin = "\u{1F4CD}"
+    static private let whale = "\u{1F433}"
+    //static private let peace = "\u{262E}"
+    //static private let normal = "*"
+    static private let positonMark = whale
+    
+    static func showPointOnCoordinatePlane(point : MyPoint) {
+        print("\(ANSICode.clear)\(ANSICode.home)")
+        print("\(ANSICode.text.whiteBright)\(ANSICode.axis.draw())")
+        
+        print("\(ANSICode.cursor.move(row: yLegend-point.y, col: xLegend+xPosScale*(point.x)))", terminator: "")
+        
+        print("\(ANSICode.text.green)\(positonMark)", terminator: "")
+        
+        print("\(ANSICode.cursor.move(row: descriptionPos.y, col: descriptionPos.x))", terminator: "")
+        print("\t\(ANSICode.text.white)(x = \(point.x), y = \(point.y))")
+    }
+}
