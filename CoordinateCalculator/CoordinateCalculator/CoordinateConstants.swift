4492bc9a53015202ee9871a91826f5edb7a6d577
Jake YOU <jake.you@kinemaster.com>
2019-09-20 02:52:09 +0900

    Implement requirement of Step-1

---
+++ CoordinateCalculator/CoordinateCalculator/CoordinateConstants.swift
@@ -0,0 +1,20 @@
+//
+//  CoordinateConstants.swift
+//  CoordinateCalculator
+//
+//  Created by sungchan.you on 2019/09/20.
+//  Copyright © 2019 Codesquad Inc. All rights reserved.
+//
+
+import Foundation
+
+struct CoordinateConstants {
+
+    static let maxUserInput: Int = 24
+    
+    static let coordinatesMagnifyingFactor: (x: Int, y: Int) = (2, 1)
+    
+    static let yAxisEndPos = 25
+    
+    static let xAxisStartPos  = 1
+}
