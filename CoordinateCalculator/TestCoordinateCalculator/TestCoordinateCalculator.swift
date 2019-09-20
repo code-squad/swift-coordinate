6be2f16e5510256e579f31151dcc30569955375b
kyler.jeon <kyler.jeon@kinemaster.com>
2019-09-21 04:19:08 +0900

    make frame
    > 프로젝트 틀 만듦

---
+++ CoordinateCalculator/TestCoordinateCalculator/TestCoordinateCalculator.swift
@@ -0,0 +1,28 @@
+//
+/******************************************************************************
+ * File Name        : TestCoordinateCalculator.swift
+ * Description      : CoordinateCalculator
+ *******************************************************************************
+ * Copyright (c) 2002-2019 KineMaster Corp. All rights reserved.
+ * https://www.kinemastercorp.com/
+ *
+ * THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY
+ * KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR
+ * PURPOSE.
+ ******************************************************************************/
+
+import XCTest
+
+class TestCoordinateCalculator: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    
+}
