6697f8b935c6deb60b70160c976889321a45f0e4
dominico.park <dominico.park@kinemaster.com>
2019-09-24 00:57:48 +0900

    step2

---
+++ CoordinateCalculator/CoordinateCalculator/MyLine.swift
@@ -0,0 +1,14 @@
+//
+//  MyLine.swift
+//  CoordinateCalculator
+//
+//  Created by junwoo on 2019/09/24.
+//  Copyright © 2019 Codesquad Inc. All rights reserved.
+//
+
+import Foundation
+
+struct MyLine {
+    var pointA: MyPoint
+    var pointB: MyPoint
+}
