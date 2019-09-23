071f56db68cd134dc15b9cb72ddaa5fdab7d08ff
dominico.park <dominico.park@kinemaster.com>
2019-09-23 23:59:11 +0900

    step1

---
+++ CoordinateCalculator/CoordinateCalculator/MyPoint.swift
@@ -0,0 +1,14 @@
+//
+//  MyPoint.swift
+//  CoordinateCalculator
+//
+//  Created by junwoo on 2019/09/23.
+//  Copyright © 2019 Codesquad Inc. All rights reserved.
+//
+
+import Foundation
+
+struct MyPoint {
+    var x: Int
+    var y: Int
+}
