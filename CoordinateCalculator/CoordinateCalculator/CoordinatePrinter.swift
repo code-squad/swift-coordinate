90ae3ca46baa19b8d0905877c44a31082e1786f0
reno-kim-km <reno.kim@kinemaster.com>
2019-09-23 21:25:13 +0900

    좌표 sync 안 맞던 이슈 해결

--- CoordinateCalculator/CoordinateCalculator/CoordinatePrinter.swift
+++ CoordinateCalculator/CoordinateCalculator/CoordinatePrinter.swift
@@ -16,8 +16,9 @@ struct CoordinatePrinter {
     }
     
     func printPoint() {
+        print(self.point.toString())
         print("\(ANSICode.clear)\(ANSICode.home)")
-        print("\(ANSICode.cursor.move(row:point.x, col: point.y))*")
+        print("\(ANSICode.cursor.move(row:25 - point.y, col: point.x * 2 + 2))⭐️")
         print("\(ANSICode.text.whiteBright)\(ANSICode.axis.draw())")
     }
 }
