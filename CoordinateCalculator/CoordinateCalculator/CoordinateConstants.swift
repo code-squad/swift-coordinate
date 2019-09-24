f00ce853e038905971262cebb49dc4ab22c7ccc4
Jake YOU <jake.you@kinemaster.com>
2019-09-25 00:18:54 +0900

    Increase max user input retry count

--- CoordinateCalculator/CoordinateCalculator/CoordinateConstants.swift
+++ CoordinateCalculator/CoordinateCalculator/CoordinateConstants.swift
@@ -10,7 +10,7 @@ import Foundation
 
 struct CoordinateConstants {
     
-    static let maxUserInput = 1
+    static let maxUserInput = 3
     
     static let coordinatesMagnifyingFactor: (x: Int, y: Int) = (2, 1)
     
