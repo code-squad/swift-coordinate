bcf18fc9c90ae1a9663d83144e97bfd58ecd8469
Jake YOU <jake.you@kinemaster.com>
2019-09-25 00:19:49 +0900

    Implement step-4 requirement

---
+++ CoordinateCalculator/CoordinateCalculator/MyRect.swift
@@ -0,0 +1,54 @@
+//
+//  MyRect.swift
+//  CoordinateCalculator
+//
+//  Created by jake.you on 2019/09/24.
+//  Copyright © 2019 Codesquad Inc. All rights reserved.
+//
+
+import Foundation
+
+struct MyRect {
+    private(set) var leftTop = MyPoint()
+    private(set) var rightBottom = MyPoint()
+    
+    private var leftBottom: MyPoint {
+        return MyPoint(x: leftTop.x, y: rightBottom.y)
+    }
+    
+    private var rightTop: MyPoint {
+        return MyPoint(x: rightBottom.x , y: leftTop.y)
+    }
+
+    init(origin: MyPoint, size: CGSize) {
+        self.leftTop = origin
+        let rightBottomX = origin.x + Int(size.width)
+        let rightBottomY = origin.y - Int(size.height)
+        self.rightBottom = MyPoint(x: rightBottomX, y: rightBottomY)
+    }
+    
+    init(points: [MyPoint]) {
+        var sorted = points.sorted { $0.x < $1.x }
+        var leftPoints = [sorted[0], sorted[1]].sorted { $0.y > $1.y }
+        var rightPoints = [sorted[2], sorted[3]].sorted { $0.y > $1.y }
+
+        self.leftTop = leftPoints[0]
+        self.rightBottom = rightPoints[1]
+    }
+    
+    var extent: Double {
+        return MyLine(x: leftTop, y: rightTop).distance * MyLine(x: leftTop, y: leftBottom).distance
+    }
+}
+
+extension MyRect: Shape {
+    var vertices: [CoordinatePresentable] {
+        return [leftTop, rightTop, leftBottom, rightBottom]
+    }
+    
+    var description: String? {
+        return "사각형의 넓이는:\(extent)"
+    }
+}
+
+
--- CoordinateCalculator/CoordinateCalculator/ShapeFactory.swift
+++ CoordinateCalculator/CoordinateCalculator/ShapeFactory.swift
@@ -20,20 +20,38 @@ struct ShapeFactory {
         }
     }
     
+    static private func createLine( points: [(x: Int, y: Int)]) -> MyLine {
+        let pointA = MyPoint(x: points[0].x, y: points[0].y)
+        let pointB = MyPoint(x: points[1].x, y: points[1].y)
+        return MyLine(x: pointA, y: pointB)
+    }
+    
+    static private func createTriangle( points: [(x: Int, y: Int)]) -> MyTriangle {
+        let pointA = MyPoint(x: points[0].x, y: points[0].y)
+        let pointB = MyPoint(x: points[1].x, y: points[1].y)
+        let pointC = MyPoint(x: points[2].x, y: points[2].y)
+        return MyTriangle(pointA: pointA, pointB: pointB, pointC: pointC)
+    }
+    
+    static private func createRect(points: [(x: Int, y: Int)]) -> MyRect {
+        let points = [MyPoint(x: points[0].x, y: points[0].y),
+                      MyPoint(x: points[1].x, y: points[1].y),
+                      MyPoint(x: points[2].x, y: points[2].y),
+                      MyPoint(x: points[3].x, y: points[3].y)]
+        return MyRect(points: points)
+    }
+    
     static private func createShape(points: [(x: Int, y: Int)]) -> Shape {
         var shape: Shape
         switch points.count {
         case 1:
             shape = MyPoint(x: points[0].x, y: points[0].y)
         case 2:
-            let pointA = MyPoint(x: points[0].x, y: points[0].y)
-            let pointB = MyPoint(x: points[1].x, y: points[1].y)
-            shape = MyLine(x: pointA, y: pointB)
+            shape = createLine(points: points)
         case 3:
-            let pointA = MyPoint(x: points[0].x, y: points[0].y)
-            let pointB = MyPoint(x: points[1].x, y: points[1].y)
-            let pointC = MyPoint(x: points[2].x, y: points[2].y)
-            shape = MyTriangle(pointA: pointA, pointB: pointB, pointC: pointC)
+            shape = createTriangle(points: points)
+        case 4:
+            shape = createRect(points: points)
             
         default:
             // Will test Later ater implementing step-3 requrement
